3b119611d4b46bd314d88e0864408dbfa08e2fef
Vladimír Oraný <vladimir@orany.cz>
2014-02-12 08:54:47 +0100

    null-safe relationships marshalling

--- ModelCatalogueCorePlugin/src/groovy/uk/co/mc/core/util/marshalling/MarshallerUtils.groovy
+++ ModelCatalogueCorePlugin/src/groovy/uk/co/mc/core/util/marshalling/MarshallerUtils.groovy
@@ -21,7 +21,7 @@ class MarshallerUtils {
             ]
         }
 
-        return toMarshall.sort{it.destinationName}
+        return toMarshall?.sort{it.destinationName} ?: []
     }
 
     static marshallOutgoingRelationships(CatalogueElement element){
@@ -35,7 +35,7 @@ class MarshallerUtils {
             ]
         }
 
-        return toMarshall.sort{it.sourceName}
+        return toMarshall?.sort{it.sourceName} ?: []
 
     }
 
