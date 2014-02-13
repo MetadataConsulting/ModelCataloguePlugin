d69d0eb534b95afc266f193b22d1daf36a4d54e7
Vladimír Oraný <vladimir@orany.cz>
2014-02-13 11:22:00 +0100

    enumerated type reworked

--- ModelCatalogueCorePlugin/grails-app/domain/uk/co/mc/core/EnumeratedType.groovy
+++ ModelCatalogueCorePlugin/grails-app/domain/uk/co/mc/core/EnumeratedType.groovy
@@ -9,19 +9,110 @@ package uk.co.mc.core
 
 class EnumeratedType extends DataType {
 
-    // TODO: save enumerations as string in predefined format
+    private static final QUOTED_CHARS = [
+            "\\": "&#92;",
+            ":": "&#58;",
+            "|": "&#124;",
+            "%": "&#37;",
+    ]
 
-    Map<String, String> enumerations
+    String enumAsString
 
     static constraints = {
-        enumerations nullable: false, validator: { val, obj ->
+        enumAsString nullable: false, validator: { encodedVal, obj ->
+            Map<String, String> val = stringToMap(encodedVal)
             if (!val) return true
             if (val.size() < 2) return false
             return true
         }
     }
 
+    static transients = ['enumerations']
+
+    /**
+     * Sets the map containing the enum values.
+     *
+     * The map is encoded and stored in {@link #enumAsString} field.
+     *
+     * @param map the map containing the enum values
+     */
+    void setEnumerations(Map<String, String> map) {
+        enumAsString = mapToString(map)
+    }
+
+    /**
+     * Returns the map containing the enum values.
+     *
+     * The map is decoded from {@link #enumAsString} field.
+     *
+     * @return the map containing the enum values
+     */
+    Map<String, String> getEnumerations() {
+        stringToMap(enumAsString)
+    }
+
     String toString() {
         "${getClass().simpleName}[id: ${id}, name: ${name}, enumerations: ${enumerations}]"
     }
+
+    /**
+     * Finds all items which contains given key as key in the enumeration.
+     * @param key key to be looked for
+     * @return list of items which contains given key as key in the enumeration
+     */
+    static List<EnumeratedType> findAllByEnumeratedKey(String key) {
+        findAllByEnumAsStringIlike("%${quote(key)}%").findAll {
+            it.enumerations.containsKey(key)
+        }
+    }
+
+    /**
+     * Finds all items which contains given value as value in the enumeration.
+     * @param value value to be looked for
+     * @return list of items which contains given value as value in the enumeration
+     */
+    static List<EnumeratedType> findAllByEnumeratedValue(String value) {
+        findAllByEnumAsStringIlike("%${quote(value)}%").findAll {
+            it.enumerations.containsValue(value)
+        }
+    }
+
+    private static String mapToString(Map<String, String> map) {
+        if (map == null) return null
+        map.sort() collect { key, val ->
+            "${quote(key)}:${quote(val)}"
+        }.join('|')
+    }
+
+    private static Map<String, String> stringToMap(String s) {
+        if (s == null) return null
+        Map<String, String> ret = [:]
+        s.split(/\|/).each { String part ->
+            if (!part) return
+            String[] pair = part.split(/:/)
+            if (pair.length != 2) throw new IllegalArgumentException("Wrong enumerated value '$part' in encoded enumeration '$s'")
+            ret[unquote(pair[0])] = unquote(pair[1])
+        }
+        return ret
+    }
+
+    private static String quote(String s) {
+        if (s == null) return null
+        String ret = s
+        QUOTED_CHARS.each { original, replacement ->
+            ret = ret.replace(original, replacement)
+        }
+        ret
+    }
+
+    private static String unquote(String s) {
+        if (s == null) return null
+        String ret = s
+        QUOTED_CHARS.reverseEach { original, pattern ->
+            ret = ret.replace(pattern, original)
+        }
+        ret
+    }
+
+
 }
