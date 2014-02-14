8e3b929fe2c8f4638a0f538acdf884de9a24a867
Vladimír Oraný <vladimir@orany.cz>
2014-02-14 13:28:08 +0100

    removed todo which is no longer needed

--- ModelCatalogueCorePlugin/grails-app/controllers/uk/co/mc/core/MeasurementUnitController.groovy
+++ ModelCatalogueCorePlugin/grails-app/controllers/uk/co/mc/core/MeasurementUnitController.groovy
@@ -13,7 +13,6 @@ class MeasurementUnitController extends RestfulController<MeasurementUnit> {
         // using deep we get relationships rendered
         // the relationships are not rendered directly but just the id, link and name is returned
         // for source, destination and type
-        // TODO: the right render from the unit tests must be registrered
         JSON.use('deep')
         XML.use('deep')
     }
