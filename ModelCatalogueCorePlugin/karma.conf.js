a67f1f5aab2279d4401ecfee98988dd1a93fec3b
Vladimír Oraný <vladimir@orany.cz>
2014-03-03 09:26:05 +0100

    fixed path to jquery

--- ModelCatalogueCorePlugin/karma.conf.js
+++ ModelCatalogueCorePlugin/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function(config) {
 
         files: [
             // Required libraries
-            'grails-app/assets/bower_components/jquery/jquery.js',
+            'grails-app/assets/bower_components/jquery/dist/jquery.js',
             'grails-app/assets/bower_components/angular/angular.js',
 
             // App under test
