09d758313de666e0d008d7432dc03a861840b250
Adam Milward <adam.milward@outlook.com>
2014-03-07 16:59:26 +0000

    finishing off domain modeller service........ maps relationships manytomany, onetomany, manytoone

--- ModelCatalogueCorePlugin/grails-app/services/org/modelcatalogue/core/DomainModellerService.groovy
+++ ModelCatalogueCorePlugin/grails-app/services/org/modelcatalogue/core/DomainModellerService.groovy
@@ -16,6 +16,8 @@ class DomainModellerService {
         def conceptualDomain = new ConceptualDomain(name:"uk.co.mc.core", description: "Model Catalogue Conceptual Domain").save()
         def domainClasses =  grailsApplication.domainClasses
 
+
+//create models for all the domain classes first (so we can create relationships to them)
         domainClasses.each{ GrailsDomainClass domainClass ->
 
             Model model = new Model(
@@ -24,34 +26,77 @@ class DomainModellerService {
 
             model.addToHasContextOf(conceptualDomain)
 
+        }
+
+//model all the domain class properties
+        domainClasses.each{ GrailsDomainClass domainClass ->
+
             def constraints = domainClass.getConstrainedProperties()
             def properties = domainClass.getProperties()
 
-
             properties.each{ property ->
                 String name = property.name
-                DataType dataType = DataType.findByName(property.type.name)
-                if(!dataType){
-                    dataType = new DataType(
-                        name: property.type.name
-                        ).save()
+                Model model = Model.findByName(domainClass.shortName)
+                def modelName = (domainClass.getRelatedClassType(name)) ? domainClass.getRelatedClassType(name).getSimpleName() : null
+
+                 if(!domainClass.hasPersistentProperty(name)){
+
+                     DataElement dataElement = new DataElement(
+                             name: name
+                     ).save()
+
+                     linkTransients(dataElement, model)
+
+                 }else if(domainClass.isBidirectional(name)){
+
+                     RelationshipType manyToMany = RelationshipType.findByName("manyToMany")
+                     if(!manyToMany){
+                         manyToMany= new RelationshipType(destinationClass: Model, sourceClass: Model, destinationToSource: "many to many", sourceToDestination: "many to many", name: "manyToMany").save()
+                     }
+                     linkModels(modelName, manyToMany, model)
+
+
+                }else if(domainClass.isManyToOne(name)){
+
+                     RelationshipType manyToOne = RelationshipType.findByName("manyToOne")
+                     if(!manyToOne){
+                         manyToOne = new RelationshipType(destinationClass: Model, sourceClass: Model, sourceToDestination: "many to one", destinationToSource: "one to many", name: "manyToOne").save()
+                     }
+                     linkModels(modelName, manyToOne, model)
+
+                }else if(domainClass.isOneToMany(name)){
+
+                     RelationshipType oneToMany = RelationshipType.findByName("oneToMany")
+                     if(!oneToMany){
+                          oneToMany = new RelationshipType(destinationClass: Model, sourceClass: Model, destinationToSource: "many to one", sourceToDestination: "one to many", name: "oneToMany").save()
+                     }
+                     linkModels(modelName, oneToMany, model)
+
+
+                }else {
+
+                    DataType dataType = DataType.findByName(property.type.name)
+                    if(!dataType){
+                        dataType = new DataType(
+                            name: property.type.name
+                            ).save()
+                    }
+
+                    def constraint = (constraints.get(name))? constraints.get(name).appliedConstraints : null
+
+                    ValueDomain valueDomain = new ValueDomain(
+                                        name: name,
+                                        dataType: dataType,
+                                        description: formatDomainConstraints(constraint)
+                                        ).save()
+                    DataElement dataElement = new DataElement(
+                                        name: name
+                                        ).save()
+
+                    dataElement.addToContainedIn(model)
+                    dataElement.addToInstantiatedBy(valueDomain)
+                    valueDomain.addToIncludedIn(conceptualDomain)
                 }
-
-                def constraint = (constraints.get(name))? constraints.get(name).appliedConstraints : null
-
-
-                ValueDomain valueDomain = new ValueDomain(
-                                    name: name,
-                                    dataType: dataType,
-                                    description: formatDomainConstraints(constraint)
-                                    ).save()
-                DataElement dataElement = new DataElement(
-                                    name: name
-                                    ).save()
-
-                dataElement.addToContainedIn(model)
-                dataElement.addToInstantiatedBy(valueDomain)
-                valueDomain.addToIncludedIn(conceptualDomain)
 
             }
         }
@@ -61,25 +106,42 @@ class DomainModellerService {
         println "-"*100
         println " "
         def models = Model.list()
+
         models.each{ model ->
+
+            def manyToMany = model.getRelationsByType(RelationshipType.findByName("manyToMany")).collect{it.name}
+            def oneToMany = model.getRelationsByType(RelationshipType.findByName("oneToMany")).collect{it.name}
+            def manyToOne = model.getRelationsByType(RelationshipType.findByName("manyToOne")).collect{it.name}
+            def transients = model.getRelationsByType(RelationshipType.findByName("transients")).collect{it.name}
+
             println "-"*100
             println "Model Name : ${model.name}"
             println "-"*100
             println "contexts"
-            println "${model.hasContextOf[0]?.name}"
-            println " -- "
+            println "--${model.hasContextOf[0]?.name}"
+            if(manyToMany){
+            println "has many (manyToMany)"
+            println "--${manyToMany}"
+            }
+            if(oneToMany){
+            println "has many (oneToMany)"
+            println "--${oneToMany}"
+            }
+            if(manyToOne){
+            println "belongs to (manyToOne)"
+            println "--${manyToOne}"
+            }
+            if(transients){
+                println "transients"
+                println "--${transients}"
+            }
             println "contains"
             def dataElements = model.contains
             dataElements.each{ DataElement dataElement->
                 def instantiatedBy = dataElement.instantiatedBy
                 println " -- ${dataElement.name} (${instantiatedBy[0].dataType.name}) (${instantiatedBy[0].description})"
             }
-
         }
-
-
-
-
     }
 
 
@@ -124,9 +186,31 @@ class DomainModellerService {
 
     }
 
+    protected static void linkModels(modelName, manyToMany, model){
+        if(modelName){
+            Model relatedModel = Model.findByName(modelName)
+            if(relatedModel){
+                Relationship.link(model, relatedModel, manyToMany)
+            }
+        }
+    }
+
+
+    protected static void linkTransients(element, model){
+
+        RelationshipType transients = RelationshipType.findByName("transients")
+        if(!transients){
+            transients= new RelationshipType(destinationClass: DataElement, sourceClass: Model, destinationToSource: "transient properties of", sourceToDestination: "transient properties for", name: "transients").save()
+        }
+        if(element){
+            Relationship.link(model, element, transients)
+        }
+
+    }
+
 
     def domainConstraintsToRegex(constraints){
-        //FIXME at present this only looks at the maxSize constraint
+        //TODO at present this only looks at the maxSize constraint
         //we may need to add an additional concept - constraint - then we can add constraints
         //to value domains. If we give constraints languages we can start to look at converting
         //different constraints into the model i.e. sql, oracle, groovy etc.
--- ModelCatalogueCorePluginTestApp/grails-app/conf/BootStrap.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/conf/BootStrap.groovy
@@ -3,12 +3,14 @@ import org.modelcatalogue.core.RelationshipType
 class BootStrap {
 
     def importService
+    def domainModellerService
 
     def init = { servletContext ->
 
         RelationshipType.initDefaultRelationshipTypes()
 
        // importService.importData()
+       // domainModellerService.modelDomains()
 
     }
     def destroy = {
