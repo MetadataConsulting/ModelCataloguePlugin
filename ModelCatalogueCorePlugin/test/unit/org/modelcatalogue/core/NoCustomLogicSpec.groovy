3d27ad9e9dfb956135eb58b782abd59aeeb6da42
Adam Milward <adam.milward@outlook.com>
2014-03-27 15:37:20 +0000

    data type now has custom logic so should not be included in the no custom logic test

--- ModelCatalogueCorePlugin/test/unit/org/modelcatalogue/core/NoCustomLogicSpec.groovy
+++ ModelCatalogueCorePlugin/test/unit/org/modelcatalogue/core/NoCustomLogicSpec.groovy
@@ -22,12 +22,12 @@ class NoCustomLogicSpec extends Specification {
         ConceptualDomainController  | CatalogueElementController     | MeasurementUnitController.declaredMethods.findAll { !it.synthetic } .size()
         DataElementController       | CatalogueElementController     | MeasurementUnitController.declaredMethods.findAll { !it.synthetic } .size()
         //DataTypeController          | CatalogueElementController     | MeasurementUnitController.declaredMethods.findAll { !it.synthetic } .size()
-        // EnumeratedTypeController    | CatalogueElementController     | MeasurementUnitController.declaredMethods.findAll { !it.synthetic } .size() + customAndTestedMethodsCount
+        //EnumeratedTypeController    | CatalogueElementController     | MeasurementUnitController.declaredMethods.findAll { !it.synthetic } .size() + customAndTestedMethodsCount
         ModelController             | CatalogueElementController     | MeasurementUnitController.declaredMethods.findAll { !it.synthetic } .size()
-        // RelationshipTypeController  | AbstractRestfulController      | MeasurementUnitController.declaredMethods.findAll { !it.synthetic } .size() + customAndTestedMethodsCount
+        //RelationshipTypeController  | AbstractRestfulController      | MeasurementUnitController.declaredMethods.findAll { !it.synthetic } .size() + customAndTestedMethodsCount
         Elements                    | ListWrapper                    | 0
         ConceptualDomainMarshaller  | CatalogueElementMarshallers    | 0
-        DataTypeMarshaller          | CatalogueElementMarshallers    | 0
+        //DataTypeMarshaller          | CatalogueElementMarshallers    | 0
         ModelMarshaller             | ExtendibleElementMarshallers   | 0
     }
 
