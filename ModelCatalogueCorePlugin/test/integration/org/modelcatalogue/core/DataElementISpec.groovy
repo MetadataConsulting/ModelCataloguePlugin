a93c4b903528e2ca87eb8c8ba1fb6164a7dd2dfc
Adam Milward <adam.milward@outlook.com>
2014-04-22 18:01:33 +0100

    fixing test: changing code to model catalogue id

--- ModelCatalogueCorePlugin/test/integration/org/modelcatalogue/core/DataElementISpec.groovy
+++ ModelCatalogueCorePlugin/test/integration/org/modelcatalogue/core/DataElementISpec.groovy
@@ -49,7 +49,7 @@ class DataElementISpec extends AbstractIntegrationSpec{
     def "create writer data elements with the same code dataElement"(){
 
         when:
-        def dataElementInstance2 = new DataElement(name: "result2", description: "this is the the result2 description", code: "XXX_1")
+        def dataElementInstance2 = new DataElement(name: "result2", description: "this is the the result2 description", modelCatalogueId: "XXX_1")
         dataElementInstance2.validate()
 
         then:
