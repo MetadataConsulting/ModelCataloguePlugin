42d794254fdb2b9667aaa2bcb2299b0aa398aa5f
Vladimír Oraný <vladimir@orany.cz>
2014-07-14 09:18:02 +0200

    reworked storage service and use in newly extracted asset service

--- ModelCatalogueCorePlugin/grails-app/controllers/org/modelcatalogue/core/AssetController.groovy
+++ ModelCatalogueCorePlugin/grails-app/controllers/org/modelcatalogue/core/AssetController.groovy
@@ -9,6 +9,7 @@ import java.security.MessageDigest
 class AssetController extends AbstractPublishedElementController<Asset> {
 
     StorageService modelCatalogueStorageService
+    AssetService assetService
 
     static allowedMethods = [upload: 'POST', download: 'GET']
 
@@ -19,64 +20,16 @@ class AssetController extends AbstractPublishedElementController<Asset> {
     def upload() {
         MultipartFile file = request.getFile('asset')
 
-        if (file.size > modelCatalogueStorageService.maxFileSize) {
-            Asset asset = new Asset()
-            asset.errors.rejectValue('md5', 'asset.uploadfailed', "You cannot upload files greater than ${toBytes(modelCatalogueStorageService.maxFileSize)}")
-            respond asset.errors, view: 'create' // STATUS CODE 422
+        Asset asset = assetService.upload(params.long('id'), params.name, params.description, file)
+
+        if (!asset) {
+            notFound()
             return
         }
-
-        Asset asset = new Asset()
-
-        asset.name              = params.name ?: file.originalFilename
-        asset.description       = params.description
-        asset.contentType       = file.contentType
-        asset.size              = file.size
-        asset.originalFileName  = file.originalFilename
-
-        asset.validate()
 
         if (asset.hasErrors()) {
             respond asset.errors, view: 'create' // STATUS CODE 422
             return
-        }
-
-        if (params.id) {
-            Asset existing = Asset.get(params.id)
-
-            if (!existing) {
-                notFound()
-                return
-            }
-
-            publishedElementService.archiveAndIncreaseVersion(existing)
-
-            existing.name              = asset.name
-            existing.description       = asset.description
-            existing.contentType       = asset.contentType
-            existing.size              = asset.size
-            existing.originalFileName  = asset.originalFileName
-
-            asset = existing
-        }
-
-        asset.save()
-
-        DigestInputStream dis = null
-
-        try {
-            MessageDigest md5 = MessageDigest.getInstance('MD5')
-            dis = new DigestInputStream(file.inputStream, md5)
-            modelCatalogueStorageService.store('assets', asset.modelCatalogueId, file.contentType, dis)
-            asset.md5 = DigestUtils.md5DigestAsHex(md5.digest())
-            asset.save()
-        } catch (e) {
-            log.error('Exception storing asset ' + asset.name, e)
-            asset.errors.rejectValue('md5', 'asset.uploadfailed', "There were problems uploading file $file.originalFilename")
-            respond asset.errors, view: 'create' // STATUS CODE 422
-            return
-        } finally {
-            dis?.close()
         }
 
         respond asset
@@ -121,19 +74,6 @@ class AssetController extends AbstractPublishedElementController<Asset> {
         response.contentType    = asset.contentType
         response.contentLength  = asset.size
         response.outputStream << modelCatalogueStorageService.fetch('assets', asset.modelCatalogueId)
-    }
-
-    private static final long GIGA = 1024 * 1024 * 1024
-    private static final long MEGA = 1024 * 1024
-    private static final long KILO = 1024
-
-    String toBytes(Long value) {
-        if (!value) return "0 B"
-
-        if (value > GIGA) return String.format("%.2f GB", value / GIGA)
-        if (value > MEGA) return String.format("%.2f MB", value / MEGA)
-        if (value > KILO) return String.format("%.2f KB", value / KILO)
-        "$value B"
     }
 
 }
---
+++ ModelCatalogueCorePlugin/grails-app/services/org/modelcatalogue/core/AssetService.groovy
@@ -0,0 +1,122 @@
+package org.modelcatalogue.core
+
+import groovy.transform.CompileStatic
+import org.apache.commons.io.input.CountingInputStream
+import org.apache.commons.io.output.CountingOutputStream
+import org.springframework.util.DigestUtils
+import org.springframework.web.multipart.MultipartFile
+
+import java.security.DigestInputStream
+import java.security.DigestOutputStream
+import java.security.MessageDigest
+
+/**
+ * Created by ladin on 10.07.14.
+ */
+@CompileStatic
+class AssetService {
+
+    StorageService modelCatalogueStorageService
+    PublishedElementService publishedElementService
+
+    private static final long GIGA = 1024 * 1024 * 1024
+    private static final long MEGA = 1024 * 1024
+    private static final long KILO = 1024
+
+    static String toBytes(Long value) {
+        if (!value) return "0 B"
+
+        if (value > GIGA) return String.format("%.2f GB", value / GIGA)
+        if (value > MEGA) return String.format("%.2f MB", value / MEGA)
+        if (value > KILO) return String.format("%.2f KB", value / KILO)
+        "$value B"
+    }
+
+    Asset upload(Long id, String name, String description, MultipartFile file) {
+        if (file.size > modelCatalogueStorageService.maxFileSize) {
+            Asset asset = new Asset()
+            asset.errors.rejectValue('md5', 'asset.uploadfailed', "You cannot upload files greater than ${toBytes(modelCatalogueStorageService.maxFileSize)}")
+            return asset
+        }
+
+        Asset asset = new Asset()
+
+        asset.name              = name ?: file.originalFilename
+        asset.description       = description
+        asset.contentType       = file.contentType
+        asset.size              = file.size
+        asset.originalFileName  = file.originalFilename
+
+        asset.validate()
+
+        if (asset.hasErrors()) {
+            return asset
+        }
+
+        if (id) {
+            Asset existing = Asset.get(id)
+
+            if (!existing) {
+                return null
+            }
+
+            publishedElementService.archiveAndIncreaseVersion(existing)
+
+            existing.name              = asset.name
+            existing.description       = asset.description
+            existing.contentType       = asset.contentType
+            existing.originalFileName  = asset.originalFileName
+
+            asset = existing
+        }
+
+        asset.save()
+
+
+        try {
+            storeAssetFromFile(file, asset)
+        } catch (e) {
+            log.error('Exception storing asset ' + asset.name, e)
+            asset.errors.rejectValue('md5', 'asset.uploadfailed', "There were problems uploading file $file.originalFilename")
+        }
+
+        return asset
+    }
+
+    void storeAssetFromFile(MultipartFile file, Asset asset) {
+        DigestInputStream dis = null
+        try {
+            MessageDigest md5 = MessageDigest.getInstance('MD5')
+            dis = new DigestInputStream(file.inputStream, md5)
+            CountingInputStream countingInputStream = new CountingInputStream(dis)
+            modelCatalogueStorageService.store('assets', asset.modelCatalogueId, file.contentType, { OutputStream it -> it << dis })
+            asset.md5 = DigestUtils.md5DigestAsHex(md5.digest())
+            asset.size = countingInputStream.byteCount
+            asset.save()
+        } finally {
+            dis?.close()
+        }
+    }
+
+    void storeAssetWithSteam(Asset asset, String contentType, Closure withOutputStream) {
+        MessageDigest md5 = MessageDigest.getInstance('MD5')
+        long size = asset.size
+        modelCatalogueStorageService.store('assets', asset.modelCatalogueId, contentType) { OutputStream it ->
+            DigestOutputStream dos      = null
+            CountingOutputStream cos    = null
+            try {
+                dos = new DigestOutputStream(it, md5)
+                cos = new CountingOutputStream(dos)
+                withOutputStream(cos)
+                size = cos.byteCount
+            } finally {
+                dos?.close()
+                cos?.close()
+            }
+        }
+        asset.md5 = DigestUtils.md5DigestAsHex(md5.digest())
+        asset.size = size
+        asset.save()
+    }
+
+}
