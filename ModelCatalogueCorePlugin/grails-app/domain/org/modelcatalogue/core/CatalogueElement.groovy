1d1a6c8af33ea01eaf7ee4e1ce231f1fa440aa85
Vladimír Oraný <vladimir@orany.cz>
2014-07-28 14:14:47 +0200

    fixed published elements could no longer be updated or finalized

--- ModelCatalogueCorePlugin/grails-app/domain/org/modelcatalogue/core/CatalogueElement.groovy
+++ ModelCatalogueCorePlugin/grails-app/domain/org/modelcatalogue/core/CatalogueElement.groovy
@@ -31,7 +31,7 @@ abstract class CatalogueElement {
     static constraints = {
         name size: 1..255
         description nullable: true, maxSize: 2000
-		modelCatalogueId nullable: true, unique: true, maxSize: 255, matches: '(?i)MC_([A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12})_\\d+'
+		modelCatalogueId bindable: false, nullable: true, unique: true, maxSize: 255, matches: '(?i)MC_([A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12})_\\d+'
     }
 
     //WIP gormElasticSearch will support aliases in the future for now we will use searchable
