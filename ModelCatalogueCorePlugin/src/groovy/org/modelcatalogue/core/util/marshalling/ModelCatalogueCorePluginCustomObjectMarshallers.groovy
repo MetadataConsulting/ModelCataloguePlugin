34558af476b8438f42751a77069a64342d6aff7b
Vladimír Oraný <vladimir@orany.cz>
2014-08-07 11:56:36 +0200

    cleaned up custom marshaller #177

--- ModelCatalogueCorePlugin/src/groovy/org/modelcatalogue/core/util/marshalling/ModelCatalogueCorePluginCustomObjectMarshallers.groovy
+++ ModelCatalogueCorePlugin/src/groovy/org/modelcatalogue/core/util/marshalling/ModelCatalogueCorePluginCustomObjectMarshallers.groovy
@@ -1,16 +1,10 @@
 package org.modelcatalogue.core.util.marshalling
 
-import org.codehaus.groovy.grails.web.mapping.LinkGenerator
-import org.modelcatalogue.core.RelationshipTypeService
-import org.modelcatalogue.core.reports.ReportsRegistry
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory
 
 class ModelCatalogueCorePluginCustomObjectMarshallers {
 
-    @Autowired ReportsRegistry reportsRegistry
-    @Autowired LinkGenerator linkGenerator
-    @Autowired RelationshipTypeService relationshipTypeService
     @Autowired AutowireCapableBeanFactory autowireCapableBeanFactory
 
     List<AbstractMarshallers> marshallers = []
