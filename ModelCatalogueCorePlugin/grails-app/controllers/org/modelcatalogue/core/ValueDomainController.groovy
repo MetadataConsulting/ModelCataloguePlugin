bda6e9fe41473c7b4a9121491c1a06d99136f30c
Vladimír Oraný <vladimir@orany.cz>
2014-08-21 15:14:01 +0200

    value domain in data element #282 - WIP

--- ModelCatalogueCorePlugin/grails-app/controllers/org/modelcatalogue/core/ValueDomainController.groovy
+++ ModelCatalogueCorePlugin/grails-app/controllers/org/modelcatalogue/core/ValueDomainController.groovy
@@ -1,9 +1,26 @@
 package org.modelcatalogue.core
 
+import org.modelcatalogue.core.util.Lists
+
 class ValueDomainController extends AbstractExtendibleElementController<ValueDomain> {
 
     ValueDomainController() {
         super(ValueDomain, false)
+    }
+
+
+    def valueDomains(Integer max){
+        handleParams(max)
+        DataType dataType = queryForResource(params.id)
+        if (!dataType) {
+            notFound()
+            return
+        }
+
+        reportCapableRespond new DataElements(list: Lists.fromCriteria(params, ValueDomain, "/${resourceName}/${params.id}/valueDomain", "valueDomains"){
+            eq "dataType", dataType
+        })
+
     }
 
 }
--- ModelCatalogueCorePlugin/src/groovy/org/modelcatalogue/core/util/marshalling/DataElementMarshaller.groovy
+++ ModelCatalogueCorePlugin/src/groovy/org/modelcatalogue/core/util/marshalling/DataElementMarshaller.groovy
@@ -1,11 +1,26 @@
 package org.modelcatalogue.core.util.marshalling
 
+import grails.converters.XML
 import org.modelcatalogue.core.DataElement
 
 class DataElementMarshaller extends PublishedElementMarshallers {
 
     DataElementMarshaller() {
         super(DataElement)
+    }
+
+    protected Map<String, Object> prepareJsonMap(el) {
+        if (!el) return [:]
+        def ret = super.prepareJsonMap(el)
+        ret.putAll valueDomain: el.valueDomain
+        ret
+    }
+
+    protected void buildXml(el, XML xml) {
+        super.buildXml(el, xml)
+        xml.build {
+            valueDomain el.valueDomain
+        }
     }
 
 }
