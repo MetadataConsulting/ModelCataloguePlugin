c33e4fd178efde9abde0f64fce22478f3a7b1ee1
Adam Milward <adam.milward@outlook.com>
2014-10-02 10:47:59 +0100

    undoing change for search

--- ModelCatalogueCorePlugin/grails-app/services/org/modelcatalogue/core/ModelCatalogueSearchService.groovy
+++ ModelCatalogueCorePlugin/grails-app/services/org/modelcatalogue/core/ModelCatalogueSearchService.groovy
@@ -102,6 +102,15 @@ class ModelCatalogueSearchService implements SearchCatalogue {
 
             searchResults.searchResults = results.items
             searchResults.total = results.total
+        } else if (ConceptualDomain.isAssignableFrom(resource) || Classification.isAssignableFrom(resource) || DataType.isAssignableFrom(resource) || MeasurementUnit.isAssignableFrom(resource) ) {
+            DetachedCriteria criteria = new DetachedCriteria(resource)
+            criteria.or {
+                ilike('name', query)
+                ilike('description', query)
+                ilike('modelCatalogueId', query)
+            }
+            searchResults.searchResults = criteria.list(params)
+            searchResults.total = criteria.count()
         } else if (CatalogueElement.isAssignableFrom(resource)) {
             String alias = CatalogueElement.simpleName[0].toLowerCase()
             String listQuery = """
@@ -122,14 +131,6 @@ class ModelCatalogueSearchService implements SearchCatalogue {
 
             searchResults.searchResults = results.items
             searchResults.total = results.total
-
-//            criteria.or {
-//                ilike('name', query)
-//                ilike('description', query)
-//                ilike('modelCatalogueId', query)
-//            }
-//            searchResults.searchResults = criteria.list(params)
-//            searchResults.total = criteria.count()
         } else if (RelationshipType.isAssignableFrom(resource)) {
             searchResults.searchResults = resource.findAllByNameIlikeOrSourceToDestinationIlikeOrDestinationToSourceIlike(query, query, query, params)
             searchResults.total = resource.countByNameIlikeOrSourceToDestinationIlikeOrDestinationToSourceIlike(query, query, query, params)
