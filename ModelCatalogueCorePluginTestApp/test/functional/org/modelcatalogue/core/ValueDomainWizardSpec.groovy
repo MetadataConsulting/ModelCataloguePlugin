5a5e7215f68de1a49a4d28ef7e2d938333d311ae
David Milward <david@beenleigh.com>
2014-11-05 15:19:09 +0000

    Added new pages

---
+++ ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/ValueDomainWizardSpec.groovy
@@ -0,0 +1,21 @@
+package org.modelcatalogue.core
+
+import geb.spock.GebReportingSpec
+import spock.lang.Stepwise
+
+/**
+ * Created by david on 05/11/14.
+ */
+@Stepwise
+class DataElementWizardSpec extends GebReportingSpec  {
+
+
+    def "Add an enumerated datatype to the value domain"(){
+
+    }
+
+    def "Check that the new classification exists"(){
+
+    }
+
+}
