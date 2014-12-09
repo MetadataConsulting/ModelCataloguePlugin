4e17725b2b50df42872be0a3b29c0dc30ecfe1e8
Vladimír Oraný <vladimir@orany.cz>
2014-12-09 09:59:59 +0000

    for mc import, create for all created or touched elements "related to" relationship

--- ModelCatalogueCorePlugin/grails-app/controllers/org/modelcatalogue/core/DataImportController.groovy
+++ ModelCatalogueCorePlugin/grails-app/controllers/org/modelcatalogue/core/DataImportController.groovy
@@ -116,6 +116,9 @@ class DataImportController  {
                     Asset updated = finalizeAsset(id)
                     Classification classification = created.find { it instanceof Classification } as Classification
                     classifyAsset(updated, classification)
+                    for (CatalogueElement element in created) {
+                        asset.addToRelatedTo(element)
+                    }
                 } catch (Exception e) {
                     logError(id, e)
                 }
