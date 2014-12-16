cfa1f445bb44e706102089354633817189ef9978
Vladimír Oraný <vladimir@orany.cz>
2014-12-16 13:31:06 +0100

    trying chrome instead of firefox

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -19,6 +19,6 @@ cd ModelCatalogueCorePluginTestApp
 ./grailsw clean-all
 ./grailsw refresh-dependencies
 ./grailsw test-app integration:
-./grailsw test-app functional: -war
+./grailsw test-app functional: -war -Dgeb.env=chrome
 
 cd ..
