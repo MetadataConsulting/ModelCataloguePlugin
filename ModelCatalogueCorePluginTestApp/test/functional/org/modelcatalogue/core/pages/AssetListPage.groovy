120064f19467d7a0a75e3d36fc33d7777f336000
Vladimír Oraný <vladimir@orany.cz>
2015-01-13 15:15:25 +0100

    MET-6 write geb test to upload asset (xsd) MET-7 validate against valid xml MET-8 validate against invalid xml

---
+++ ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/pages/AssetListPage.groovy
@@ -0,0 +1,17 @@
+package org.modelcatalogue.core.pages
+
+/**
+ * Created by david on 04/11/14.
+ */
+class AssetListPage extends ModelCataloguePage {
+
+    static url = "#/catalogue/asset/all"
+
+    static at = {
+        url == "#/catalogue/asset/all"
+    }
+
+    static content={
+
+    }
+}
