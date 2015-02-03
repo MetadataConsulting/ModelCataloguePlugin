ed1b910e90a77bb3de83eeb594eb013203a045bc
Vladimír Oraný <vladimir@orany.cz>
2015-02-03 15:14:29 +0100

    attempt to publish geb reports to gh-pages

---
+++ update-gh-pages.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo -e "Starting to update gh-pages\n"
+
+  #copy data we're interested in to other place
+  cp -R geb-reports $HOME/ModelCatalogueCorePluginTestApp/target/geb-reports
+
+  #go to home and setup git
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis"
+
+  #using token clone gh-pages branch
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/MetadataRegistry/ModelCataloguePlugin.git  gh-pages > /dev/null
+
+  #go into diractory and copy data we're interested in to that directory
+  cd gh-pages
+  cp -Rf $HOME/geb-reports/* .
+
+  #add, commit and push files
+  git add -f .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Done magic with geb\n"
+#fi
