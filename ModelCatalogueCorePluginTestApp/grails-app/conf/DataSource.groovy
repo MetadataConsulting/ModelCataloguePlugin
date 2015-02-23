5bd31d0d21e23a245299e67184d002099d305b17
Vladimír Oraný <vladimir@orany.cz>
2015-02-23 15:54:12 +0100

    reverted to in memory database for development

--- ModelCatalogueCorePluginTestApp/grails-app/conf/DataSource.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/conf/DataSource.groovy
@@ -15,11 +15,8 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:mysql://localhost:3306/GE?autoReconnect=true&useUnicode=yes"
-            username = "root"
-            password = "root"
-            driverClassName = "com.mysql.jdbc.Driver"
+            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
     test {
