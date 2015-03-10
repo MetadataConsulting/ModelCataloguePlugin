a06acdb78251d17887b4ee4606261c269f1b4eab
Vladimír Oraný <vladimir@orany.cz>
2015-03-10 15:33:42 +0100

    MET-645 xml roundtripping
    the xml is pretty printed so the users won't be tempted to reformat it manually themselves

--- ModelCatalogueCorePlugin/src/groovy/org/modelcatalogue/core/xml/CatalogueXmlPrinter.groovy
+++ ModelCatalogueCorePlugin/src/groovy/org/modelcatalogue/core/xml/CatalogueXmlPrinter.groovy
@@ -1,6 +1,7 @@
 package org.modelcatalogue.core.xml
 
 import groovy.xml.StreamingMarkupBuilder
+import groovy.xml.XmlUtil
 import org.modelcatalogue.core.CatalogueElement
 import org.modelcatalogue.core.ClassificationService
 import org.modelcatalogue.core.ModelService
@@ -21,11 +22,14 @@ class CatalogueXmlPrinter {
         PrintContext context = new PrintContext(classificationService, modelService)
         context.with contextConfigurer
         StreamingMarkupBuilder builder = new StreamingMarkupBuilder()
-        builder.bind {
+        return { Writer writer ->
+            def printer = new XmlNodePrinter(new PrintWriter(writer))
+            printer.preserveWhitespace = true
+            printer.print(new XmlParser().parseText(XmlUtil.serialize(builder.bind {
             catalogue ('xmlns' : NAMESPACE_URL) { doc ->
                 CatalogueElementPrintHelper.printElement(doc, element, context, null)
             }
-        }
+        })))} as Writable
     }
 
 }
