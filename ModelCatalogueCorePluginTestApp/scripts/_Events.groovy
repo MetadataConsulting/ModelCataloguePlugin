8b5c880bde2db1dee9b83ce901212240cef165ed
Vladimír Oraný <vladimir@orany.cz>
2015-03-13 11:46:38 +0100

    cosmetic - making revision number a clickable link

--- ModelCatalogueCorePluginTestApp/scripts/_Events.groovy
+++ ModelCatalogueCorePluginTestApp/scripts/_Events.groovy
@@ -1,10 +1,17 @@
 eventCompileStart = { msg ->
-    def revision = 'git rev-list --count HEAD'.execute().text.trim()
-    def hash = 'git rev-parse --short HEAD'.execute().text.trim()
-    def branch = 'git rev-parse --abbrev-ref HEAD'.execute().text.trim()
-    def version = "r${revision}.${hash}"
-    if (branch != 'master') {
-        version = "$version ($branch)"
+    try {
+        def revision = 'git rev-list --count HEAD'.execute().text.trim()
+        def hashShort = 'git rev-parse --short HEAD'.execute().text.trim()
+        def hash = 'git rev-parse HEAD'.execute().text.trim()
+        def branch = 'git rev-parse --abbrev-ref HEAD'.execute().text.trim()
+        def version = "r${revision}.${hashShort}"
+        if (branch != 'master') {
+            version = "$version ($branch)"
+        }
+        new FileOutputStream("grails-app/views/_version.gsp", false) << """<a href="https://github.com/MetadataRegistry/ModelCataloguePlugin/tree/$hash" target="_blank">$version</a>"""
+    } catch (e) {
+        println "Cannot determine current version: $e"
+        new FileOutputStream("grails-app/views/_version.gsp", false) << """Unknown Version"""
     }
-    new FileOutputStream("grails-app/views/_version.gsp", false) << version
+
 }
