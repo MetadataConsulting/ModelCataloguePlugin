1de92e755fe7e1d0f7a6c02a897212ccd864fc88
Vladimír Oraný <vladimir@orany.cz>
2015-03-27 08:32:05 +0100

    speeding up travis builds by splitting them into suites

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -4,45 +4,59 @@
 set -e
 
 cd ModelCatalogueCorePlugin
-npm install
-bower install
+
+if [ "$TEST_SUITE" = "core_karma" ] || [ "$TEST_SUITE" = "app_functional" ] || [ "$TEST_SUITE" = "" ] ; then
+    npm install
+    bower install
+fi
 
 ./grailsw clean-all
 ./grailsw refresh-dependencies
 
+# plugin unit tests
+if [ "$TEST_SUITE" = "core_unit" ] || [ "$TEST_SUITE" = "" ] ; then
+    ./grailsw test-app unit:
+    mkdir -p $HOME/reports/unit-tests-reports
+    cp -Rf target/test-reports $HOME/reports/unit-tests-reports
+fi
 
-./grailsw test-app unit:
-mkdir -p $HOME/reports/unit-tests-reports
-cp -Rf target/test-reports $HOME/reports/unit-tests-reports
-
-
-./grailsw test-app integration: org.modelcatalogue.**.*
-mkdir -p $HOME/reports/fast-integration-tests-reports
-cp -Rf target/test-reports $HOME/reports/fast-integration-tests-reports
-
+# plugin integration tests
+if [ "$TEST_SUITE" = "core_integration" ] || [ "$TEST_SUITE" = "" ] ; then
+    ./grailsw test-app integration: org.modelcatalogue.**.*
+    mkdir -p $HOME/reports/fast-integration-tests-reports
+    cp -Rf target/test-reports $HOME/reports/fast-integration-tests-reports
+fi
 
 # slow and polluting
-./grailsw test-app integration: x.org.modelcatalogue.**.*
-mkdir -p $HOME/reports/slow-integration-tests-reports
-cp -Rf target/test-reports $HOME/reports/slow-integration-tests-reports
+if [ "$TEST_SUITE" = "core_integration_slow" ] || [ "$TEST_SUITE" = "" ] ; then
+    ./grailsw test-app integration: x.org.modelcatalogue.**.*
+    mkdir -p $HOME/reports/slow-integration-tests-reports
+    cp -Rf target/test-reports $HOME/reports/slow-integration-tests-reports
+fi
 
+# karma tests
+if [ "$TEST_SUITE" = "core_karma" ] || [ "$TEST_SUITE" = "" ] ; then
+    ./node_modules/karma/bin/karma start --single-run --browsers Firefox
+fi
 
-./node_modules/karma/bin/karma start --single-run --browsers Firefox
+if [ "$TEST_SUITE" = "app_integration" ] || [ "$TEST_SUITE" = "app_functional" ] || [ "$TEST_SUITE" = "" ] ; then
+    cd ..
+    cd ModelCatalogueCorePluginTestApp
 
-cd ..
-cd ModelCatalogueCorePluginTestApp
+    ./grailsw clean-all
+    ./grailsw refresh-dependencies
+fi
 
-./grailsw clean-all
-./grailsw refresh-dependencies
+if [ "$TEST_SUITE" = "app_integration" ] || [ "$TEST_SUITE" = "" ] ; then
+    ./grailsw test-app integration:
+    mkdir -p $HOME/reports/test-app-integration-tests-reports
+    cp -Rf target/test-reports $HOME/reports/test-app-integration-tests-reports
+fi
 
-
-./grailsw test-app integration:
-mkdir -p $HOME/reports/test-app-integration-tests-reports
-cp -Rf target/test-reports $HOME/reports/test-app-integration-tests-reports
-
-
-./grailsw test-app functional: -war
-mkdir -p $HOME/reports/test-app-functional-tests-reports
-cp -Rf target/test-reports $HOME/reports/test-app-functional-tests-reports
+if [ "$TEST_SUITE" = "app_functional" ] || [ "$TEST_SUITE" = "" ] ; then
+    ./grailsw test-app functional: -war
+    mkdir -p $HOME/reports/test-app-functional-tests-reports
+    cp -Rf target/test-reports $HOME/reports/test-app-functional-tests-reports
+fi
 
 cd ..
