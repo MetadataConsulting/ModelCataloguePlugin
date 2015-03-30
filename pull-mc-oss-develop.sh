3132529851c4986e792a9b5435f9f33a6f4fc8e8
Vladimír Oraný <vladimir@orany.cz>
2015-03-30 13:44:30 +0200

    fetch from mc-oss instead of pull (as there are usually conflicts)

--- pull-mc-oss-develop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-git pull mc-oss develop
-echo "Changes pulled from the https://github.com/MetadataRegistry/ModelCataloguePlugin repository. You have to merge them in IDEA or SourceTree manually (branch mc-oss/develop)"
