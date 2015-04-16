49cd976e10e44bcb92b0f457ac3c958941ab2650
Vladimír Oraný <vladimir@orany.cz>
2015-04-16 09:55:01 +0200

    MET-660 basic geb spec for changelog
    display global changelogs feed
    display one change
    undo one change
    display user's activity
    display classification activity
    show history as treeview
    expand element in history treeview

---
+++ ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/ChangesSpec.groovy
@@ -0,0 +1,139 @@
+package org.modelcatalogue.core
+
+import geb.navigator.Navigator
+import org.modelcatalogue.core.pages.GlobalChangesPage
+import spock.lang.Stepwise
+
+@Stepwise
+class ChangesSpec extends AbstractModelCatalogueGebSpec {
+
+    def "go to login"() {
+        go "#/"
+        loginAdmin()
+
+        when:
+        go "#/catalogue/change/all"
+
+        then:
+        at GlobalChangesPage
+        waitFor(120) {
+            viewTitle.displayed
+        }
+        waitFor {
+            viewTitle.text().trim() == 'Change List'
+        }
+    }
+
+    def "check the unit shows up with own detail page"(){
+        when:
+        waitFor {
+            infTableCell(1, 4).find('a span.fa.fa-fw.fa-link').displayed
+        }
+        noStale({ infTableCell(1, 4).find('a span.fa.fa-fw.fa-link').parent('a') }, { Navigator link ->
+            link.click()
+        })
+        then:
+        waitFor {
+            $("li[data-tab-name='changes']").displayed
+        }
+
+        $(".pp-table-property-element-value", 'data-value-for': 'Undone').displayed
+        $(".pp-table-property-element-value", 'data-value-for': 'Undone').text() == 'false'
+
+        when:
+        noStale({ actionButton('undo-change') }) { Navigator button ->
+            button.click()
+        }
+
+        then:
+        waitFor {
+            confirmDialog.displayed
+        }
+
+        when:
+        confirmOk.click()
+
+        then:
+        waitFor {
+            $(".pp-table-property-element-value", 'data-value-for': 'Undone').text() == 'true'
+        }
+    }
+
+    def "users have activity feed"() {
+        go "#/catalogue/user/all"
+
+        expect:
+        waitFor {
+            viewTitle.text().trim() == 'User List'
+        }
+        waitFor {
+            infTableCell(1, 2).find('a', text: 'admin').displayed
+        }
+
+        when:
+        noStale({ infTableCell(1, 2).find('a', text: 'admin') }) {
+            it.click()
+        }
+
+        then:
+        waitFor {
+            subviewTitle.displayed
+        }
+        waitFor {
+            subviewTitle.text().trim() == 'admin FINALIZED'
+        }
+        waitFor {
+            $("li[data-tab-name='activity']").displayed
+        }
+        waitFor {
+            $("li[data-tab-name='history']").displayed
+        }
+
+        when:
+        selectTab 'history'
+
+        then: "the history tab contains treeview item which does not resize"
+        waitFor {
+            $('#history-tab .catalogue-element-treeview-list-container.no-resize')
+        }
+        waitFor {
+            $('span.catalogue-element-treeview-name', text: "admin 1").displayed
+        }
+        waitFor {
+            $('.glyphicon-folder-close').parent('a').displayed
+        }
+
+        when:
+        noStale({$('.glyphicon-folder-close').parent('a')}) {
+            it.click()
+        }
+
+        then:
+        waitFor {
+            $('span.catalogue-element-treeview-name', text: "admin (User) [1] created").displayed
+        }
+    }
+
+    def "classifications have activity feed"() {
+        go "#/catalogue/classification/all"
+
+        expect:
+        waitFor {
+            viewTitle.text().trim() == 'Classification List'
+        }
+
+        when:
+        noStale({ infTableCell(1, 2).find('a') }) {
+            it.click()
+        }
+
+        then:
+        waitFor {
+            subviewTitle.displayed
+        }
+        waitFor {
+            $("li[data-tab-name='activity']").displayed
+        }
+    }
+
+}
---
+++ ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/pages/GlobalChangesPage.groovy
@@ -0,0 +1,17 @@
+package org.modelcatalogue.core.pages
+
+/**
+ * Created by david on 04/11/14.
+ */
+class GlobalChangesPage extends ModelCataloguePage {
+
+    static url = "#/catalogue/change/all"
+
+    static at = {
+        url == "#/catalogue/change/all"
+    }
+
+    static content = {
+
+    }
+}
