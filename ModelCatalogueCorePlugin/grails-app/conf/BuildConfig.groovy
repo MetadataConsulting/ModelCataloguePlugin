41551e05c0dc6abbc96fcfdb3e6488a0ebf69c96
Vladimír Oraný <vladimir@orany.cz>
2015-06-30 09:01:13 +0200

    testing grails dependencies stupidity

--- ModelCatalogueCorePlugin/grails-app/conf/BuildConfig.groovy
+++ ModelCatalogueCorePlugin/grails-app/conf/BuildConfig.groovy
@@ -51,11 +51,10 @@ grails.project.dependency.resolution = {
         // runtime 'mysql:mysql-connector-java:5.1.27'
         compile 'com.google.guava:guava:18.0'
 
-        String mcToolkitVersion = '1.0.0-alpha-5'
-        compile "org.modelcatalogue:mc-core-api:$mcToolkitVersion"
-        compile "org.modelcatalogue:mc-builder-api:$mcToolkitVersion"
-        compile "org.modelcatalogue:mc-integration-xml:$mcToolkitVersion"
-        compile "org.modelcatalogue:mc-integration-obo:$mcToolkitVersion"
+        compile "org.modelcatalogue:mc-core-api:1.0.0-alpha-5"
+        compile "org.modelcatalogue:mc-builder-api:1.0.0-alpha-5"
+        compile "org.modelcatalogue:mc-integration-xml:1.0.0-alpha-5"
+        compile "org.modelcatalogue:mc-integration-obo:1.0.0-alpha-5"
 
 		//----- Jasper Reports specific dependencies 
 		 //from the bintray
