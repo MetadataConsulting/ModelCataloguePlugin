b881d4348546cba4b6d362957e322462abed9626
Vladimir Orany <vladimir@orany.cz>
2015-08-07 09:51:11 +0200

    migrate value domains

--- ModelCatalogueCorePluginTestApp/grails-app/migrations/changelog_016_dataModelAndDataClass.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/migrations/changelog_016_dataModelAndDataClass.groovy
@@ -1,7 +1,7 @@
 databaseChangeLog = {
 
     changeSet(author: "Vladimir Orany", id: "1432717633383-01") {
-        preConditions (onFail: 'MARK_RAN') {
+        preConditions(onFail: 'MARK_RAN') {
             not {
                 sqlCheck expectedResult: '0', """
                     select count(id) from relationship_type
@@ -25,7 +25,7 @@ databaseChangeLog = {
     }
 
     changeSet(author: "Vladimir Orany", id: "1432717633383-02") {
-        preConditions (onFail: 'MARK_RAN') {
+        preConditions(onFail: 'MARK_RAN') {
             not {
                 sqlCheck expectedResult: '0', """
                     select count(id) from relationship_type
@@ -40,7 +40,7 @@ databaseChangeLog = {
     }
 
     changeSet(author: "Vladimir Orany", id: "1432717633383-03") {
-        preConditions (onFail: 'MARK_RAN') {
+        preConditions(onFail: 'MARK_RAN') {
             not {
                 sqlCheck expectedResult: '0', """
                     select count(id) from relationship_type
@@ -55,7 +55,7 @@ databaseChangeLog = {
     }
 
     changeSet(author: "Vladimir Orany", id: "1432717633383-04") {
-        preConditions (onFail: 'MARK_RAN') {
+        preConditions(onFail: 'MARK_RAN') {
             not {
                 sqlCheck expectedResult: '0', """
                     select count(id) from relationship_type
@@ -70,7 +70,7 @@ databaseChangeLog = {
     }
 
     changeSet(author: "Vladimir Orany", id: "1432717633383-05") {
-        preConditions (onFail: 'MARK_RAN') {
+        preConditions(onFail: 'MARK_RAN') {
             not {
                 sqlCheck expectedResult: '0', """
                     select count(id) from relationship_type
@@ -91,8 +91,10 @@ databaseChangeLog = {
         """
     }
 
+    // without dbCreate=update
     changeSet(author: "Vladimir Orany", id: "1432717633383-07") {
-        preConditions (onFail: 'MARK_RAN') {
+        preConditions(onFail: 'MARK_RAN') {
+            tableExists tableName: "model"
             not {
                 tableExists tableName: "data_class"
             }
@@ -101,7 +103,8 @@ databaseChangeLog = {
     }
 
     changeSet(author: "Vladimir Orany", id: "1432717633383-08") {
-        preConditions (onFail: 'MARK_RAN') {
+        preConditions(onFail: 'MARK_RAN') {
+            tableExists tableName: "classification"
             not {
                 tableExists tableName: "data_model"
             }
@@ -109,10 +112,9 @@ databaseChangeLog = {
         renameTable oldTableName: 'classification', newTableName: 'data_model'
     }
 
-
-
     changeSet(author: "Vladimir Orany", id: "1432717633383-09") {
-        preConditions (onFail: 'MARK_RAN') {
+        preConditions(onFail: 'MARK_RAN') {
+            tableExists tableName: "model"
             not {
                 columnExists tableName: "relationship", columnName: "data_model_id"
             }
@@ -120,7 +122,169 @@ databaseChangeLog = {
         renameColumn tableName: "relationship", columnDataType: "BIGINT", newColumnName: "data_model_id", oldColumnName: "classification_id"
     }
 
+    // with dbCreate=update
+    changeSet(author: "Vladimir Orany", id: "1432717633383-10") {
+        preConditions(onFail: 'MARK_RAN') {
+            tableExists tableName: "model"
+            tableExists tableName: "data_class"
+        }
+        sql """
+        insert into data_class (id)
+        select id from model
+        where id not in (select id from data_class)
+        """
+    }
 
+    changeSet(author: "Vladimir Orany", id: "1432717633383-11") {
+        preConditions(onFail: 'MARK_RAN') {
+            tableExists tableName: "classification"
+            tableExists tableName: "data_model"
+        }
+        sql """
+        insert into data_model (id)
+        select id from classification where id not in (select id from data_model)
+        """
+    }
+
+
+    changeSet(author: "Vladimir Orany", id: "1432717633383-12") {
+        preConditions(onFail: 'MARK_RAN') {
+            not {
+                sqlCheck expectedResult: '0', """
+                    select count(id) from value_domain
+                """
+            }
+        }
+        grailsChange {
+            change {
+                sql.eachRow """
+                        select vd.id, vd.unit_of_measure_id, vd.rule
+                        from value_domain vd
+                        where vd.data_type_id is null
+                """, { row ->
+                    def vdid = row[0]
+                    def muid = row[1]
+                    def rule = row[2]
+                    def newDataTypeId = sql.executeInsert("""
+                        insert into catalogue_element
+                        (version, date_created, description, last_updated, latest_version_id, model_catalogue_id, name, status, version_created, version_number)
+                        select version, date_created, description, last_updated, latest_version_id, model_catalogue_id, name, status, version_created, version_number
+                        from catalogue_element
+                        where id = $vdid
+                    """)[0][0]
+                    sql.executeInsert "insert into data_type (id) values ($newDataTypeId)"
+
+                    if (muid) {
+                        sql.executeInsert "insert into primitive_type (id, measurement_unit_id) values ($newDataTypeId, $muid)"
+                    }
+                }
+
+                // migrate rules
+
+                sql.eachRow """
+                        select vd.rule, dt.id
+                        from value_domain vd
+                        join data_type dt on vd.data_type_id = dt.id
+                        join catalogue_element vdce on vdce.id = vd.id
+                        join catalogue_element dtce on dtce.id = dt.id
+                        where vd.rule is not null and vdce.name = dtce.name
+                """, { row ->
+                    def rule = row[0]
+                    def dtid = row[1]
+                    sql.executeUpdate """
+                        update data_type set rule = $rule where id = $dtid
+                    """
+                }
+
+                // migrate data elements
+
+                sql.eachRow """
+                        select de.id, dt.id
+                        from data_element de
+                        join value_domain vd on de.value_domain_id = vd.id
+                        join data_type dt on vd.data_type_id = dt.id
+                """, { row ->
+                    def deid = row[0]
+                    def dtid = row[1]
+                    sql.executeUpdate """
+                        update data_element
+                        set value_domain_id = null, data_type_id = $dtid where id = $deid
+                    """
+                }
+
+                // migrate relationships
+
+                sql.eachRow """
+                    select rel.id, dt.id, rel.destination_id
+                    from relationship rel
+                    join value_domain vd on rel.source_id = vd.id
+                    join data_type dt on vd.data_type_id = dt.id
+                    join catalogue_element vdce on vdce.id = vd.id
+                    join catalogue_element dtce on dtce.id = dt.id
+                    where dtce.name = vdce.name
+                """, { row ->
+                    def relid = row[0]
+                    def dtid = row[1]
+                    def destid = row[2]
+
+                    def destDataType = sql.firstRow """
+                        select data_type_id from value_domain where id = $destid
+                    """
+
+                    if (destDataType && destDataType[0]) {
+                        println  "dest data type: $destDataType"
+                        destid = destDataType[0]
+                    }
+
+
+                    try {
+                        sql.executeUpdate """
+                            update relationship
+                            set source_id = $dtid,
+                            destination_id = $destid
+                            where id = $relid
+                        """
+                    } catch (ignored) {
+                        // duplicate entry - ignored
+                    }
+                }
+
+                sql.eachRow """
+                    select rel.id, dt.id, rel.source_id
+                    from relationship rel
+                    join value_domain vd on rel.destination_id = vd.id
+                    join data_type dt on vd.data_type_id = dt.id
+                    join catalogue_element vdce on vdce.id = vd.id
+                    join catalogue_element dtce on dtce.id = dt.id
+                    where dtce.name = vdce.name
+                """, { row ->
+                    def relid = row[0]
+                    def dtid = row[1]
+                    def sourceid = row[2]
+
+                    def sourceDataType = sql.firstRow """
+                        select data_type_id from value_domain where id = $sourceid
+                    """
+
+                    if (sourceDataType && sourceDataType[0]) {
+                        sourceid = sourceDataType[0]
+                    }
+
+
+                    try {
+                        sql.executeUpdate """
+                            update relationship
+                            set destination_id = $dtid,
+                            source_id = $sourceid
+                            where id = $relid
+                        """
+                    } catch (ignored) {
+                        // duplicate entry - it's fine
+                    }
+                }
+            }
+        }
+    }
 
 
 }
--- migrate-db.sh
+++ migrate-db.sh
@@ -6,8 +6,7 @@ set -e
 cd ModelCatalogueCorePluginTestApp
 
 if [[ "$1" ]]; then
-    ./grailsw prod dbm-update -Dmc.config.location="$1"
+    ./grailsw prod dbm-update --non-interactive -Dmc.config.location="$1"
 else
-    ./grailsw prod dbm-update
+    ./grailsw prod dbm-update --non-interactive
 fi
-
