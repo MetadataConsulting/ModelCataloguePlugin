f70641ff8dcdeacb4c8c5e0e5a6ba231b5143f4c
Vladimír Oraný <vladimir@orany.cz>
2015-10-30 14:41:10 +0100

    improved clean.sh

--- clean.sh
+++ clean.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
+
 rm -rf build
 find . -name '*.gen.fixture.js' -delete
 
 for file in */ .*/ ; do
-    if [ -f $file/grailsw ]; then
+    if [ -f "$file/grailsw" ]; then
         echo "found grails wrapper in $file"
         cd "$file"
-        ./grailsw clean-all
+
+        PROP_VALUE=`cat "application.properties" | grep "app.grails.version" | cut -d'=' -f2`
+
+        echo "$PROP_VALUE"
+
+        ./grailsw clean-all --stacktrace
         rm -rf target
         rm -rf target-eclipse
-        ./grailsw refresh-dependencies --non-interactive
+
+        GRAILS_PROJECT_FOLDER="~/.grails/$PROP_VALUE/projects/${PWD##*/}"
+
+        rm -rf "$GRAILS_PROJECT_FOLDER"
+
+        # echo -e "\nDeleted $GRAILS_PROJECT_FOLDER\n"
+
+        ./grailsw refresh-dependencies --non-interactive --stacktrace
 	cd ..
     fi
 done
