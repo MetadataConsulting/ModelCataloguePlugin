b7957c9ea9275f250d16095653cea448b54df936
Vladimír Oraný <vladimir@orany.cz>
2015-11-10 09:53:55 +0100

    removed duplicate mappings class

--- ModelCatalogueElasticSearchPlugin/grails-app/conf/ModelCatalogueFormsUrlMappings.groovy
+++
@@ -1,9 +0,0 @@
-import org.springframework.http.HttpMethod
-
-class ModelCatalogueFormsUrlMappings {
-
-    static mappings = {
-        "/api/modelCatalogue/core/forms/generate/$id" (controller: 'formGenerator', action: 'generateForm', method: HttpMethod.GET)
-    }
-
-}
