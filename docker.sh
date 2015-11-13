098c512da2dcc5231c74d47ecfc6defd98509f6c
Vladimír Oraný <vladimir@orany.cz>
2015-11-13 15:48:38 +0100

    added dockerized mysql

---
+++ docker.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# you can have multiple docker machines on your computer, default should present after installation
+DOCKER_MACHINE_ENV="default"
+
+command -v docker-machine >/dev/null 2>&1 || { echo "Docker Machine is required to run this script.  Please, install it from https://www.docker.com/docker-toolbox." >&2; exit 1; }
+
+docker-machine start "$DOCKER_MACHINE_ENV" &>/dev/null
+
+eval "$(docker-machine env "$DOCKER_MACHINE_ENV")"
+
+DOCKER_MACHINE_IP=$(docker-machine ip default)
+
+export DOCKER_TLS_VERIFY
+export DOCKER_HOST
+export DOCKER_CERT_PATH
+export DOCKER_MACHINE_NAME
+
+function docker_exec() {
+    local NAME=$1
+    shift
+    docker exec -it "$NAME" "$@"  &>/dev/null
+}
+
+function docker_restart() {
+    docker stop "$1" && mute docker start "$1" &>/dev/null
+}
+
+function docker_stop() {
+    docker stop "$1" &>/dev/null
+}
+
+function docker_rm() {
+    docker rm -f "$1" &>/dev/null
+}
+
+function docker_start_or_run() {
+    local NAME=$1
+    shift
+    docker start "$NAME"  &>/dev/null || docker run -d --name="$NAME" "$@" &>/dev/null
+}
+
+function write_docker_file() {
+    echo "$DOCKER_MACHINE_IP" > .docker-ip
+}
+
+function remove_docker_file() {
+    rm .docker-ip &>/dev/null
+}
--- elasticsearch.sh
+++ elasticsearch.sh
@@ -1,43 +1,34 @@
 #!/usr/bin/env bash
 
+source docker.sh
+
 if [[ -z "$1" ]]; then
     echo "Usage: elasticsearch.sh <stop|start|rm>"
     exit 1
 fi
 
 ES_VERSION="2.0.0"
-ES_PORT=9200
-DOCKER_MACHINE_ENV="default"
-
-
-command -v docker-machine >/dev/null 2>&1 || { echo "Docker Machine is required to run this script.  Please, install it from https://www.docker.com/docker-toolbox." >&2; exit 1; }
-
-docker-machine start "$DOCKER_MACHINE_ENV" &>/dev/null
-
-eval "$(docker-machine env default)"
-
-DOCKER_MACHINE_IP=$(docker-machine ip default)
+ES_PORT="9200"
 
 if [ "$1" = "start" ] ; then
-    echo -e "\nStarting ElasticSearch v$ES_VERSION instance with docker.\nIf you have upgraded the version of your client, you should upgrade the version in this script as well.\nInitializing containers may take a while, please be patient."
-    docker start mc-es &>/dev/null || docker run -d --name=mc-es -p 9200:9200 -p 9300:9300 elasticsearch:"$ES_VERSION" -Dnetwork.host=0.0.0.0 &>/dev/null
-    docker start mc-kibana &>/dev/null || docker run --name=mc-kibana -e "ELASTICSEARCH_URL=http://$DOCKER_MACHINE_IP:9200" -p 5601:5601 -d kibana &>/dev/null && docker exec -it mc-kibana kibana plugin --install elastic/sense &>/dev/null && docker stop mc-kibana &>/dev/null && docker start mc-kibana &>/dev/null
-    echo
+    echo -e "Starting ElasticSearch v$ES_VERSION instance with docker.\nIf you have upgraded the version of your client, you should upgrade the version in this script as well.\nInitializing containers may take a while, please be patient."
+    docker_start_or_run mc-es -p 9200:9200 -p 9300:9300 elasticsearch:"$ES_VERSION" -Dnetwork.host=0.0.0.0
+    docker_start_or_run mc-kibana -e "ELASTICSEARCH_URL=http://$DOCKER_MACHINE_IP:9200" -p 5601:5601 kibana
+    docker_exec mc-kibana kibana plugin --install elastic/sense
+    docker_restart mc-kibana
     echo -e "ElasticSearch v$ES_VERSION started at http://$DOCKER_MACHINE_IP:9200.\nUse $DOCKER_MACHINE_IP:9300 to connect to with the Java client.\nYou can also use Kibana dashboard at http://$DOCKER_MACHINE_IP:5601.\nDocker IP $DOCKER_MACHINE_IP stored in '.docker-ip' file for reference from the scripts.\n"
-    echo "$DOCKER_MACHINE_IP" > .docker-ip
+    write_docker_file
 elif [ "$1" = "stop" ] ; then
-    echo -e "\nStopping ElasticSearch v$ES_VERSION instance with docker.\nIf you have upgraded the version of your client, you should upgrade the version is this script as well.\n"
-    docker stop mc-es &>/dev/null
-    docker stop mc-kibana &>/dev/null
-    echo
-    echo -e "\nElasticSearch v$ES_VERSION is no longer available at http://$DOCKER_MACHINE_IP:9200"
-    rm .docker-ip &>/dev/null
+    echo -e "Stopping ElasticSearch v$ES_VERSION instance with docker.\nIf you have upgraded the version of your client, you should upgrade the version is this script as well.\n"
+    docker_stop mc-es
+    docker_stop mc-kibana
+    echo -e "ElasticSearch v$ES_VERSION is no longer available at http://$DOCKER_MACHINE_IP:9200"
+    remove_docker_file
 elif [ "$1" = "rm" ] ; then
-    echo -e "\nRemoving containers for ElasticSearch v$ES_VERSION instance with docker."
-    docker rm -f mc-es &>/dev/null
-    docker rm -f mc-kibana &>/dev/null
-    echo
-    echo -e "\nElasticSearch v$ES_VERSION is no longer available at http://$DOCKER_MACHINE_IP:9200.\nContainers were removed as well.\n"
-    rm .docker-ip&>/dev/null
+    echo -e "Removing containers for ElasticSearch v$ES_VERSION instance with docker."
+    docker_rm mc-es
+    docker_rm mc-kibana
+    echo -e "ElasticSearch v$ES_VERSION is no longer available at http://$DOCKER_MACHINE_IP:9200.\nContainers were removed as well.\n"
+    remove_docker_file
 fi
 
