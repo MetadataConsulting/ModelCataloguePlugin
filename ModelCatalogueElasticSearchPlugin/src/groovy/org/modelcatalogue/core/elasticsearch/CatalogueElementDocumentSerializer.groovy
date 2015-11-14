31e7b24bd468756432870796571a9939c330b64b
Vladimír Oraný <vladimir@orany.cz>
2015-11-14 09:31:32 +0100

    added link and element type to elastic search document

--- ModelCatalogueElasticSearchPlugin/src/groovy/org/modelcatalogue/core/elasticsearch/CatalogueElementDocumentSerializer.groovy
+++ ModelCatalogueElasticSearchPlugin/src/groovy/org/modelcatalogue/core/elasticsearch/CatalogueElementDocumentSerializer.groovy
@@ -8,11 +8,14 @@ import org.modelcatalogue.core.Relationship
 class CatalogueElementDocumentSerializer implements DocumentSerializer<CatalogueElement> {
 
     Map getDocument(CatalogueElement element) {
+        Class<? extends CatalogueElement> clazz = HibernateProxyHelper.getClassWithoutInitializingProxy(element)
         [
                 name: element.name,
                 name_not_analyzed: element.name,
+                link: "/${GrailsNameUtils.getPropertyName(clazz)}/${element.getId()}",
                 description: element.description,
-                type_human_readable: GrailsNameUtils.getNaturalName(HibernateProxyHelper.getClassWithoutInitializingProxy(element).simpleName),
+                fully_qualified_type: clazz.name,
+                type_human_readable: GrailsNameUtils.getNaturalName(clazz.simpleName),
                 data_model: element.dataModels ? element.dataModels.first().name : null,
                 full_version: element.combinedVersion,
                 latest_id: (element.latestVersionId ?: element.getId()).toString(),
