96217eb3e7dd873c69fd1f55ffa193f2ad621ede
Vladimír Oraný <vladimir@orany.cz>
2016-01-15 14:58:50 +0100

    better reports for parallel builds

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -5,11 +5,11 @@ if [ "$TEST_SUITE" = "app_functional" ] || [ "$TEST_SUITE" = "app_functional_a" 
 
   #copy data we're interested in to other place
   mkdir -p $HOME/reports/last-tests-reports
-  cp -Rf ModelCatalogueCorePlugin/target/test-reports $HOME/reports/last-tests-reports
+  cp -Rf ModelCatalogueCorePlugin/target/test-reports "$HOME/$TEST_SUITE/reports/last-tests-reports"
   mkdir -p $HOME/reports/test-app-last-tests-reports
-  cp -Rf ModelCatalogueCorePluginTestApp/target/test-reports $HOME/reports/test-app-last-tests-reports
+  cp -Rf ModelCatalogueCorePluginTestApp/target/test-reports "$HOME/$TEST_SUITE/reports/test-app-last-tests-reports"
   mkdir -p $HOME/reports/test-app-functional-geb-reports
-  cp -Rf ModelCatalogueCorePluginTestApp/target/geb-reports $HOME/reports/test-app-functional-geb-reports
+  cp -Rf ModelCatalogueCorePluginTestApp/target/geb-reports "$HOME/$TEST_SUITE/reports/test-app-functional-geb-reports"
 
   #go to home and setup git
   cd $HOME
@@ -17,14 +17,15 @@ if [ "$TEST_SUITE" = "app_functional" ] || [ "$TEST_SUITE" = "app_functional_a" 
   git config --global user.name "Travis"
 
   #using token clone gh-pages branch
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/MetadataRegistry/ModelCataloguePluginReports.git gh-pages > /dev/null
+  git clone --quiet --depth=1 --branch=gh-pages https://${GH_TOKEN}@github.com/MetadataRegistry/ModelCataloguePluginReports.git gh-pages > /dev/null
 
   #go into directory and copy data we're interested in to that directory
   cd gh-pages
-  # cannot remove everything as the tests are running in parallel
-  # rm -rf ./reports/latest/
+
+  rm -rf ./reports/latest/
+  rm -rf "./$TEST_SUITE/reports/latest/"
   mkdir -p ./reports/latest/
-  cp -Rf $HOME/reports/* ./reports/latest/
+  cp -Rf "$HOME/$TEST_SUITE/reports/* ./$TEST_SUITE/reports/latest/"
 
   #add, commit and push files
   git add -A .
