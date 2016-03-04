8f79820fbe804d392eafb5aae532c8273215be14
Vladimír Oraný <vladimir@orany.cz>
2016-03-04 08:07:24 +0100

    updated geb specs after changing the format of titles for the lists (e.g. Data Classes instead of Data Class List)

--- ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/geb/AbstractModelCatalogueGebSpec.groovy
+++ ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/geb/AbstractModelCatalogueGebSpec.groovy
@@ -63,6 +63,8 @@ abstract class AbstractModelCatalogueGebSpec extends GebReportingSpec {
 
         $("#loginForm").find("button.btn-primary").click()
 
+        go "#/dataModels"
+
         waitFor {
             $("#role_navigation-right_user-menu-menu-item-link").displayed
         }
