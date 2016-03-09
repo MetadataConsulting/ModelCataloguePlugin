92134306a198f7acbaf4ee761d7c4d9f794e947a
Dexter Awoyemi <dexter.awoyemi@genomicsengland.co.uk>
2016-03-09 10:26:07 +0000

    Added missing metadata class

---
+++ ModelCatalogueCorePlugin/src/groovy/org/modelcatalogue/core/util/Metadata.groovy
@@ -0,0 +1,13 @@
+package org.modelcatalogue.core.util
+
+/**
+ * Created by dexterawoyemi on 03/03/2016.
+ */
+class Metadata {
+
+    public static final def ORGANISATION = "http://www.modelcatalogue.org/metadata/#organization"
+    public static final def OWNER = "http://www.modelcatalogue.org/metadata/#owner"
+    public static final def AUTHORS = "http://www.modelcatalogue.org/metadata/#authors"
+    public static final def REVIEWERS = "http://www.modelcatalogue.org/metadata/#reviewers"
+
+}
