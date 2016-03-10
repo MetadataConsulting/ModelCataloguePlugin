634d20520abf2257bd4609c3470495fb4554f76b
Vladimír Oraný <vladimir@orany.cz>
2016-03-10 17:36:16 +0100

    simplified GenomicsController
    please, see https://github.com/MetadataRegistry/ModelCataloguePlugin/blob/2.x/docs/development/executor_service.md for how to use executor service in error proof way

--- ModelCatalogueGenomicsPlugin/grails-app/controllers/org/modelcatalogue/core/genomics/GenomicsController.groovy
+++ ModelCatalogueGenomicsPlugin/grails-app/controllers/org/modelcatalogue/core/genomics/GenomicsController.groovy
@@ -11,58 +11,26 @@ import org.springframework.http.HttpStatus
  * Controller for GEL specific reports.
  */
 @Log4j
-class GenomicsController extends AbstractCatalogueElementController<DataClass> {
+class GenomicsController {
 
     def gelJsonService
-    def executorService
-    def auditService
-    def springSecurity2SecurityService
-
-
-    GenomicsController() {
-        super(DataClass, false)
-    }
+    def assetService
 
     def exportRareDiseaseHPOAndClinicalTests() {
         DataClass model = DataClass.get(params.id)
 
+        Long classId = model.getId()
 
-        def asset = new Asset(
+        Long assetId = assetService.storeReportAsAsset(model.dataModel,
             name: "${model.name} report as Json",
             originalFileName: "${model.name}-${model.status}-${model.version}.json",
-            description:"Asset Pending",
-            status: ElementStatus.PENDING,
             contentType: "application/json",
-            size: 0).save(flush:true )
-
-        int id = springSecurity2SecurityService.currentUser.id
-        executorService.submit() {
-            try {
-                auditService.withDefaultAuthorId(id)
-                    {
-                         assetService.storeAssetFromInputStream(
-                            new ByteArrayInputStream(gelJsonService.printDiseaseOntology(model).bytes),
-                            "application/json", asset
-                        )
-
-                        asset.status = ElementStatus.FINALIZED
-                        asset.description = "Your Json is ready to download"
-                        asset.save(flush:true)
-                    }
-            } catch (e) {
-                log.error("Exception of type ${e.class} with id=${id} ", e)
-
-                asset.refresh()
-                asset.status = ElementStatus.FINALIZED
-                asset.name = asset.name + " - Error during Json generation"
-                asset.description = "Error generating Json"
-                asset.save(flush:true)
-            }
-
+        ) {
+            it << gelJsonService.printDiseaseOntology(DataClass.get(classId))
         }
 
-        response.setHeader("X-Asset-ID",asset.id.toString())
-        redirect controller: 'asset', id: asset.id, action: 'show'
+        response.setHeader("X-Asset-ID",assetId.toString())
+        redirect controller: 'asset', id: assetId, action: 'show'
     }
 
 
