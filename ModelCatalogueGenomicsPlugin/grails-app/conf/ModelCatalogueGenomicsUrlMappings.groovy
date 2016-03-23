5d6f0989416eaf0e24a789cc9c397877fe5e4550
Rick Rees <rick.rees@genomicsengland.co.uk>
2016-03-23 16:26:43 +0000

    ODA-838 & ODA-839 Rare Diseases word doc generation

--- ModelCatalogueGenomicsPlugin/grails-app/conf/ModelCatalogueGenomicsUrlMappings.groovy
+++ ModelCatalogueGenomicsPlugin/grails-app/conf/ModelCatalogueGenomicsUrlMappings.groovy
@@ -5,6 +5,8 @@ class ModelCatalogueGenomicsUrlMappings {
     static mappings = {
         "/api/modelCatalogue/core/genomics/exportGelSpecification/$id" (controller: 'genomics', action: 'exportGelSpecification', method: HttpMethod.GET)
         "/api/modelCatalogue/core/genomics/exportRareDiseaseHPOAndClinicalTests/$id" (controller: 'genomics', action: 'exportRareDiseaseHPOAndClinicalTests', method: HttpMethod.GET)
+        "/api/modelCatalogue/core/genomics/exportRareDiseaseEligibilityDoc/$id"(controller: 'genomics', action: 'exportRareDiseaseEligibilityDoc', method: HttpMethod.GET)
+        "/api/modelCatalogue/core/genomics/exportRareDiseasePhenotypesAndClinicalTestsDoc/$id"(controller: 'genomics', action: 'exportRareDiseasePhenotypesAndClinicalTestsDoc', method: HttpMethod.GET)
     }
 
 }
