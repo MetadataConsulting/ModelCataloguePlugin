b898dd4c6b667236b1ec9a4a2ecc203dff41edec
Rick Rees <rick.rees@genomicsengland.co.uk>
2016-04-29 16:23:38 +0100

    ODA-847 Cancer Types/Presentations/Subtypes CSV - enums fix

--- ModelCatalogueGenomicsPlugin/test/integration/org/modelcatalogue/gel/export/AbstractCancerTypesExporterSpec.groovy
+++ ModelCatalogueGenomicsPlugin/test/integration/org/modelcatalogue/gel/export/AbstractCancerTypesExporterSpec.groovy
@@ -4,6 +4,7 @@ import grails.test.spock.IntegrationSpec
 import org.modelcatalogue.core.DataClass
 import org.modelcatalogue.core.DataElement
 import org.modelcatalogue.core.DataModelService
+import org.modelcatalogue.core.DataType
 import org.modelcatalogue.core.ElementService
 import org.modelcatalogue.core.InitCatalogueService
 import org.modelcatalogue.core.util.builder.DefaultCatalogueBuilder
@@ -16,20 +17,24 @@ class AbstractCancerTypesExporterSpec extends IntegrationSpec{
     public static final String ROOT_CANCER_TYPE = 'cancer types'
     public static final String ADULT_GLIOMA_TYPE1 = 'Adult Glioma'
     public static final String ADULT_GLIOMA_SUBTYPE_1 = 'Adult Glioma subtypes 1.1'
+    public static final String ADULT_GLIOMA_ENUM = 'Adult Glioma Enum'
     public static final String ADULT_GLIOMA_PRESENTATION_1 = 'Adult Glioma presentations 1'
     public static final String SOME_CANCER_TYPE2 = 'Some other Cancer Type 2'
     public static final String SOME_CANCER_SUBTYPE_21 = 'some other cancer subtypes 2.1'
     public static final String SOME_CANCER_SUBTYPE_22 = 'some other cancer subtypes 2.2'
+    public static final String SOME_CANCER_ENUM = 'Some Cancer Enum'
     public static final String SOME_CANCER_PRESENTATION_21 = 'Cancer presentations 21'
     public static final String SOME_CANCER_PRESENTATION_22 = 'Cancer presentations 22'
     protected Long cancer_type_1_adult_glioma_id
     protected Long cancer_type_2_some_cancer_id
     protected String cancer_type_1_adult_glioma_combined_id
     protected String cancer_type_1_adult_subType_combined_id
+    protected String cancer_type_1_adult_glioma_enum_combined_id
     protected String cancer_type_1_adult_presentation_combined_id
     protected String cancer_type_2_some_cancer_combined_id
     protected String cancer_type_2_some_cancer_subType21_combined_id
     protected String cancer_type_2_some_cancer_subType22_combined_id
+    protected String cancer_type_2_some_cancer_enum_combined_id
     protected String cancer_type_2_some_cancer_presentation2_combined_id
     protected String cancer_type_2_some_cancer_presentation22_combined_id
     ElementService elementService
@@ -48,7 +53,7 @@ class AbstractCancerTypesExporterSpec extends IntegrationSpec{
                     dataClass(name: ADULT_GLIOMA_TYPE1) {
                         dataElement name: ADULT_GLIOMA_SUBTYPE_1, {
                             description "adult glioma description"
-                            dataType name: "Data Element11", enumerations: [    //shouldn't display this level
+                            dataType name: ADULT_GLIOMA_ENUM, enumerations: [    //shouldn't display this level
                                 'one': '1',
                                 'two': '2',
                             ]
@@ -67,7 +72,10 @@ class AbstractCancerTypesExporterSpec extends IntegrationSpec{
                         }
                         dataElement(name: SOME_CANCER_SUBTYPE_22) {
                             description "some cancer description subtype22"
-                            dataType name: "Data Element22"
+                            dataType name: SOME_CANCER_ENUM, enumerations: [
+                                'first': 'a',
+                                'second': 'b',
+                            ]
                         }
                         dataElement(name: SOME_CANCER_PRESENTATION_21) {
                             description "some cancer presentation description21"
@@ -98,6 +106,10 @@ class AbstractCancerTypesExporterSpec extends IntegrationSpec{
         cancer_type_2_some_cancer_subType22_combined_id = DataElement.findByName(SOME_CANCER_SUBTYPE_22).getCombinedVersion()
         cancer_type_2_some_cancer_presentation2_combined_id = DataElement.findByName(SOME_CANCER_PRESENTATION_21).getCombinedVersion()
         cancer_type_2_some_cancer_presentation22_combined_id = DataElement.findByName(SOME_CANCER_PRESENTATION_22).getCombinedVersion()
+
+        cancer_type_1_adult_glioma_enum_combined_id = DataType.findByName(ADULT_GLIOMA_ENUM).getCombinedVersion()
+        cancer_type_2_some_cancer_enum_combined_id = DataType.findByName(SOME_CANCER_ENUM).getCombinedVersion()
+
     }
 
     protected void printOutput(String response) {
