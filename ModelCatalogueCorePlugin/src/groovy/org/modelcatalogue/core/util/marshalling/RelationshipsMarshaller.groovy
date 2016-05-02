65815de3c0ea4f7e6002c157e73541c2c0ac9dac
Vladimír Oraný <vladimir@orany.cz>
2016-05-02 15:23:24 +0200

    MET-1069 not returning missing relationships from relationships marshaller

--- ModelCatalogueCorePlugin/src/groovy/org/modelcatalogue/core/util/marshalling/RelationshipsMarshaller.groovy
+++ ModelCatalogueCorePlugin/src/groovy/org/modelcatalogue/core/util/marshalling/RelationshipsMarshaller.groovy
@@ -15,12 +15,17 @@ class RelationshipsMarshaller extends ListWrapperMarshaller {
         def ret = super.prepareJsonMap(relationsList)
         ret.type = relationsList.type
         ret.direction = relationsList.direction.actionName
-        ret.list = relationsList.items.collect {
+        ret.list = relationsList.items.grep().collect {
             [id: it.id, type: it.relationshipType, ext: OrderedMap.toJsonMap(it.ext), element: CatalogueElementMarshaller.minimalCatalogueElementJSON(relationsList.direction.getElement(relationsList.owner, it)),  relation: relationsList.direction.getRelation(relationsList.owner, it), direction: relationsList.direction.getDirection(relationsList.owner, it), removeLink: getDeleteLink(relationsList.owner, it), archived: it.archived, inherited: it.inherited, elementType: Relationship.name, classification: CatalogueElementMarshaller.minimalCatalogueElementJSON(it.dataModel)]
         }
+        ret.total -= relationsList.items.size() - relationsList.items.grep().size()
         ret
     }
 
+
+    protected getSize(Object elements) {
+        elements.items.grep().size()
+    }
 
     static getDeleteLink(theOwner, Relationship rel) {
         "${theOwner.info.link}/${theOwner == rel.source ? 'outgoing' : 'incoming'}/${rel.relationshipType.name}"
