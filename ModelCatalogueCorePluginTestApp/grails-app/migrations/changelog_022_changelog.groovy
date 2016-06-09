1769049536937e31b3f9d7a7740b57b326b374d6
Vladimír Oraný <vladimir@orany.cz>
2016-06-09 17:04:17 +0200

    added one more crutial index to run smootly even with huge changelog

--- ModelCatalogueCorePluginTestApp/grails-app/migrations/changelog_022_changelog.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/migrations/changelog_022_changelog.groovy
@@ -1,6 +1,11 @@
 databaseChangeLog = {
 
-    changeSet(author: "Rick Rees", id: "20160511112830") {
+    changeSet(author: "Rick Rees", id: "20160511112830-1") {
+        preConditions(onFail: 'MARK_RAN') {
+            not {
+                indexExists(tableName: 'change', indexName: 'change_idx')
+            }
+        }
 
         createIndex(indexName: "change_idx", tableName: "change") {
             column(name: "changed_id")
@@ -9,6 +14,23 @@ databaseChangeLog = {
             column(name: "parent_id")
             column(name: "type")
             column(name: "date_created")
+        }
+
+    }
+
+
+    changeSet(author: "Vladimir Orany", id: "20160511112830-2") {
+        preConditions (onFail: 'MARK_RAN') {
+            not {
+                indexExists(tableName: 'change', indexName: 'change_idx_2')
+            }
+        }
+
+        createIndex(indexName: "change_idx_2", tableName: "change") {
+            column(name: "parent_id")
+            column(name: "system")
+            column(name: "other_side")
+            column(name: "changed_id")
         }
     }
 
