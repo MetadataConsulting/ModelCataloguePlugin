dfc3b175213477f7ea8080c1fa8a978051df486a
Vladimír Oraný <vladimir@orany.cz>
2016-06-11 07:28:54 +0200

    udpdated tests

--- ModelCatalogueCorePlugin/test/integration/org/modelcatalogue/core/MeasurementUnitSpec.groovy
+++ ModelCatalogueCorePlugin/test/integration/org/modelcatalogue/core/MeasurementUnitSpec.groovy
@@ -27,7 +27,7 @@ class MeasurementUnitSpec extends IntegrationSpec {
 
         validates | size | args
         false     | 0    | [name: "x" * 256, description: "this is the the result description"]
-        false     | 0    | [name: "x", description: "x" * 2001]
+        false     | 0    | [name: "x", description: "x" * 20001]
         true      | 1    | [name: "MPH", description: "Miles per Hour",]
         true      | 1    | [name: "Miles per Hour", description: "Number of miles the object achieves within one hour moving the current speed.", symbol: "MPH"]
         false     | 0    | [name: "Miles per Hour", description: "Number of miles the object achieves within one hour moving the current speed.", symbol: "x" * 101]
