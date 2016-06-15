1bd5c6586ea426d6de42f7c2f0af5ef65e03ce20
Vladimir Orany <vladimir@orany.cz>
2016-06-15 17:04:54 +0200

    Update truncateChanges.groovy

--- ModelCatalogueCorePluginTestApp/console-scripts/truncateChanges.groovy
+++ ModelCatalogueCorePluginTestApp/console-scripts/truncateChanges.groovy
@@ -10,7 +10,7 @@ SessionFactory sessionFactory = ctx.getBean(SessionFactory)
 
 @Field JsonSlurper slurper = new JsonSlurper()
 @Field List<String> keysToRemove = ImmutableList.of('dateCreated', 'versionCreated', 'lastUpdated', 'classification', 'archived', 'ext', 'description', 'enumerations', 'internalModelCatalogueId', 'modelCatalogueId')
-@Field List<String> nestedToClean = ImmutableList.of('source', 'destination', 'dataModel', 'dataType', 'measurementUnit', 'unitOfMeasure', 'relationship', 'element')
+@Field List<String> nestedToClean = ImmutableList.of('source', 'destination', 'dataModel', 'dataType', 'measurementUnit', 'unitOfMeasure')
 
 BuildProgressMonitor monitor = BuildProgressMonitor.create('Truncate changes', 123)
 
@@ -21,7 +21,7 @@ BuildProgressMonitor monitor = BuildProgressMonitor.create('Truncate changes', 1
 
         Map<Long, List<String>> values = [:].withDefault { [] }
 
-        sql.eachRow("SELECT id, old_value, new_value FROM `change` limit 1000 offset ${count * pageSize}") { row ->
+        sql.eachRow("SELECT id, old_value, new_value FROM `change` limit 1000 offset ${count * 1000}") { row ->
             trimValues(row, values)
         }
 
