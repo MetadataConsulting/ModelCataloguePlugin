06de2939a5e2e0e150af15ee3b38dcd1e70169f0
Vladimír Oraný <vladimir@orany.cz>
2016-06-22 12:20:03 +0200

    MET-1001 better reporting, updated test suite for deploy

--- bin/test.sh
+++ bin/test.sh
@@ -21,15 +21,17 @@ if [ "$TEST_SUITE" = "unit_and_integration" ] || [ "$TEST_SUITE" = "" ] ; then
     # plugin unit tests
     ./grailsw test-app unit: --non-interactive
     mkdir -p "$HOME/reports/core-unit-tests-reports"
-    cp -Rf target/test-reports/ "$HOME/reports/core-unit-tests-reports"
+    cp -Rf target/test-reports/ "$HOME/reports/core-unit-tests-reports" || true
 
     # plugin integration all tests
     ./grailsw test-app integration: --non-interactive
     mkdir -p "$HOME/reports/core-integration-tests-reports"
-    cp -Rf target/test-reports/ "$HOME/reports/core-integration-tests-reports"
+    cp -Rf target/test-reports/ "$HOME/reports/core-integration-tests-reports" || true
 
     # karma tests, part of the integration as they needs the fixtures generated from the integration tests
     ./node_modules/karma/bin/karma start --single-run --browsers Firefox
+    mkdir -p "$HOME/reports/karma-tests-reports"
+    cp -Rf target/test-reports/ "$HOME/reports/karma-tests-reports" || true
 fi
 
 cd ..
@@ -45,10 +47,14 @@ fi
 if [ "$TEST_SUITE" = "unit_and_integration" ] || [ "$TEST_SUITE" = "" ] ; then
     export MC_ES_DISABLED=true
     ./grailsw test-app integration: --non-interactive
+    mkdir -p "$HOME/reports/app-integration-tests-reports"
+    cp -Rf target/test-reports/ "$HOME/reports/app-integration-tests-reports" || true
 fi
 
 if [ "$TEST_SUITE" = "functional" ] || [ "$TEST_SUITE" = "" ] ; then
     ./grailsw test-app functional: -war --non-interactive
+    ./grailsw test-app integration: --non-interactive
+    copy_functional_test_results
 fi
 
 cd ..
