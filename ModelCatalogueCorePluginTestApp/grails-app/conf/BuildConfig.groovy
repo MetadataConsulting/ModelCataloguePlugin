493ff6faadb5d01b3068b9d482c9b75d72ad3da5
Vladimír Oraný <vladimir@orany.cz>
2016-07-05 18:37:27 +0200

    more memory for tests

--- ModelCatalogueCorePluginTestApp/grails-app/conf/BuildConfig.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/conf/BuildConfig.groovy
@@ -12,9 +12,8 @@ grails.project.fork = [
         //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
 
         // configure settings for the test-app JVM, uses the daemon by default
-        test   : false,
+        test   : [maxMemory: 2042, minMemory: 1024, debug: false, maxPerm: 512, forkReserve:false],
         // configure settings for the run-app JVM
-//        run    : false, //[maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
         run    : [maxMemory: 1536, minMemory: 128, debug: false, maxPerm: 512, forkReserve:false],
         // configure settings for the run-war JVM
         war    : [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve: false],
