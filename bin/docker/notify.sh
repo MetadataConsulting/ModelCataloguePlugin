c03a769c93064076c2f5b736318bb5ba382ca1af
Vladimír Oraný <vladimir@orany.cz>
2016-07-14 12:12:05 +0200

    manually notifying the docker hub
    after it has been broken by renaming the GitHub org
    
    [ci skip]

--- bin/docker/notify.sh
+++ bin/docker/notify.sh
@@ -5,7 +5,7 @@ cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 
-git clone --quiet --branch=2.x https://${GH_TOKEN}@github.com/MetadataRegistry/registry.git registry > /dev/null
+git clone --quiet --branch=2.x https://${GH_TOKEN}@github.com/MetadataConsulting/registry.git registry > /dev/null
 
 cd registry
 
@@ -14,9 +14,12 @@ rm Dockerfile
 mv TemporaryDockerfile Dockerfile
 
 git add -A .
-git commit -m "Travis MetadataRegistry/ModelCataloguePlugin build $TRAVIS_BUILD_NUMBER pushed MetadataRegistry/registry"
+git commit -m "Travis MetadataConsulting/ModelCataloguePlugin build $TRAVIS_BUILD_NUMBER pushed MetadataConsulting/registry"
 git tag "$TRAVIS_TAG"
 git push -fq origin 2.x > /dev/null
 git push -fq origin "$TRAVIS_TAG" > /dev/null
+
+curl -H "Content-Type: application/json" --data '{"source_type": "Branch", "source_name": "2.x"}' -X POST "https://registry.hub.docker.com/u/metadata/registry/trigger/$DOCKER_HUB_TRIGGER_TOKEN/"
+curl -H "Content-Type: application/json" --data '{"source_type": "Tag", "source_name": "'"$TRAVIS_TAG"'"}' -X POST "https://registry.hub.docker.com/u/metadata/registry/trigger/$DOCKER_HUB_TRIGGER_TOKEN/"
 
 echo "Metadata Registry notified of successful build"
