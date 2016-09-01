d8bd43dc8012f477f2add02fd9278d6cdeb7d958
Vladimír Oraný <vladimir@orany.cz>
2016-09-01 15:07:42 +0200

    admin password not expired on production when demo is enabled

--- ModelCatalogueCorePluginTestApp/grails-app/conf/BootStrap.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/conf/BootStrap.groovy
@@ -59,7 +59,7 @@ class BootStrap {
 
         if (!production || System.getenv("METADATA_DEMO")) {
             def supervisor = User.findByNameOrUsername('supervisor', 'supervisor') ?: new User(name: 'supervisor', username: 'supervisor', enabled: true, password: System.getenv('MC_SUPERVISOR_PASSWORD') ?: 'supervisor', email: System.getenv(UserService.ENV_SUPERVISOR_EMAIL)).save(failOnError: true)
-            def admin = User.findByNameOrUsername('admin', 'admin') ?: new User(name: 'admin', username: 'admin', enabled: true, password: 'admin', passwordExpired: production, email: System.getenv('MC_ADMIN_EMAIL')).save(failOnError: true)
+            def admin = User.findByNameOrUsername('admin', 'admin') ?: new User(name: 'admin', username: 'admin', enabled: true, password: 'admin', email: System.getenv('MC_ADMIN_EMAIL')).save(failOnError: true)
             def viewer = User.findByNameOrUsername('viewer', 'viewer') ?: new User(name: 'viewer', username: 'viewer', enabled: true, password: 'viewer').save(failOnError: true)
             def curator = User.findByNameOrUsername('curator', 'curator') ?: new User(name: 'curator', username: 'curator', enabled: true, password: 'curator').save(failOnError: true)
             User.findByNameOrUsername('registered', 'registered') ?: new User(name: 'registered', username: 'registered', enabled: true, password: 'registered').save(failOnError: true)
