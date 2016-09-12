f0d7499e81c2def6c9309a38812239abfdd99acc
Matouš Kučera <matous.kucera@gmail.com>
2016-09-12 14:26:31 +0200

    fix: test, here is the table footer action

--- ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/DataTypeWizardSpec.groovy
+++ ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/DataTypeWizardSpec.groovy
@@ -410,7 +410,7 @@ class DataTypeWizardSpec extends AbstractModelCatalogueGebSpec {
         waitFor { tabActive('relatedTo') }
 
         when: "click the footer action"
-        click CatalogueContent.create('#relatedTo-tab').find(detailSectionHeaderAddAction)
+        click CatalogueContent.create('#relatedTo-tab').find(tableFooterAction)
 
         then: "modal is shown"
         check modalDialog displayed
