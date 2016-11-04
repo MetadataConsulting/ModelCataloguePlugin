d96682e352d7ae22c53c4dd7285840e67a2f6a0e
Vladimír Oraný <vladimir@orany.cz>
2016-11-04 13:48:44 +0100

    MET-1330 dropped support for discourse

--- ModelCatalogueCorePluginTestApp/grails-app/conf/BuildConfig.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/conf/BuildConfig.groovy
@@ -23,7 +23,6 @@ grails.project.fork = [
 ]
 
 grails.plugin.location.'model-catalogue-core' = "../ModelCatalogueCorePlugin"
-grails.plugin.location.'model-catalogue-discourse' = "../ModelCatalogueDiscoursePlugin"
 grails.plugin.location.'model-catalogue-forms' = '../ModelCatalogueFormsPlugin'
 grails.plugin.location.'model-catalogue-genomics' = '../ModelCatalogueGenomicsPlugin'
 
