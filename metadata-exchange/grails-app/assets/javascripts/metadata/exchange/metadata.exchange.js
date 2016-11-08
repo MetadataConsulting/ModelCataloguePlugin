c573122e4d388738f429888d0cad72e12279c841
Vladimír Oraný <vladimir@orany.cz>
2016-11-08 12:16:25 +0100

    MET-1330 got ready to merged cleaned-up application back to the 2.x

--- metadata-exchange/grails-app/assets/javascripts/metadata/exchange/metadata.exchange.js
+++
@@ -1,9 +0,0 @@
-//= wrapped
-//= require /angular/angular
-//= require /metadata/exchange/core/metadata.exchange.core
-//= require /metadata/exchange/index/metadata.exchange.index
-
-angular.module("metadata.exchange", [
-        "metadata.exchange.core",
-        "metadata.exchange.index"
-    ]);
--- metadata-exchange/grails-app/init/metadata/exchange/BootStrap.groovy
+++
@@ -1,9 +0,0 @@
-package metadata.exchange
-
-class BootStrap {
-
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
-}
