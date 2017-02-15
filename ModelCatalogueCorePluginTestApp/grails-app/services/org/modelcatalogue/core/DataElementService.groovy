28481233c4712633f582fdc6275af6f0719579c3
David Milward <david@beenleigh.com>
2017-02-15 14:40:44 +0100

    MET-1331 Tags/Classifications added to version, so updated SQL to handle different Tag-sets

--- ModelCatalogueCorePluginTestApp/grails-app/services/org/modelcatalogue/core/DataElementService.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/services/org/modelcatalogue/core/DataElementService.groovy
@@ -1,5 +1,6 @@
 package org.modelcatalogue.core
 
+import org.hibernate.SQLQuery
 import org.modelcatalogue.core.util.lists.ListWithTotalAndType
 import org.modelcatalogue.core.util.lists.Lists
 
@@ -14,17 +15,70 @@ class DataElementService {
         long modelId = model.id
         long hierarchyType = RelationshipType.hierarchyType.id
         long containmentType = RelationshipType.containmentType.id
+        long tagTypeId = RelationshipType.tagType.id
+
+        def tagId = params.tag
+
+        // if tagId is null, run the same query as it is at the moment
+        // if tagId in ['none', 'null', 'undefined'] then search for every data element not destination of tag relationship
+        // if tagId search for every data element which has relationship with given tag (given tag is a source, data element is destination)
+
+        if (!tagId) {
+            return  buildDataElementsList(params,  """
+                    SELECT DISTINCT ce.*, de.data_type_id FROM catalogue_element ce
+                    JOIN data_element de on ce.id = de.id
+                    LEFT JOIN catalogue_element dm on ce.data_model_id = dm.id
+                    WHERE
+                    ce.data_model_id = :modelId
+                    or find_in_set(de.id, GetAllDestinations(GetTopLevelDataClasses(:modelId, :hierarchytypeId), :hierarchytypeId, :containmentTypeId))
+                    ORDER BY ce.name;
+                """) {
+                        setLong('modelId', modelId)
+                        setLong('hierarchytypeId', hierarchyType)
+                        setLong('containmentTypeId', containmentType)
+                    }
+        }
+
+        if (tagId in ['none', 'null', 'undefined']) {
+            return buildDataElementsList(params,  """
+                SELECT DISTINCT ce.*, de.data_type_id FROM catalogue_element ce
+                  JOIN data_element de on ce.id = de.id
+                  LEFT JOIN catalogue_element dm on ce.data_model_id = dm.id
+                  LEFT JOIN relationship rel on rel.destination_id = de.id and rel.relationship_type_id = :tagTypeId
+                WHERE
+                  (ce.data_model_id = :modelId
+                  or find_in_set(de.id, GetAllDestinations(GetTopLevelDataClasses(:modelId, :hierarchytypeId), :hierarchytypeId, :containmentTypeId)))
+                  AND rel.id is null
+                ORDER BY ce.name;
+            """) {
+                    setLong('modelId', modelId)
+                    setLong('hierarchytypeId', hierarchyType)
+                    setLong('containmentTypeId', containmentType)
+                    setLong('tagTypeId', tagTypeId)
+                }
+        }
 
 
+        buildDataElementsList(params,  """
+            SELECT DISTINCT ce.*, de.data_type_id FROM catalogue_element ce
+              JOIN data_element de on ce.id = de.id
+              LEFT JOIN catalogue_element dm on ce.data_model_id = dm.id
+            WHERE
+              (ce.data_model_id = :modelId
+              or find_in_set(de.id, GetAllDestinations(GetTopLevelDataClasses(:modelId, :hierarchytypeId), :hierarchytypeId, :containmentTypeId)))
+              AND de.id in (select destination_id from relationship where relationship_type_id = :tagTypeId and source_id = :tagId)
+            ORDER BY ce.name;
+        """) {
+            setLong('modelId', modelId)
+            setLong('hierarchytypeId', hierarchyType)
+            setLong('containmentTypeId', containmentType)
+            setLong('tagTypeId', tagTypeId)
+            setLong('tagId', tagId as Long)
+        }
+    }
+
+    private ListWithTotalAndType<DataElement>  buildDataElementsList(Map params, String query, @DelegatesTo(SQLQuery) Closure closure) {
         Lists.lazy(params, DataElement, {
-            String query = """SELECT DISTINCT ce.*, de.data_type_id FROM catalogue_element ce
-        JOIN data_element de on ce.id = de.id
-        LEFT JOIN catalogue_element dm on ce.data_model_id = dm.id
-        WHERE
-        ce.data_model_id = :modelId
-        or find_in_set(de.id, GetAllDestinations(GetTopLevelDataClasses(:modelId, :hierarchytypeId), :hierarchytypeId, :containmentTypeId))
-        ORDER BY ce.name;"""
-
             final session = sessionFactory.currentSession
 
             // Create native SQL query.
@@ -32,27 +86,14 @@ class DataElementService {
 
             // Use Groovy with() method to invoke multiple methods
             // on the sqlQuery object.
-            final results = sqlQuery.with {
-                // Set domain class as entity.
-                // Properties in domain class id, name, level will
-                // be automatically filled.
-                addEntity(DataElement)
+            sqlQuery.addEntity(DataElement)
 
-                // Set value for parameter startId.
-                setLong('modelId', modelId)
-                setLong('hierarchytypeId', hierarchyType)
-                setLong('containmentTypeId', containmentType)
+            sqlQuery.with closure
 
-                // Get all results.
-                list()
-            }
-
-            results
+            // Get all results.
+            sqlQuery.list()
         })
-
-
-
-
-
     }
+
+
 }
