64c82a19cf3deb8bae7b9994b5aed93f41771a55
Vladimir Orany <vladimir@orany.cz>
2017-03-07 15:50:42 +0100

    MET-1412 fixed the unique checker so it can handle transient data model

--- ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/policy/UniqueChecker.groovy
+++ ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/policy/UniqueChecker.groovy
@@ -12,6 +12,10 @@ import static com.google.common.base.Preconditions.checkNotNull
 
     @Override
     def <T extends CatalogueElement & GroovyObject> void check(VerificationPhase phase, DataModel model, Class<T> resource, T item, String property, String ignored, String messageOverride, boolean errorsToItem) {
+        if (!model.readyForQueries) {
+            return
+        }
+
         checkNotNull(property, 'Property must be set', new Object[0])
 
         if (!item.hasProperty(property)) {
@@ -28,9 +32,15 @@ import static com.google.common.base.Preconditions.checkNotNull
             DetachedCriteria<? extends T> criteria = new DetachedCriteria<? extends T>(resource)
             criteria.eq('dataModel', model).eq(property, value).ne('status', ElementStatus.DEPRECATED)
 
-            if (criteria.list().find { (it as CatalogueElement).getId() != item.getId() }) {
-                (errorsToItem ? item : model).errors.reject('uniqueChecker.duplicate', [model, ignored, item, property, ignored] as Object[], messageOverride ?: "Property {3} must be unique for every {2} within {0}")
-            }
+            if (item.readyForQueries) {
+                if (criteria.list().find { (it as CatalogueElement).getId() != item.getId() }) {
+                    (errorsToItem ? item : model).errors.reject('uniqueChecker.duplicate', [model, ignored, item, property, ignored] as Object[], messageOverride ?: "Property {3} must be unique for every {2} within {0}")
+                }
+            } else {
+                if (criteria.list()) {
+                    (errorsToItem ? item : model).errors.reject('uniqueChecker.duplicate', [model, ignored, item, property, ignored] as Object[], messageOverride ?: "Property {3} must be unique for every {2} within {0}")
+                }}
+
         }
 
     }
