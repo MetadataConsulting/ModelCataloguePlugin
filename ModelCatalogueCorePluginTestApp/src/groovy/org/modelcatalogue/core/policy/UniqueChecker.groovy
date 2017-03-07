79d87f0a21c3ff50b1595acb1f41be43fb695e94
Vladimir Orany <vladimir@orany.cz>
2017-03-07 16:44:54 +0100

    MET-1412 fixed the unique checker so it can handle transient data model

--- ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/policy/UniqueChecker.groovy
+++ ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/policy/UniqueChecker.groovy
@@ -12,11 +12,13 @@ import static com.google.common.base.Preconditions.checkNotNull
 
     @Override
     def <T extends CatalogueElement & GroovyObject> void check(VerificationPhase phase, DataModel model, Class<T> resource, T item, String property, String ignored, String messageOverride, boolean errorsToItem) {
-        if (!model.readyForQueries) {
+        if (!model.id) {
             return
         }
 
-        checkNotNull(property, 'Property must be set', new Object[0])
+        if (property == null) {
+            return
+        }
 
         if (!item.hasProperty(property)) {
             return
@@ -32,15 +34,15 @@ import static com.google.common.base.Preconditions.checkNotNull
             DetachedCriteria<? extends T> criteria = new DetachedCriteria<? extends T>(resource)
             criteria.eq('dataModel', model).eq(property, value).ne('status', ElementStatus.DEPRECATED)
 
-            if (item.readyForQueries) {
+            if (item.id) {
                 if (criteria.list().find { (it as CatalogueElement).getId() != item.getId() }) {
                     (errorsToItem ? item : model).errors.reject('uniqueChecker.duplicate', [model, ignored, item, property, ignored] as Object[], messageOverride ?: "Property {3} must be unique for every {2} within {0}")
                 }
             } else {
-                if (criteria.list()) {
+                if (criteria.count()) {
                     (errorsToItem ? item : model).errors.reject('uniqueChecker.duplicate', [model, ignored, item, property, ignored] as Object[], messageOverride ?: "Property {3} must be unique for every {2} within {0}")
-                }}
-
+                }
+            }
         }
 
     }
