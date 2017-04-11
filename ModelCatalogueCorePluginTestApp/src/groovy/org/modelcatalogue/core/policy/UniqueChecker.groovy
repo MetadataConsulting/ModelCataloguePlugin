1d67e26fc3740b5a118baa987de5b58f7eb88cb3
Vladimir Orany <vladimir@orany.cz>
2017-04-11 15:13:25 +0200

    MET-1460 user cannot populate metadata
    make UniqueChecker proof to working with not-yet-saved DataModel

--- ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/policy/UniqueChecker.groovy
+++ ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/policy/UniqueChecker.groovy
@@ -5,6 +5,7 @@ import grails.gorm.DetachedCriteria
 import org.modelcatalogue.core.CatalogueElement
 import org.modelcatalogue.core.DataModel
 import org.modelcatalogue.core.api.ElementStatus
+import org.modelcatalogue.core.util.HibernateHelper
 
 import static com.google.common.base.Preconditions.checkNotNull
 
@@ -12,7 +13,9 @@ import static com.google.common.base.Preconditions.checkNotNull
 
     @Override
     def <T extends CatalogueElement & GroovyObject> void check(VerificationPhase phase, DataModel model, Class<T> resource, T item, String property, String ignored, String messageOverride, boolean errorsToItem) {
-        if (!model.id) {
+        final Long dataModelId = model.id
+
+        if (!dataModelId) {
             return
         }
 
@@ -29,18 +32,20 @@ import static com.google.common.base.Preconditions.checkNotNull
         }
 
         CatalogueElement.withNewSession {
+            DataModel dataModel = DataModel.load(dataModelId)
+
             Object value = item.getProperty(property)
 
             DetachedCriteria<? extends T> criteria = new DetachedCriteria<? extends T>(resource)
-            criteria.eq('dataModel', model).eq(property, value).ne('status', ElementStatus.DEPRECATED)
+            criteria.eq('dataModel', dataModel).eq(property, value).ne('status', ElementStatus.DEPRECATED)
 
             if (item.id) {
                 if (criteria.list().find { (it as CatalogueElement).getId() != item.getId() }) {
-                    (errorsToItem ? item : model).errors.reject('uniqueChecker.duplicate', [model, ignored, item, property, ignored] as Object[], messageOverride ?: "Property {3} must be unique for every {2} within {0}")
+                    (errorsToItem ? item : dataModel).errors.reject('uniqueChecker.duplicate', [HibernateHelper.ensureNoProxy(dataModel), ignored, item, property, ignored] as Object[], messageOverride ?: "Property {3} must be unique for every {2} within {0}")
                 }
             } else {
                 if (criteria.count()) {
-                    (errorsToItem ? item : model).errors.reject('uniqueChecker.duplicate', [model, ignored, item, property, ignored] as Object[], messageOverride ?: "Property {3} must be unique for every {2} within {0}")
+                    (errorsToItem ? item : dataModel).errors.reject('uniqueChecker.duplicate', [HibernateHelper.ensureNoProxy(dataModel), ignored, item, property, ignored] as Object[], messageOverride ?: "Property {3} must be unique for every {2} within {0}")
                 }
             }
         }
