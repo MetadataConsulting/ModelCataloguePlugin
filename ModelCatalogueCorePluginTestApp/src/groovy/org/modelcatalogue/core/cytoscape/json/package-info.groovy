bc65b481d99d37266f920e0e2142078a60d3980d
James Dai <james.dai.uk@googlemail.com>
2017-04-28 16:20:43 +0100

    MET-1594 todos

--- ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/cytoscape/json/package-info.groovy
+++ ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/cytoscape/json/package-info.groovy
@@ -6,6 +6,9 @@
  * Note the JSON structure is flat: A list of nodes and list of edges, rather than
  * the XML tree structure which more or less directly reflects the class relationships
  * in a model.
+ *
+ * Still TODO: implement more ValidationRule and DataType printing
+ * TODO: use RelationshipType.sourceToDestination as relationship name
  * @see org.modelcatalogue.core.xml
  */
 package org.modelcatalogue.core.cytoscape.json
