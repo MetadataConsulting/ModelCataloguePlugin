16d68063b4d15925d407b78202f1734189f852b1
Adam Milward <adam.milward@outlook.com>
2017-07-19 12:05:14 +0100

    Fixing bootstrap and datasource before merge

--- ModelCatalogueCorePluginTestApp/grails-app/conf/DataSource.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/conf/DataSource.groovy
@@ -10,25 +10,14 @@ environments {
     // XXX: never commit your local configuration overrides for this file!!!
     development {
         dataSource {
-            driverClassName = "com.mysql.jdbc.Driver"
-            dialect = 'org.hibernate.dialect.MySQL5InnoDBDialect'
-            url = "jdbc:mysql://localhost:3306/gel_latest?autoReconnect=true&useUnicode=yes"
-            username = 'root'
-            password = ''
-            dbCreate = "update"
-            properties {
-                maxActive = -1
-                minEvictableIdleTimeMillis = 1800000
-                timeBetweenEvictionRunsMillis = 1800000
-                numTestsPerEvictionRun = 3
-                testOnBorrow = true
-                testWhileIdle = true
-                testOnReturn = false
-                validationQuery = "SELECT 1"
-                jdbcInterceptors = "ConnectionState"
-
-            }
+            pooled = true
+            driverClassName = "org.h2.Driver"
+            username = "sa"
+            password = ""
+            dbCreate = "create-drop"
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
+
     }
     test {
         if (System.getenv('DOCKERIZED_TESTS') && System.properties["grails.test.phase"] == 'functional') {
