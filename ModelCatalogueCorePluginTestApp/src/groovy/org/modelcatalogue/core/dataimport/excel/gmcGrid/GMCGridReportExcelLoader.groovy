52bc839c1f7472b4ddfede36fceb65d63388acc4
James Dai <james.dai.uk@googlemail.com>
2017-08-15 10:59:34 +0100

    created new class GMCGridReportExcelLoader

---
+++ ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/dataimport/excel/gmcGrid/GMCGridReportExcelLoader.groovy
@@ -0,0 +1,10 @@
+package org.modelcatalogue.core.dataimport.excel.gmcGrid
+
+import org.modelcatalogue.core.dataimport.excel.ExcelLoader
+
+/**
+ * Created by james on 15/08/2017.
+ */
+class GMCGridReportExcelLoader extends ExcelLoader {
+
+}
