3e3a96e6537a55adbb509119442aab725282f251
James Dai <james.dai.uk@googlemail.com>
2017-08-16 17:10:39 +0100

    modified CatalogueXmlImportSpec to use closure/build instructions so it can test defaultCatalogueBuilder directly

--- ModelCatalogueCorePluginTestApp/test/integration/org/modelcatalogue/core/xml/CatalogueXmlImportSpec.groovy
+++ ModelCatalogueCorePluginTestApp/test/integration/org/modelcatalogue/core/xml/CatalogueXmlImportSpec.groovy
@@ -1,30 +1,33 @@
 package org.modelcatalogue.core.xml
 
 import org.codehaus.groovy.grails.commons.GrailsApplication
+import org.modelcatalogue.builder.xml.XmlCatalogueBuilder
 import org.modelcatalogue.core.*
 import org.modelcatalogue.core.api.ElementStatus
 import org.modelcatalogue.core.util.builder.DefaultCatalogueBuilder
 import org.modelcatalogue.integration.xml.CatalogueXmlLoader
 import spock.lang.Shared
 
+import java.nio.charset.StandardCharsets
+
 class CatalogueXmlImportSpec extends AbstractIntegrationSpec {
 
     @Shared
     GrailsApplication grailsApplication
 
-    def dataModelService
-    def elementService
-    @Shared DefaultCatalogueBuilder builder
+    DataModelService dataModelService
+    ElementService elementService
+    @Shared DefaultCatalogueBuilder defaultCatalogueBuilder
     @Shared CatalogueXmlLoader catalogueXmlLoader
 
     def setup() {
         initCatalogue()
-        builder = new DefaultCatalogueBuilder(dataModelService, elementService, true)
-        catalogueXmlLoader = new CatalogueXmlLoader(builder)
+        defaultCatalogueBuilder = new DefaultCatalogueBuilder(dataModelService, elementService, true)
+        catalogueXmlLoader = new CatalogueXmlLoader(defaultCatalogueBuilder)
 
     }
 
-    def "load bases"(){
+    def "load bases"() {
         final String DATA_MODEL_NAME = "based on test"
         final String GRAND_PARENT_NAME = "grand parent"
         final String PARENT_NAME = "parent"
@@ -34,39 +37,76 @@ class CatalogueXmlImportSpec extends AbstractIntegrationSpec {
 
 
         when:
-            InputStream nhic = getClass().getResourceAsStream('grand_child_without_id.catalogue.xml')
-            catalogueXmlLoader.load(nhic)
+        InputStream nhic = getClass().getResourceAsStream('grand_child_without_id.catalogue.xml')
+        catalogueXmlLoader.load(nhic)
 
-            DataModel dataModel = DataModel.findByName(DATA_MODEL_NAME)
-            DataType grandParent = DataType.findByName(GRAND_PARENT_NAME)
-            DataType parent = DataType.findByName(PARENT_NAME)
-            DataType child = DataType.findByName(CHILD_NAME)
-            DataType grandChild = DataType.findByName(GRAND_CHILD_NAME)
+        DataModel dataModel = DataModel.findByName(DATA_MODEL_NAME)
+        DataType grandParent = DataType.findByName(GRAND_PARENT_NAME)
+        DataType parent = DataType.findByName(PARENT_NAME)
+        DataType child = DataType.findByName(CHILD_NAME)
+        DataType grandChild = DataType.findByName(GRAND_CHILD_NAME)
 
         then:
-            dataModel
-            grandParent
-            grandParent.dataModel == dataModel
-            parent
-            parent.dataModel == dataModel
-            child
-            child.dataModel == dataModel
-            grandChild
-            grandChild.dataModel == dataModel
+        dataModel
+        grandParent
+        grandParent.dataModel == dataModel
+        parent
+        parent.dataModel == dataModel
+        child
+        child.dataModel == dataModel
+        grandChild
+        grandChild.dataModel == dataModel
+    }
+    Closure testUpdateModelsInitialInstructions = {
+        dataModel(name:'DM1'){
+            dataElement(name:'DE1'){
+                ext 'EXT1', 'EXTVAL1'
+            }
+        }
+        dataModel(name:'DM2') {
+            dataElement(name:'DE2')
+        }
+    } as Closure
+    Closure testUpdateModelsUpdate1Instructions = {
+        dataModel(name:'DM1')
+        dataModel(name:'DM2') {
+            dataElement(name:'DE1') {
+                ext 'EXT1', 'EXTVAL1'
+            }
+        }
+    } as Closure
+    Closure testUpdateModelsUpdate2Instructions = {
+        dataModel(name:'DM1'){
+            dataElement(name:'DE1'){
+                ext 'EXT1', 'EXTVAL2'
+                ext 'EXT2', 'EXTVAL3'
+            }
+        }
+    } as Closure
+    InputStream inputStreamFromString(String s) {
+        return new ByteArrayInputStream(s.getBytes(StandardCharsets.UTF_8))
+    }
+    String xmlFromInstructions(Closure instructions) {
+        StringWriter stringWriter = new StringWriter()
+        XmlCatalogueBuilder xmlCatalogueBuilder = new XmlCatalogueBuilder(stringWriter, true)
+        xmlCatalogueBuilder.build(instructions)
+        return stringWriter.toString()
     }
 
     def "test updating a data model"() {
         when: "initial models loaded"
-            catalogueXmlLoader.load(getClass().getResourceAsStream('test_update_models_initial.catalogue.xml'))
-
+            catalogueXmlLoader.load(inputStreamFromString(xmlFromInstructions(testUpdateModelsInitialInstructions)))
+            //defaultCatalogueBuilder.build(testUpdateModelsInitialInstructions)
         then:
             DataModel dataModel = DataModel.findByName('DM1')
             assert dataModel
             dataModel.dataElements.findAll {dataElement ->
                 dataElement.name == 'DE1'
             }.size() == 1 // a DM1 data model has a DE1 data element
+
         when: "models updated 1st time (trying to 'move' DE1 to DM2)"
-            catalogueXmlLoader.load(getClass().getResourceAsStream('test_update_models_update1.catalogue.xml'))
+            catalogueXmlLoader.load(inputStreamFromString(xmlFromInstructions(testUpdateModelsUpdate1Instructions)))
+            //defaultCatalogueBuilder.build(testUpdateModelsUpdate1Instructions)
         then:
             List<DataModel> dataModelsDM1U1 = DataModel.findAllByName('DM1')
             /**
@@ -95,8 +135,10 @@ class CatalogueXmlImportSpec extends AbstractIntegrationSpec {
             dataModelDM2Current.dataElements.find {dataElement ->
                 dataElement.name == 'DE2'
             }
+
         when: "models updated 2nd time (just changing DE1's metadata in DM1)"
-            catalogueXmlLoader.load(getClass().getResourceAsStream('test_update_models_update2.catalogue.xml'))
+            catalogueXmlLoader.load(inputStreamFromString(xmlFromInstructions(testUpdateModelsUpdate2Instructions)))
+            //defaultCatalogueBuilder.build(testUpdateModelsUpdate2Instructions)
         then:
             List<DataModel> dataModelsDM1U2 = DataModel.findAllByName('DM1')
             /**
@@ -109,9 +151,5 @@ class CatalogueXmlImportSpec extends AbstractIntegrationSpec {
             }
             dm1de1s.size() == 1
             dm1de1s[0].ext.get('EXT1') == 'EXTVAL2'
-
-
-
     }
-
 }
