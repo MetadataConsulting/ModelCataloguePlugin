b5810f7bdc97afac87dd307800cb2d5fd2380041
James Dai <james.dai.uk@googlemail.com>
2017-08-17 18:30:50 +0100

    GMCGridReportExcelLoaderSpec stub.

---
+++ ModelCatalogueCorePluginTestApp/test/integration/org/modelcatalogue/core/dataimport/excel/gmcGridReport/GMCGridReportExcelLoaderSpec.groovy
@@ -0,0 +1,18 @@
+package org.modelcatalogue.core.dataimport.excel.gmcGridReport
+
+import org.modelcatalogue.core.dataimport.excel.ExcelLoaderSpec
+
+/**
+ * Should test the following sequence:
+ * Load/import a GridReport r2 for the first time so that it creates new models;
+ * Check results;
+ * Export a GridReport r2;
+ * (Automatically alter it to r2+dr);
+ * Load/import r2+dr so that it updates;
+ * Check changes;
+ * Created by james on 17/08/2017.
+ */
+class GMCGridReportExcelLoaderSpec extends ExcelLoaderSpec {
+
+}
+
