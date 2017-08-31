12e7e2024abbd27dfaa32ee9306754e8eb96e217
David Milward <david@beenleigh.com>
2017-08-31 09:55:33 +0100

    MNOR-7 OpenEhr importer - skeleton added

---
+++ ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/dataimport/excel/nt/uclh/OpenEhrExcelLoader.groovy
@@ -0,0 +1,14 @@
+package org.modelcatalogue.core.dataimport.excel.nt.uclh
+
+import org.modelcatalogue.core.dataimport.excel.uclh.UCLHExcelLoader
+
+/**
+ * Created by davidmilward on 31/08/2017.
+ */
+class OpenEhrExcelLoader extends UCLHExcelLoader {
+
+
+
+
+
+}
---
+++ ModelCatalogueCorePluginTestApp/test/integration/org/modelcatalogue/core/dataimport/excel/nt/uclh/OpenEhrExcelLoaderSpec.groovy
@@ -0,0 +1,34 @@
+package org.modelcatalogue.core.dataimport.excel.nt.uclh
+
+import org.modelcatalogue.core.dataimport.excel.uclh.UCLHExcelLoaderSpec
+
+/**
+ * Created by davidmilward on 31/08/2017.
+ */
+class OpenEhrExcelLoaderSpec extends UCLHExcelLoaderSpec{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
