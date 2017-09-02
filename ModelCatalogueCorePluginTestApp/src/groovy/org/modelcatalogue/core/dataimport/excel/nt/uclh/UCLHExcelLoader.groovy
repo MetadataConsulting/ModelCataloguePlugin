b5b2afca275593fb5140b581b1704f632bb994b5
David Milward <david@beenleigh.com>
2017-09-02 15:28:03 +0100

    NOR-7 Tidy - Removed commented out code

--- ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/dataimport/excel/nt/uclh/UCLHExcelLoader.groovy
+++ ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/dataimport/excel/nt/uclh/UCLHExcelLoader.groovy
@@ -83,76 +83,7 @@ class UCLHExcelLoader extends ExcelLoader{
     }  // map from header keys to their capitalized forms used as metadata keys
 
 
-//    void buildModelFromInstructions(DefaultCatalogueBuilder defaultCatalogueBuilder, Closure buildInstructions = {}) {
-//
-//        defaultCatalogueBuilder.build buildInstructions
-//    }
 
-
-
-//    Pair<Closure, List<String>> buildInstructionsAndModelNamesFromWorkbookSheet(Workbook workbook, String sheetName, String owner='') {
-//
-//        if (owner == '') {
-//            ownerSuffix = ''
-//        }
-//        else {
-//            ownerSuffix = '_' + owner
-//        }
-//
-//        if (!workbook) {
-//            throw new IllegalArgumentException("Excel file contains no worksheet!")
-//        }
-//        //Sheet sheet = workbook.getSheetAt(index);
-//        Sheet sheet = workbook.getSheet(sheetName)
-//        Iterator<Row> rowIt = sheet.rowIterator()
-//        Row row = rowIt.next()
-//        List<String> headers = getRowData(row)
-//        List<Map<String, String>> rowMaps = []
-//
-//        while (rowIt.hasNext()) {
-//            row = rowIt.next()
-//            if(!isRowEmpty(row)){
-//                Map<String, String> rowMap = createRowMap(row, headers)
-//                rowMaps << rowMap
-//            }
-//        }
-//
-//        Map<String, List<Map<String, String>>> modelMaps = rowMaps.groupBy{
-//            String modelName = it.get('Collected from')
-//            if(modelName){
-//                it.get('Collected from')+getOwnerSuffixWithRandom()
-//            }
-//        }
-//
-//        List<String> modelNames = modelMaps.keySet() as List<String>
-//        Map<String, String> metadataHeaders = ['Semantic Matching',	'Known issue',	'Immediate solution', 'Immediate solution Owner',
-//                                               'Long term solution',	'Long term solution owner',	'Data Item', 'Unique Code',
-//                                               'Related To',	'Part of standard data set',
-//                                               'Data Completeness','Estimated quality',
-//                                               'Timely?', 'Comments'].collectEntries {
-//            header -> [(header), WordUtils.capitalizeFully(header).replaceAll(/\?/,'')]
-//        } // map from header keys to their capitalized forms used as metadata keys
-//        String defaultMetadataValue = ''
-//        Closure resultClosure =    {
-//            modelMaps.each{ String modelName, List<Map<String,String>> modelRowMaps ->
-//                dataModel('name': modelName) {
-//                    ext 'http://www.modelcatalogue.org/metadata/#organization', 'UCL'
-//                    modelRowMaps.each { Map<String, String> rowMap ->
-//                        String dename = getNTElementName(rowMap)
-//                        if(!dename.equalsIgnoreCase('blankcell_ph')) {
-//                            dataElement(name: dename) {
-//                                metadataHeaders.each { k, v ->
-//                                    ext v, (rowMap[k] ?: defaultMetadataValue)
-//                                }
-//                                ext 'represents', "${getMCIdFromSpreadSheet(rowMap)}"
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        } as Closure
-//        return Pair.of(resultClosure, modelNames)
-//    }
 
 
     List<String>  loadModel(Workbook workbook, String sheetName, String owner='') {
