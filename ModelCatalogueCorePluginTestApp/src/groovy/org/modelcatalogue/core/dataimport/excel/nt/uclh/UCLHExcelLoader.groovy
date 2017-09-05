88076c29d2d10410ba88dd00d2128f9f2543f173
James Dai <james.dai.uk@googlemail.com>
2017-09-05 16:16:47 +0100

    add model organization metadata

--- ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/dataimport/excel/nt/uclh/UCLHExcelLoader.groovy
+++ ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/dataimport/excel/nt/uclh/UCLHExcelLoader.groovy
@@ -20,6 +20,7 @@ import org.modelcatalogue.core.dataimport.excel.ExcelLoader
 import org.apache.commons.lang.WordUtils
 import org.apache.commons.lang3.tuple.Pair
 import org.modelcatalogue.core.dataimport.excel.gmcGridReport.GMCGridReportExcelLoader
+import org.modelcatalogue.core.dataimport.excel.gmcGridReport.GMCGridReportXlsxExporter
 import org.modelcatalogue.core.util.builder.DefaultCatalogueBuilder
 
 /**
@@ -83,70 +84,70 @@ class UCLHExcelLoader extends ExcelLoader{
     }  // map from header keys to their capitalized forms used as metadata keys
 
 
-//    void buildModelFromInstructions(DefaultCatalogueBuilder defaultCatalogueBuilder, Closure buildInstructions = {}) {
-//
-//        defaultCatalogueBuilder.build buildInstructions
-//    }
+/*    void buildModelFromInstructions(DefaultCatalogueBuilder defaultCatalogueBuilder, Closure buildInstructions = {}) {
 
+        defaultCatalogueBuilder.build buildInstructions
+    }*/
 
+/*
 
-//    Pair<Closure, List<String>> buildInstructionsAndModelNamesFromWorkbookSheet(Workbook workbook, String sheetName, String owner='') {
-//
-//        if (owner == '') {
-//            ownerSuffix = ''
-//        }
-//        else {
-//            ownerSuffix = '_' + owner
-//        }
-//
-//        if (!workbook) {
-//            throw new IllegalArgumentException("Excel file contains no worksheet!")
-//        }
-//        //Sheet sheet = workbook.getSheetAt(index);
-//        Sheet sheet = workbook.getSheet(sheetName)
-//        Iterator<Row> rowIt = sheet.rowIterator()
-//        Row row = rowIt.next()
-//        List<String> headers = getRowData(row)
-//        List<Map<String, String>> rowMaps = []
-//
-//        while (rowIt.hasNext()) {
-//            row = rowIt.next()
-//            if(!isRowEmpty(row)){
-//                Map<String, String> rowMap = createRowMap(row, headers)
-//                rowMaps << rowMap
-//            }
-//        }
-//
-//        Map<String, List<Map<String, String>>> modelMaps = rowMaps.groupBy{
-//            String modelName = it.get('Collected from')
-//            if(modelName){
-//                it.get('Collected from')+getOwnerSuffixWithRandom()
-//            }
-//        }
-//
-//        List<String> modelNames = modelMaps.keySet() as List<String>
+    Pair<Closure, List<String>> buildInstructionsAndModelNamesFromWorkbookSheet(Workbook workbook, String sheetName, String owner='') {
 
-//        String defaultMetadataValue = ''
-//        Closure resultClosure =    {
-//            modelMaps.each{ String modelName, List<Map<String,String>> modelRowMaps ->
-//                dataModel('name': modelName) {
-//                    ext 'http://www.modelcatalogue.org/metadata/#organization', 'UCL'
-//                    modelRowMaps.each { Map<String, String> rowMap ->
-//                        String dename = getNTElementName(rowMap)
-//                        if(!dename.equalsIgnoreCase('blankcell_ph')) {
-//                            dataElement(name: dename) {
-//                                metadataHeaders.each { k, v ->
-//                                    ext v, (rowMap[k] ?: defaultMetadataValue)
-//                                }
-//                                ext 'represents', "${getMCIdFromSpreadSheet(rowMap)}"
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        } as Closure
-//        return Pair.of(resultClosure, modelNames)
-//    }
+        if (owner == '') {
+            ownerSuffix = ''
+        }
+        else {
+            ownerSuffix = '_' + owner
+        }
+
+        if (!workbook) {
+            throw new IllegalArgumentException("Excel file contains no worksheet!")
+        }
+        //Sheet sheet = workbook.getSheetAt(index);
+        Sheet sheet = workbook.getSheet(sheetName)
+        Iterator<Row> rowIt = sheet.rowIterator()
+        Row row = rowIt.next()
+        List<String> headers = getRowData(row)
+        List<Map<String, String>> rowMaps = []
+
+        while (rowIt.hasNext()) {
+            row = rowIt.next()
+            if(!isRowEmpty(row)){
+                Map<String, String> rowMap = createRowMap(row, headers)
+                rowMaps << rowMap
+            }
+        }
+
+        Map<String, List<Map<String, String>>> modelMaps = rowMaps.groupBy{
+            String modelName = it.get('Collected from')
+            if(modelName){
+                it.get('Collected from')+getOwnerSuffixWithRandom()
+            }
+        }
+
+        List<String> modelNames = modelMaps.keySet() as List<String>
+
+        String defaultMetadataValue = ''
+        Closure resultClosure =    {
+            modelMaps.each{ String modelName, List<Map<String,String>> modelRowMaps ->
+                dataModel('name': modelName) {
+                    ext 'http://www.modelcatalogue.org/metadata/#organization', 'UCL'
+                    modelRowMaps.each { Map<String, String> rowMap ->
+                        String dename = getNTElementName(rowMap)
+                        if(!dename.equalsIgnoreCase('blankcell_ph')) {
+                            dataElement(name: dename) {
+                                metadataHeaders.each { k, v ->
+                                    ext v, (rowMap[k] ?: defaultMetadataValue)
+                                }
+                                ext 'represents', "${getMCIdFromSpreadSheet(rowMap)}"
+                            }
+                        }
+                    }
+                }
+            }
+        } as Closure
+        return Pair.of(resultClosure, modelNames)
+    }*/
 
 
     List<String>  loadModel(Workbook workbook, String sheetName, String owner='') {
@@ -196,6 +197,7 @@ class UCLHExcelLoader extends ExcelLoader{
             "Start import of model $name",
             {
             DataModel newModel = getDataModel(name)
+            newModel.addExtension(GMCGridReportXlsxExporter.organizationMetadataKey, owner)
             //Iterate through each row to build an new DataElement
             rowMapsForModel.each{ Map<String, String> rowMap ->
                 String ntname = getNTElementName(rowMap)
