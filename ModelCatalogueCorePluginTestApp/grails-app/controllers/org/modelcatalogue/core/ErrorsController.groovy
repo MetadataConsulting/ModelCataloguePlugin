2c26ffe177e929f3281f553d603fe528bce5b44f
sdelamo <sergio.delamo@softamo.com>
2017-10-23 18:03:48 +0200

    Add individual actions for different erros

--- ModelCatalogueCorePluginTestApp/grails-app/controllers/org/modelcatalogue/core/ErrorsController.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/controllers/org/modelcatalogue/core/ErrorsController.groovy
@@ -1,18 +1,29 @@
 package org.modelcatalogue.core
 
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.acls.model.NotFoundException
 import groovy.transform.CompileStatic
 import grails.plugin.springsecurity.annotation.Secured
+import static org.springframework.http.HttpStatus.UNAUTHORIZED
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
 
 @Secured('permitAll')
 @CompileStatic
 class ErrorsController {
 
     def error403() {
-        response.status = 403
-        [:]
+        render status: UNAUTHORIZED
+    }
+
+    def handleAccessDeniedException() {
+        render status: UNAUTHORIZED
+    }
+
+    def handleNotFoundException() {
+        render status: UNAUTHORIZED
     }
 
     def error500() {
-        render view: '/error'
+        render view: '/error', status: INTERNAL_SERVER_ERROR
     }
 }
