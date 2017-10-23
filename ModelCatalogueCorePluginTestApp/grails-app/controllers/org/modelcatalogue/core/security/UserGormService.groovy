2fa11fe361cf6e102cae9604bdb45d54357af1d1
sdelamo <sergio.delamo@softamo.com>
2017-10-23 19:24:52 +0200

    Move switchEnabled to service

--- ModelCatalogueCorePluginTestApp/grails-app/controllers/org/modelcatalogue/core/security/UserGormService.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/controllers/org/modelcatalogue/core/security/UserGormService.groovy
@@ -15,4 +15,28 @@ class UserGormService {
     User findById(long id) {
         User.get(id)
     }
+
+    /**
+     *
+     * @param userId
+     * @param enabled
+     * @return null if the user is not found, user with or without erros after the updates
+     */
+    @Transactional
+    User switchEnabled(long userId, boolean enabled) {
+        User user = findById(userId)
+        if (!user) {
+            return null
+        }
+
+        if (user.authorities.contains(UserService.ROLE_SUPERVISOR)) {
+            user.errors.rejectValue('enabled', 'user.cannot.edit.supervisor', 'Cannot edit supervisor account')
+            return user
+        }
+
+        user.enabled = enabled
+
+        user.save(flush: true)
+        user
+    }
 }
