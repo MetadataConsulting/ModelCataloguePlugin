bf22ca50c5f27974e2e4865db0e550d520620648
sdelamo <sergio.delamo@softamo.com>
2017-11-10 12:28:49 +0100

    Extract initPoliciesAndTags to Service

--- ModelCatalogueCorePluginTestApp/grails-app/conf/BootStrap.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/conf/BootStrap.groovy
@@ -47,13 +47,13 @@ class BootStrap {
         if (Environment.current in [ Environment.TEST] && !System.getenv('MC_BLANK_DEV')) {
             TestDataHelper.initFreshDb(sessionFactory, 'initTestDatabase.sql') {
                 initCatalogueService.initCatalogue(false)
-                initPoliciesAndTags()
+                initPoliciesAndTagsService.initPoliciesAndTags()
                 initSecurity(false)
                 setupDevTestStuff()
             }
         } else {
             initCatalogueService.initDefaultRelationshipTypes()
-            initPoliciesAndTags()
+            initPoliciesAndTagsService.initPoliciesAndTags()
             initSecurity(!System.getenv('MC_BLANK_DEV'))
         }
 
@@ -94,45 +94,7 @@ class BootStrap {
 
         final def var1 = log.info("completed:initSecurity")
     }
-
-    def initPoliciesAndTags() {
-        log.info "start:initPoliciesAndTags"
-
-        catalogueBuilder.build {
-            dataModelPolicy(name: 'Unique of Kind', overwrite: true) {
-                check dataClass property 'name' is 'unique'
-                check dataElement property 'name' is 'unique'
-                check dataType property 'name' is 'unique'
-                check validationRule property 'name' is 'unique'
-                check measurementUnit property 'name' is 'unique'
-                check ModelCatalogueTypes.CATALOGUE_ELEMENT property 'modelCatalogueId' is 'unique'
-            }
-            dataModelPolicy(name: 'Default Checks') {
-                check dataModel extension 'http://www.modelcatalogue.org/metadata/#authors' is 'required' otherwise 'Metadata "Authors" is missing for {2}'
-                check dataModel extension 'http://www.modelcatalogue.org/metadata/#reviewers' is 'required' otherwise 'Metadata "Reviewers" is missing for {2}'
-                check dataModel extension 'http://www.modelcatalogue.org/metadata/#owner' is 'required' otherwise 'Metadata "Owner" is missing for {2}'
-                check dataModel extension 'http://www.modelcatalogue.org/metadata/#reviewed' is 'required' otherwise 'Metadata "Reviewed" is missing for {2}'
-                check dataModel extension 'http://www.modelcatalogue.org/metadata/#approved' is 'required' otherwise 'Metadata "Approved" is missing for {2}'
-                check dataModel extension 'http://www.modelcatalogue.org/metadata/#namespace' is 'required' otherwise 'Metadata "Namespace" is missing for {2}'
-                check dataModel extension 'http://www.modelcatalogue.org/metadata/#organization' is 'required' otherwise 'Metadata "Organization" is missing for {2}'
-
-                check dataElement property 'dataType' is 'required' otherwise 'Data type is missing for {2}'
-                check dataElement property 'name' is 'unique' otherwise 'Data element\'s name is not unique for {2}'
-                check dataType property 'name' is 'unique' otherwise 'Data type\'s name is not unique for {2}'
-                check dataType property 'name' apply regex: /[^_ -]+/ otherwise 'Name of {2} contains illegal characters ("_", "-" or " ")'
-            }
-            dataModel(name: 'Clinical Tags') {
-                tag(name: 'Highly Sensitive PI data')
-                tag(name: 'Sensitive PI data')
-                tag(name: 'Highly Sensitive data')
-                tag(name: 'Sensitive data')
-                tag(name: 'Internal data')
-                tag(name: 'External data')
-            }
-            log.info "complete:initPoliciesAndTags"
-        }
-    }
-
+    
     def setupDevTestStuff(){
         actionService.resetAllRunningActions()
         try {
