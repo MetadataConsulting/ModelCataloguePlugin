d901207a145d5f9242a11f2846341f6d5b024e9d
sdelamo <sergio.delamo@softamo.com>
2017-11-26 10:20:22 +0100

    findByName methods to DataModelPolicyGormService

--- ModelCatalogueCorePluginTestApp/grails-app/services/org/modelcatalogue/core/persistence/DataModelPolicyGormService.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/services/org/modelcatalogue/core/persistence/DataModelPolicyGormService.groovy
@@ -1,12 +1,22 @@
 package org.modelcatalogue.core.persistence
 
+import grails.transaction.Transactional
 import org.modelcatalogue.core.DataModelPolicy
-import org.springframework.transaction.annotation.Transactional
+import grails.gorm.DetachedCriteria
 
 class DataModelPolicyGormService {
 
     @Transactional
     DataModelPolicy findById(long id) {
         DataModelPolicy.get(id)
+    }
+
+    @Transactional(readOnly = true)
+    DataModelPolicy findByName(String name) {
+        findQueryByName(name).get()
+    }
+
+    protected DetachedCriteria<DataModelPolicy> findQueryByName(String nameParam) {
+        DataModelPolicy.where { name == nameParam }
     }
 }
