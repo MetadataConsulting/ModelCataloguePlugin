3508ea48d9a130b1c83d7642cdcd2fabcf1c6253
sdelamo <sergio.delamo@softamo.com>
2017-11-28 09:25:51 +0100

    reorder import

--- ModelCatalogueCorePluginTestApp/grails-app/controllers/org/modelcatalogue/core/ErrorsController.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/controllers/org/modelcatalogue/core/ErrorsController.groovy
@@ -1,10 +1,10 @@
 package org.modelcatalogue.core
 
-import groovy.transform.CompileStatic
-import grails.plugin.springsecurity.annotation.Secured
 import static org.springframework.http.HttpStatus.UNAUTHORIZED
 import static org.springframework.http.HttpStatus.NOT_FOUND
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
+import groovy.transform.CompileStatic
+import grails.plugin.springsecurity.annotation.Secured
 
 @Secured('permitAll')
 @CompileStatic
