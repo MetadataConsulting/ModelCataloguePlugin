0dc915fa69de757456649661d0c6c6d09c614db5
sdelamo <sergio.delamo@softamo.com>
2017-11-28 09:25:42 +0100

    Remove public qualifier

--- ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/comments/CommentsService.groovy
+++ ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/comments/CommentsService.groovy
@@ -2,7 +2,7 @@ package org.modelcatalogue.core.comments
 
 import org.modelcatalogue.core.CatalogueElement
 
-public interface CommentsService {
+interface CommentsService {
     List<Comment> getComments(CatalogueElement element)
     boolean isForumEnabled()
 }
