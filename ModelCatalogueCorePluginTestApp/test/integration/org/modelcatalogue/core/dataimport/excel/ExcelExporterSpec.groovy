f70187ce0697157b0264525b9e01886330c0189b
David Milward <david@beenleigh.com>
2017-12-06 21:56:07 +0000

    IgnoreIf for functional tests

--- ModelCatalogueCorePluginTestApp/test/integration/org/modelcatalogue/core/dataimport/excel/ExcelExporterSpec.groovy
+++ ModelCatalogueCorePluginTestApp/test/integration/org/modelcatalogue/core/dataimport/excel/ExcelExporterSpec.groovy
@@ -11,7 +11,7 @@ import org.modelcatalogue.spreadsheet.query.api.SpreadsheetCriteria
 import org.modelcatalogue.spreadsheet.query.poi.PoiSpreadsheetQuery
 import spock.lang.IgnoreIf
 
-@IgnoreIf( { System.getenv('jenkins.ignore') })
+
 class ExcelExporterSpec extends AbstractIntegrationSpec {
 
     public static final String ROOT_DATA_MODEL_NAME = 'Grid Report Data Model'
@@ -35,7 +35,7 @@ class ExcelExporterSpec extends AbstractIntegrationSpec {
 
     }
 
-    @IgnoreIf( { System.getProperty('spock.ignore.slow')| System.getenv('jenkins.ignore') })
+    @IgnoreIf( { System.getProperty('spock.ignore.slow')|| System.getenv('jenkins.ignore') })
     def "export model to excel"() {
         setup:
         def file = temporaryFolder.newFile("${System.currentTimeMillis()}.xlsx")
