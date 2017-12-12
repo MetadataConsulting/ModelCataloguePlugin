efdd0147283b7bc6051449fda8fb0451e32b0a2f
sdelamo <sergio.delamo@softamo.com>
2017-12-12 12:45:39 +0100

    Use types

--- ModelCatalogueCorePluginTestApp/grails-app/controllers/org/modelcatalogue/core/DataTypeController.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/controllers/org/modelcatalogue/core/DataTypeController.groovy
@@ -62,19 +62,21 @@ class DataTypeController<T> extends AbstractCatalogueElementController<DataType>
             return
         }
 
-        if (!params.value) {
+        Object value = params.value
+
+        if (!value) {
             respond result: "Please, enter value."
             return
         }
 
-        def valid = dataType.validateRule(params.value)
+        boolean valid = dataType.validateRule(value)
 
-        if (!(valid instanceof Boolean && valid)) {
+        if ( !valid ) {
             respond result: "INVALID: Please, enter valid value"
             return
         }
 
-        def result = mapping.map(params.value)
+        def result = mapping.map(value)
 
         if (result instanceof Exception) {
             respond result: "ERROR: ${result.class.simpleName}: $result.message"
