MetadataConsulting/ModelCataloguePlugin

commit 52e1865f57d1c6177d6dd5b792214f6b73b2dc3f

added new north thames mapping exporter added the register reports code to the bootstrap (fixes the no exports problem)

ModelCatalogueCorePluginTestApp/grails-app/conf/ModelCatalogueNorthThamesUrlMappings.groovy
@@ -4,6 +4,7 @@ class ModelCatalogueNorthThamesUrlMappings {
 
     static mappings = {
         "/api/modelCatalogue/core/northThames/northThamesSummaryReport/$id" (controller: 'northThames', action: 'northThamesSummaryReport', method: HttpMethod.GET)
+        "/api/modelCatalogue/core/northThames/northThamesSummaryReport/$id" (controller: 'northThames', action: 'northThamesMappingReport', method: HttpMethod.GET)
     }
 
 }

ModelCatalogueCorePluginTestApp/grails-app/conf/BootStrap.groovy
@@ -65,6 +65,8 @@ class BootStrap {
 
         }  else if ( isProduction() ) {
             initProd()
+            log.info 'init register reports'
+            registerReportsService.register()
         }
     }
 

ModelCatalogueCorePluginTestApp/grails-app/controllers/org/modelcatalogue/core/norththames/NorthThamesController.groovy
@@ -1,6 +1,8 @@
 package org.modelcatalogue.core.norththames
 
+import org.hibernate.SessionFactory
 import org.modelcatalogue.core.DataModel
+import org.modelcatalogue.core.dataexport.excel.norththamesreport.NorthThamesMappingReportXlsxExporter
 import org.modelcatalogue.core.persistence.DataModelGormService
 import org.modelcatalogue.core.dataexport.excel.gmcgridreport.GMCGridReportXlsxExporter
 import org.springframework.http.HttpStatus
@@ -12,7 +14,8 @@ class NorthThamesController {
 
     DataModelGormService dataModelGormService
 
-    def dataClassService, assetService
+    def dataClassService, assetService, dataElementService
+    SessionFactory sessionFactory
 
     //produce a grid report spreadsheet where the whole data set is displayed as a grid with metadata and relationships (rather then tabs)
 
@@ -38,6 +41,37 @@ class NorthThamesController {
 
         response.setHeader("X-Asset-ID", assetId.toString())
         redirect controller: 'asset', id: assetId, action: 'show'
+    }
+
+
+    //produce a mapping report
+
+    def northThamesMappingReport(String name, Integer depth) {
+        DataModel dataModel = dataModelGormService.findById(params.long('id'))
+        Long dataModelId = dataModel.id
+
+        if (!dataModel) {
+            respond status: HttpStatus.NOT_FOUND
+            return
+        }
+
+        def assetId = assetService.storeReportAsAsset(
+                dataModel,
+                name: name ? name : "${dataModel.name} report as MS Excel Document-for-North Thames",
+                originalFileName: "North Thames-${dataModel.name}-${dataModel.status}-${dataModel.version}.xlsx",
+                contentType: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        ) { OutputStream outputStream ->
+            // reload domain class as this is called in separate thread
+            NorthThamesMappingReportXlsxExporter.create(dataModelGormService.findById(dataModelId), dataClassService, dataElementService, grailsApplication, isDatabaseFallback()).export(outputStream)
+        }
+
+        response.setHeader("X-Asset-ID", assetId.toString())
+        redirect controller: 'asset', id: assetId, action: 'show'
+    }
+
+    private boolean isDatabaseFallback() {
+        if(sessionFactory.currentSession.connection().metaData.databaseProductName != 'MySQL') return false
+        true
     }
 
 }

ModelCatalogueCorePluginTestApp/grails-app/services/org/modelcatalogue/core/reports/RegisterReportsService.groovy
@@ -36,17 +36,17 @@ class RegisterReportsService {
             link controller: 'dataModel', action: 'inventorySpreadsheet', id: true
         }
 
-//        reportsRegistry.register {
-//            creates asset
-//            title { "Grid Report Spreadsheet" }
-//            defaultName { "${it.name} report as MS Excel Document Grid" }
-//            depth 3
-//            type DataModel
-//            when { DataModel dataModel ->
-//                dataModel.countDeclares() > 0
-//            }
-//            link controller: 'dataModel', action: 'gridSpreadsheet', id: true
-//        }
+        reportsRegistry.register {
+            creates asset
+            title { "Grid Report Spreadsheet" }
+            defaultName { "${it.name} report as MS Excel Document Grid" }
+            depth 3
+            type DataModel
+            when { DataModel dataModel ->
+                dataModel.countDeclares() > 0
+            }
+            link controller: 'dataModel', action: 'gridSpreadsheet', id: true
+        }
 
         reportsRegistry.register {
             creates asset
@@ -75,6 +75,22 @@ class RegisterReportsService {
                 link controller: 'northThames', action: 'northThamesSummaryReport', id: true, params: [organization: name]
             }
         }
+
+
+        reportsRegistry.register {
+            creates asset
+            title { "North Thames Mapping Report Document" }
+            defaultName { "North Thames Mapping Report Document" }
+            depth 3
+            type DataModel
+            when { DataModel dataModel ->
+                dataModel.name == "GEL LDPC Codes"
+            }
+            link controller: 'northThames', action: 'northThamesMappingReport', id: true
+        }
+
+
+
 
 
         reportsRegistry.register {

ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/dataexport/excel/norththamesreport/NorthThamesMappingReportXlsxExporter.groovy
@@ -1,0 +1,354 @@
+package org.modelcatalogue.core.dataexport.excel.norththamesreport
+
+import org.codehaus.groovy.grails.commons.GrailsApplication
+import org.hibernate.SessionFactory
+import org.modelcatalogue.core.*
+import org.modelcatalogue.core.dataexport.excel.gmcgridreport.GMCGridReportHeaders
+import org.modelcatalogue.core.export.inventory.ModelCatalogueStyles
+import org.modelcatalogue.core.util.lists.ListWithTotalAndType
+import org.modelcatalogue.gel.export.GridReportXlsxExporter
+import org.modelcatalogue.spreadsheet.builder.api.RowDefinition
+import org.modelcatalogue.spreadsheet.builder.api.SheetDefinition
+import org.modelcatalogue.spreadsheet.builder.api.SpreadsheetBuilder
+import org.modelcatalogue.spreadsheet.builder.poi.PoiSpreadsheetBuilder
+
+import static org.modelcatalogue.core.export.inventory.ModelCatalogueStyles.H1
+
+/**
+ * GridReportXlsxExporter.groovy
+ * Generates a report from a source data model which, as in GridReportXlsxExporter, shows its data classes in a hierarchy and
+ * lists its data elements; furthermore, some of the data elements are linked to a representative placeholder in GMC data models
+ * which represent data sources of particular hospitals in a GMC; and the placeholders have associated metadata;
+ * the placeholders and their metadata are listed with the data elements.
+ *
+ * Excel headers format v0.2. At the moment based on UCL data
+ * @author Adam Milward
+ * @version 31/03/2017
+ */
+class NorthThamesMappingReportXlsxExporter {
+
+    /**
+     * Map of data source systems
+     */
+    final DataModel sourceModel
+    final DataClassService dataClassService
+    final DataElementService dataElementService
+    final GrailsApplication grailsApplication
+    final List<DataModel> mappingModels
+    final Boolean mySQL
+
+    List<CatalogueElement> mappedSourceElements = []
+    List<CatalogueElement> unmappedSourceElements = []
+
+
+    protected List<String> excelHeaders = ['LOCAL SITE', 'LOCAL CODESET 1', 'LOCAL CODE 1', 'LOCAL CODE 1 NAME', "LOCAL CODESET 2",	"LOCAL CODE 2",	"LOCAL CODE 2 DESCRIPTION",	"LOINC CODE",	"LOINC CODE DESCRIPTION",	"LOINC SYSTEM(SPECIMEN)",	"GEL CODE",	"GEL CODE DESCRIPTION",	"OPENEHR QUERY"]
+
+
+
+    /**
+     * The report is triggered from a DataModel (element), and is on the
+     * location of data elements specified by that DataModel in the given 'organization'.
+     * @param element
+     * @param dataClassService
+     * @param grailsApplication
+     * @return
+     */
+    static NorthThamesMappingReportXlsxExporter create(DataModel sourceModel, DataClassService dataClassService, DataElementService dataElementService, GrailsApplication grailsApplication, Boolean mySQL) {
+        return new NorthThamesMappingReportXlsxExporter(sourceModel, dataClassService, dataElementService, grailsApplication, mySQL)
+    }
+
+
+    NorthThamesMappingReportXlsxExporter(DataModel sourceModel, DataClassService dataClassService, DataElementService dataElementService, GrailsApplication grailsApplication, Boolean mySQL) {
+        this.sourceModel = sourceModel
+        this.dataClassService = dataClassService
+        this.grailsApplication = grailsApplication
+        this.mappingModels = mappingModels
+        this.dataElementService = dataElementService
+        this.mySQL = mySQL
+    }
+
+
+    // export will do as it normally does, with new headers, and a different printDataElement, and then do sheetsAfterMainSheetExport at the end.
+
+    static String noSourceMessage = 'No source identified'
+    static String multipleSourcesMessage = 'Multiple sources identified, please see entries in the online catalogue'
+    static String oneSourceNoMetadataMessage = 'Source identified, metadata not recorded'
+
+
+    void export(OutputStream outputStream) {
+        SpreadsheetBuilder builder = new PoiSpreadsheetBuilder()
+        getMappedDataElements()
+
+        builder.build(outputStream) {
+            apply ModelCatalogueStyles
+            sheet("Mapped Elements") { SheetDefinition sheetDefinition ->
+                row {
+                    excelHeaders.each { header ->
+                        cell {
+                            value header
+                            width auto
+                            style H1
+                        }
+                    }
+                }
+
+                mappedSourceElements.each { DataElement de ->
+                    printMapping(de, sheetDefinition)
+                }
+            }
+        }
+    }
+
+
+
+
+
+    List<DataElement> getMappedDataElements(){
+        //get all the data elements from the model
+
+        if(mySQL) {
+            //populate the mapped elements list with the ones that have relationships
+            // and the unmapped element list for the ones that don't have any
+            dataElementService.findAllDataElementsInModel([:], sourceModel).getItems().each{ de ->
+                if(de.relatedTo.size()>0){
+                    mappedSourceElements.add(de)
+                }else{
+                    unmappedSourceElements.add(de)
+                }
+            }
+        }else{
+            //populate the mapped elements list with the ones that have relationships
+            // and the unmapped element list for the ones that don't have any
+            DataElement.findAllByDataModel(sourceModel).each{ de ->
+                if(de.relatedTo.size()>0){
+                    mappedSourceElements.add(de)
+                }else{
+                    unmappedSourceElements.add(de)
+                }
+            }
+        }
+
+
+    }
+
+    void printMapping(DataElement sourceDE, SheetDefinition sheet){
+
+        //print a row with all the mappings form the source models to the mapped models
+
+        //get the mapped items from the source dataelement
+        List<CatalogueElement> mappedElements = sourceDE.relatedTo
+
+        //def local1
+        //localCodeset1 is the source model
+        CatalogueElement local2
+        CatalogueElement loinc
+        CatalogueElement gel
+        CatalogueElement openEHR
+
+        mappedElements.each { CatalogueElement ce ->
+
+            String modelName = ce.dataModel.name
+
+            //need to make this more generic
+            switch (modelName) {
+//                case "localCodeset1":
+//                    local1 = ce
+//                    break
+                case "WinPath":
+                    local2 = ce
+                    break
+                case "LOINC":
+                    loinc = ce
+                    break
+                case "Rare Diseases":
+                    gel = ce
+                    break
+            }
+        }
+
+
+
+            sheet.with { SheetDefinition sheetDefinition ->
+                row {
+                    cell {
+                        value "RFH"
+                        width auto
+                        style {
+                            wrap text
+                            border left, {
+                                color black
+                                style medium
+                            }
+                        }
+                    }
+                    cell {
+                        value  "${(sourceDE)?sourceDE.dataModel.name:''}"
+                        width auto
+                        style {
+                            wrap text
+                            border left, {
+                                color black
+                                style medium
+                            }
+                        }
+                    }
+                    cell {
+                        value "${(sourceDE)?sourceDE.ext.get("Index"):''}"
+                        width auto
+                        style {
+                            wrap text
+                            border left, {
+                                color black
+                                style medium
+                            }
+                        }
+                    }
+                    cell {
+                        value sourceDE?.name
+                        width auto
+                        style {
+                            wrap text
+                            border left, {
+                                color black
+                                style medium
+                            }
+                        }
+                    }
+
+                    //mapped items
+
+                    cell {
+                        value  "${(local2)?local2.dataModel.name:''}"
+                        width auto
+                        style {
+                            wrap text
+                            border left, {
+                                color black
+                                style medium
+                            }
+                        }
+                    }
+
+                    cell {
+                        value  "${(local2)?local2.ext.get("WinPath TFC"):''}"
+                        width auto
+                        style {
+                            wrap text
+                            border left, {
+                                color black
+                                style medium
+                            }
+                        }
+                    }
+                    cell {
+                        value local2?.name
+                        width auto
+                        style {
+                            wrap text
+                            border left, {
+                                color black
+                                style medium
+                            }
+                        }
+                    }
+
+                    //mapped items loinc
+
+                    cell {
+                        value "${(loinc)?loinc.ext.get("LOINC_NUM"):''}"
+                        width auto
+                        style {
+                            wrap text
+                            border left, {
+                                color black
+                                style medium
+                            }
+                        }
+                    }
+
+                    cell {
+                        value "${(loinc)?loinc.ext.get("SYSTEM"):''}"
+                        width auto
+                        style {
+                            wrap text
+                            border left, {
+                                color black
+                                style medium
+                            }
+                        }
+                    }
+                    cell {
+                        value loinc?.name
+                        width auto
+                        style {
+                            wrap text
+                            border left, {
+                                color black
+                                style medium
+                            }
+                        }
+                    }
+
+                    //gel items
+
+                    cell {
+                        value "${(gel)? (gel.modelCatalogueId)?: (gel.latestVersionId)?:gel.id : ''}"
+                        width auto
+                        style {
+                            wrap text
+                            border left, {
+                                color black
+                                style medium
+                            }
+                        }
+                    }
+                    cell {
+                        value gel?.name
+                        width auto
+                        style {
+                            wrap text
+                            border left, {
+                                color black
+                                style medium
+                            }
+                        }
+                    }
+
+
+                    cell {
+                        value   "${(gel)?getOpenEHR(gel):''}"
+                        width auto
+                        style {
+                            wrap text
+                            border left, {
+                                color black
+                                style medium
+                            }
+                        }
+                    }
+
+                }
+            }
+
+        }
+
+    String getOpenEHR(CatalogueElement ce){
+
+        List<CatalogueElement> mappedElements = ce.relatedTo
+        String openQuery = ''
+
+        mappedElements.each { CatalogueElement me ->
+            if(me.dataModel.name=="Open EHR"){
+                openQuery =  me.ext.get("Archetype Path Query Statement")
+            }
+        }
+        return openQuery
+
+    }
+
+
+
+
+
+
+}

ModelCatalogueCorePluginTestApp/test/integration/org/modelcatalogue/core/dataimport/excel/norththamesreport/NorthThamesMappingReportXlsxExporterSpec.groovy
@@ -1,0 +1,148 @@
+package org.modelcatalogue.core.dataimport.excel.norththamesreport
+
+import org.codehaus.groovy.grails.commons.GrailsApplication
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import org.modelcatalogue.builder.api.CatalogueBuilder
+import org.modelcatalogue.core.*
+import org.modelcatalogue.core.dataexport.excel.gmcgridreport.GMCGridReportXlsxExporter
+import org.modelcatalogue.core.dataexport.excel.norththamesreport.NorthThamesMappingReportXlsxExporter
+import org.modelcatalogue.core.export.inventory.DataModelToXlsxExporterSpec
+import org.modelcatalogue.core.util.builder.DefaultCatalogueBuilder
+import org.modelcatalogue.core.util.test.FileOpener
+import org.modelcatalogue.integration.xml.CatalogueXmlLoader
+import org.modelcatalogue.spreadsheet.query.api.SpreadsheetCriteria
+import org.modelcatalogue.spreadsheet.query.poi.PoiSpreadsheetQuery
+import spock.lang.IgnoreIf
+
+@IgnoreIf( { System.getProperty('spock.ignore.slow')|| System.getenv('JENKINS_IGNORE') })
+class NorthThamesMappingReportXlsxExporterSpec extends AbstractIntegrationSpec {
+
+    public static final String ROOT_DATA_MODEL_NAME = 'North Thames Mapping Report Data Model'
+    ElementService elementService
+    DataModelService dataModelService
+    DataClassService dataClassService
+    DataElementService dataElementService
+    GrailsApplication grailsApplication
+    @Rule TemporaryFolder temporaryFolder = new TemporaryFolder()
+
+
+    DataModel dataModel
+    CatalogueBuilder catalogueBuilder
+
+    def setup() {
+
+        initRelationshipTypes()
+
+
+//        LOCAL SITE	LOCAL CODESET 1	LOCAL CODE 1	LOCAL CODE 1 NAME	LOCAL CODESET 2	LOCAL CODE 2	LOCAL CODE 2 DESCRIPTION	LOINC CODE	LOINC CODE DESCRIPTION	LOINC SPECIMEN TYPE	GEL CODE	GEL CODE DESCRIPTION	OPENEHR QUERY
+//        RFH	LDPC	1844		XXX	1234		49765-1		Bld	34983@1.4.0	blah 	asdfs
+//        RFH	LDPC	1410		XXX	532112		26464-8		Bld	35043@1.4.0	blahb	asdfadsf
+
+
+        catalogueBuilder.build {
+            automatic dataType
+
+            dataModel(name: 'GEL LDPC Codes') {
+
+                ext 'http://www.modelcatalogue.org/metadata/#organization', 'RFH'
+
+                dataElement(name: 'localCodeset1.Test Element 1', description: 'blah blah') {
+                    ext  "Index", "A18379"
+                    dataType(name: 'Same Name')
+                }
+                dataElement(name: 'localCodeset1.Test Element 2', description: 'blah blah') {
+                    ext  "Index", "A18379"
+                    dataType(name: 'Same Name')
+                }
+            }
+
+            dataModel(name: 'WinPath') {
+                copy relationships
+                ext 'http://www.modelcatalogue.org/metadata/#organization', 'RFH'
+
+                dataElement(name: 'localCodeset2.Test Element 1', description: 'blah blah') {
+                    ext  "WinPath TFC", "B36246233"
+                    dataType(name: 'Same Name')
+                    rel 'relatedTo' to 'GEL LDPC Codes','localCodeset1.Test Element 1'
+                }
+                dataElement(name: 'localCodeset2.Test Element 2', description: 'blah blah') {
+                    ext  "WinPath TFC", "B1234"
+                    dataType(name: 'Same Name')
+                    rel 'relatedTo' to 'GEL LDPC Codes','localCodeset1.Test Element 2'
+                }
+            }
+
+            dataModel(name: 'loinc') {
+                copy relationships
+                ext 'http://www.modelcatalogue.org/metadata/#organization', 'LOINC'
+
+                dataElement(name: 'loinc.Test Element 1', description: 'blah blah') {
+                    ext  "LOINC_NUM", "1234-1"
+                    ext  "SYSTEM", "1234-1"
+                    dataType(name: 'Urine')
+                    rel 'relatedTo' to 'GEL LDPC Codes','localCodeset1.Test Element 1'
+                }
+                dataElement(name: 'loinc.Test Element 2', description: 'blah blah') {
+                    ext  "LOINC_NUM", "98234-1"
+                    ext  "SYSTEM", "Body fld"
+                    dataType(name: 'Same Name')
+                    rel 'relatedTo' to 'GEL LDPC Codes','localCodeset1.Test Element 2'
+                }
+            }
+
+
+            dataModel(name: 'Rare Diseases') {
+                copy relationships
+                ext 'http://www.modelcatalogue.org/metadata/#organization', 'GEL'
+
+                dataElement(name: 'genomics.Test Element 1', description: 'blah blah') {
+                    ext  "code", "B36246233"
+                    dataType(name: 'Same Name')
+                    rel 'relatedTo' to 'GEL LDPC Codes','localCodeset1.Test Element 1'
+                }
+                dataElement(name: 'genomics.Test Element 2', description: 'blah blah') {
+                    ext  "code", "B1234"
+                    dataType(name: 'Same Name')
+                    rel 'relatedTo' to 'GEL LDPC Codes','localCodeset1.Test Element 2'
+                }
+            }
+
+
+            dataModel(name: 'Open EHR') {
+                copy relationships
+                ext 'http://www.modelcatalogue.org/metadata/#organization', 'Open EHR'
+
+                dataElement(name: 'genomics.Test Element 1', description: 'blah blah') {
+                    ext  "Archetype Path Query Statement", "select * from adsflkjdasafd"
+                    dataType(name: 'Same Name')
+                    rel 'relatedTo' to 'GEL LDPC Codes','localCodeset1.Test Element 1'
+                }
+                dataElement(name: 'genomics.Test Element 2', description: 'blah blah') {
+                    ext  "Archetype Path Query Statement", "select * from adsflkjdasafd"
+                    dataType(name: 'Same Name')
+                    rel 'relatedTo' to 'GEL LDPC Codes','localCodeset1.Test Element 2'
+                }
+            }
+
+            }
+
+        }
+
+
+    @IgnoreIf( { System.getenv('JENKINS_IGNORE') })
+    def "export model to excel"() {
+        setup:
+        def file = temporaryFolder.newFile("${System.currentTimeMillis()}.xlsx")
+        dataModel = DataModel.findByName("GEL LDPC Codes")
+
+        when:
+        NorthThamesMappingReportXlsxExporter.create(dataModel, dataClassService, dataElementService, grailsApplication, false).export(file.newOutputStream())
+        FileOpener.open(file)
+
+//        SpreadsheetCriteria query = PoiSpreadsheetQuery.FACTORY.forFile(file)
+
+        then:
+        noExceptionThrown()
+    }
+}

commit f7eb8ecb5f14698b74355fbfc4c13c8a31335841

correct ModelCatalogueNorthThamesUrlMappings
Commit https://github.com/MetadataConsulting/ModelCataloguePlugin/commit/52e1865f57d1c6177d6dd5b792214f6b73b2dc3f broke a unit test.

This commit fixes it by:

Point   /api/modelCatalogue/core/northThames/northThamesMappingReport/$id” to action northThamesMappingReport

Leave "/api/modelCatalogue/core/northThames/northThamesSummaryReport/$id" pointing to action: 'northThamesSummaryReport’

ModelCatalogueCorePluginTestApp/grails-app/conf/ModelCatalogueNorthThamesUrlMappings.groovy
@@ -4,7 +4,7 @@ class ModelCatalogueNorthThamesUrlMappings {
 
     static mappings = {
         "/api/modelCatalogue/core/northThames/northThamesSummaryReport/$id" (controller: 'northThames', action: 'northThamesSummaryReport', method: HttpMethod.GET)
-        "/api/modelCatalogue/core/northThames/northThamesSummaryReport/$id" (controller: 'northThames', action: 'northThamesMappingReport', method: HttpMethod.GET)
+        "/api/modelCatalogue/core/northThames/northThamesMappingReport/$id" (controller: 'northThames', action: 'northThamesMappingReport', method: HttpMethod.GET)
     }
 
 }

ModelCatalogueCorePluginTestApp/test/unit/ModelCatalogueNorthThamesUrlMappingsSpec.groovy
@@ -7,8 +7,9 @@ import spock.lang.Specification
 @Mock(NorthThamesController)
 class ModelCatalogueNorthThamesUrlMappingsSpec extends Specification {
 
-    void "test UserUrlMappings GET Request mappings"() {
+    void "test ModelCatalogueNorthThamesUrlMappings GET Request mappings"() {
         expect:
         assertForwardUrlMapping('/api/modelCatalogue/core/northThames/northThamesSummaryReport/$id', controller: 'northThames', action: 'northThamesSummaryReport')
+        assertForwardUrlMapping('/api/modelCatalogue/core/northThames/northThamesMappingReport/$id', controller: 'northThames', action: 'northThamesMappingReport')
     }
 }