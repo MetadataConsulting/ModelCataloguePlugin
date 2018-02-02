594fbb3a56bb8cf80551a8213386b4a1415f56b9
johncross <johnatcross@gmail.com>
2018-02-02 17:22:06 +0000

    Removed the superfluous import types from the menu and data import controller

--- ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/dataimport/excel/ExcelImportType.groovy
+++ ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/dataimport/excel/ExcelImportType.groovy
@@ -8,8 +8,8 @@ enum ExcelImportType {
 //    LOINC('LOINC'),
     GOSH_LAB_TEST_CODES('GOSH Lab Test Codes'),
     NORTH_THAMES_DATA_SOURCE_MAPPING('North Thames Data Source Mapping'),
-    REIMPORT_FROM_EXCEL_EXPORTER('Re-import from Excel Exporter'),
-    STANDARD('Standard')
+//    REIMPORT_FROM_EXCEL_EXPORTER('Re-import from Excel Exporter'),
+//    STANDARD('Standard')
 
     final String humanReadableName
 
