5604673f189ac0f5d88c835a367b97e72fb7709e
Vijay Shukla <vshukla684@gmail.com>
2018-03-08 11:28:58 +0530

    Complete 1478 and 1654

---
+++ ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/regression/nexthoughts/MET1654Spec.groovy
@@ -0,0 +1,36 @@
+package org.modelcatalogue.core.regression.nexthoughts
+
+import org.modelcatalogue.core.geb.Common
+
+import static org.modelcatalogue.core.geb.Common.*
+import org.modelcatalogue.core.geb.AbstractModelCatalogueGebSpec
+import org.modelcatalogue.core.geb.CatalogueAction
+import spock.lang.Stepwise
+import spock.lang.Ignore
+
+@Stepwise
+//@Ignore
+class MET1654Spec extends AbstractModelCatalogueGebSpec {
+    private static final myModel = "#my-models"
+    static
+    final String addItemIcon = "div.inf-table-body>table>tfoot>tr>td>table>tfoot>tr>td.text-center>span.fa-plus-circle"
+
+    def "Login to Model Catalouge"() {
+
+        when: "Login using Curator Account"
+        loginCurator()
+
+        then: "My Modal Should be displayed"
+        check myModel displayed
+    }
+
+    def "Select Admin Data Model"() {
+        when:
+        select 'Cancer Model'
+        selectInTree 'Data Types'
+
+        then:
+        check Common.rightSideTitle contains 'Active Data Types'
+        $(addItemIcon).displayed == false
+    }
+}
