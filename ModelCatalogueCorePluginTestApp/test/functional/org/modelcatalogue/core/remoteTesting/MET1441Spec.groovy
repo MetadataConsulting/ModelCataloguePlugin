ce66b4ddbbdf63c90baa929a5d7594511c6ffd2b
David Milward <david@beenleigh.com>
2018-03-13 09:33:15 +0000

    Added various test files MET1433-MET1654

---
+++ ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/remoteTesting/MET1441Spec.groovy
@@ -0,0 +1,137 @@
+package org.modelcatalogue.core.regression.nexthoughts
+
+import org.modelcatalogue.core.geb.Common
+
+import static org.modelcatalogue.core.geb.Common.*
+import org.modelcatalogue.core.geb.AbstractModelCatalogueGebSpec
+import org.modelcatalogue.core.geb.CatalogueAction
+import spock.lang.Stepwise
+import spock.lang.Ignore
+
+@Stepwise
+@Ignore
+class MET1441Spec extends AbstractModelCatalogueGebSpec {
+    private static final myModel = "#my-models"
+    private static final CatalogueAction create = CatalogueAction.runFirst('data-models', 'create-data-model')
+    private static final String name = "input#name"
+    private static final String policies = "input#dataModelPolicy"
+    private static final String finishButton = "button#step-finish"
+    private static final long TIME_TO_REFRESH_SEARCH_RESULTS = 5000L
+    private static final String uuid = UUID.randomUUID().toString()
+    private static final String modelHeaderName = 'h3.ce-name'
+    private static final String dataModelMenuButton = 'a#role_item_catalogue-element-menu-item-link'
+    private static final String finalize = 'a#finalize-menu-item-link'
+    private static final String deleteMenu = 'a#delete-menu-item-link'
+    private static final String versionNote = 'textarea#revisionNotes'
+    private static final String finalizeButton = 'a#role_modal_modal-finalize-data-modalBtn'
+    private static final String table = 'tbody.ng-scope>tr:nth-child(1)>td:nth-child(4)'
+    private static final String metadataStep = "button#step-metadata"
+    private static final String wizardSummary = 'td.col-md-4'
+    private static final String exitButton = 'button#exit-wizard'
+    private static final String modelInTree = 'ul.catalogue-element-treeview-list-root>li>div>span>span'
+
+    def "Login to Model Catalouge"() {
+
+        when: "Login using Curator Account"
+        login curator
+
+        then: "My Modal Should be displayed"
+        check myModel displayed
+        check create displayed
+    }
+
+    def "Create a Data model using Create button"() {
+        when: "Click on the create button"
+        click create
+
+        then: "Data model popup should open"
+        check modalHeader contains "Data Model Wizard"
+    }
+
+    def "Click on the green button to complete the Data Model Creation"() {
+        when: "Clicked on Green Button"
+        fill name with uuid
+        Thread.sleep(TIME_TO_REFRESH_SEARCH_RESULTS)
+        check finishButton displayed
+        click finishButton
+        Thread.sleep(TIME_TO_REFRESH_SEARCH_RESULTS)
+
+        then: "Summary Should display New Data Model Creation Message"
+        check '#summary' displayed
+        println $("#summary").text()
+        check '#summary' contains uuid
+    }
+
+    def "When close the data model then new created model should open"() {
+        when: "Click on the close button of model"
+
+        click Common.modalCloseButton
+
+        Thread.sleep(TIME_TO_REFRESH_SEARCH_RESULTS)
+
+        then: "New Created Data Model Page should open"
+        check modelHeaderName displayed
+        check modelHeaderName contains uuid
+    }
+
+    def "Add Data Classes "() {
+        when:
+        selectInTree 'Data Classes'
+
+        then:
+        check Common.rightSideTitle contains 'Active Data Classes'
+    }
+
+    def "Navigate to Create data classes page"() {
+        when:
+        click Common.create
+
+        then:
+        check Common.modalHeader contains "Data Class Wizard"
+    }
+
+    def "Create a Data Class and select the created data class"() {
+        when: ' fill data class step'
+        fill Common.nameLabel with "NEW_TESTING_MODEL "
+        fill Common.modelCatalogueId with "${UUID.randomUUID()}"
+        fill Common.description with 'THIS IS MY DATA CLASS'
+
+        then:
+        $(metadataStep).isDisplayed()
+
+        when: 'click green button'
+        click finishButton
+        Thread.sleep(TIME_TO_REFRESH_SEARCH_RESULTS)
+        click exitButton
+        Thread.sleep(TIME_TO_REFRESH_SEARCH_RESULTS)
+
+        then:
+        check wizardSummary contains "NEW_TESTING_MODEL"
+    }
+
+    def "Finalized the Data Model"() {
+        when: "Click on the Main Menu and finalized data model"
+        click modelInTree
+        click dataModelMenuButton
+        click finalize
+
+        and:
+        fill versionNote with 'THIS IS THE VERSION NOTE'
+        click finalizeButton
+        Thread.sleep(TIME_TO_REFRESH_SEARCH_RESULTS)
+        click Common.modalPrimaryButton
+
+        then:
+        check table contains "$uuid (0.0.1) finalized"
+    }
+
+    def "Check Delete Button must be disabled"() {
+        when: "Click on the Main Menu"
+//        search uuid
+        click modelInTree
+        click dataModelMenuButton
+
+        then: "No Option for the delete"
+        $(deleteMenu).displayed == false
+    }
+}
---
+++ ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/remoteTesting/MET1469Spec.groovy
@@ -0,0 +1,204 @@
+package org.modelcatalogue.core.regression.nexthoughts
+
+import org.modelcatalogue.core.geb.Common
+
+import static org.modelcatalogue.core.geb.Common.*
+import org.modelcatalogue.core.geb.AbstractModelCatalogueGebSpec
+import org.modelcatalogue.core.geb.CatalogueAction
+import spock.lang.Stepwise
+import spock.lang.Ignore
+
+@Stepwise
+//@Ignore
+class MET1469Spec extends AbstractModelCatalogueGebSpec {
+    private static final myModel = "#my-models"
+    private static final String modelHeaderName = 'h3.ce-name'
+    private static final String metadataStep = "button#step-metadata"
+    private static final String label = "textarea#section-label"
+    private static final String section_title = "textarea#section-title"
+    private static final String instruction = "textarea#section-instructions"
+    private static final String page_number = 'input#form-page-number'
+    private static final String occurrence = 'ul.nav-pills>li:nth-child(3)>a'
+    private static final String finishButton = "button#step-finish"
+    private static final String appearance = 'ul.nav-pills>li:nth-child(4)>a'
+    private static final String name = "input#local-name"
+    private static final String elementStep = "button#step-elements"
+    private static final String dataElement = "input#data-element"
+    private static final String plusButton = "span.input-group-btn>button"
+    private static final String raw = "ul.nav-pills>li:nth-child(4)>a"
+    private static final String wizardSummary = 'td.col-md-4'
+    private static final String parentStep = "button#step-parents"
+    private static final String formSection = 'ul.nav-pills>li:nth-child(1)>a'
+    private static final long TIME_TO_REFRESH_SEARCH_RESULTS = 4000L
+    private static final String exitButton = 'button#exit-wizard'
+    private static final String nameLabel = "NEW_TESTING_MODEL"
+    private static final String saveElement = "a#role_modal_modal-save-elementBtn"
+    private static final String tagElement = "tbody.ng-scope>tr:nth-child(1)>td:nth-child(2)"
+    private static final String search = "input#dataType"
+
+    static String myName = " testing data element "
+    static String myCatalogue = UUID.randomUUID().toString()
+    static String myDescription = "This a test element"
+    static String tagName = "myTag"
+
+    def "Login to Model Catalouge"() {
+
+        when: "Login using Curator Account"
+        login curator
+
+        then: "My Modal Should be displayed"
+        check myModel displayed
+    }
+
+    def "Select a finalized Data Model"() {
+        String dataModel = "Test 1"
+        when: "Selected an Finalized Data Model"
+        select dataModel
+
+        then: "Data Model Page Should Open"
+        check modelHeaderName displayed
+        check modelHeaderName contains dataModel
+    }
+
+    def "login and navigate to the model "() {
+        when:
+        selectInTree 'Data Classes'
+
+        then:
+        check Common.rightSideTitle contains 'Active Data Classes'
+    }
+
+    def "Navigate to Create data classes page"() {
+        when:
+        click Common.create
+
+        then:
+        check Common.modalHeader contains "Data Class Wizard"
+    }
+
+    def "Create a Data Class and select the created data class"() {
+        when: ' fill data class step'
+        fill Common.nameLabel with nameLabel
+        fill Common.modelCatalogueId with "${UUID.randomUUID()}"
+        fill Common.description with 'THIS IS MY DATA CLASS'
+
+        then:
+        $(metadataStep).isDisplayed()
+
+        when: 'fill metadata step'
+        click metadataStep
+        fillMetadata foo: 'one', bar: 'two', baz: 'three', fubor: 'four'
+
+        and: 'click on parent button'
+        click parentStep
+
+        then:
+        check finishButton displayed
+
+        when: 'fill parent step'
+        click formSection
+        fill label with 'TEST_LABEL'
+        fill section_title with 'MY_TITLE'
+        fill instruction with 'this is my instruction'
+        fill page_number with '1'
+
+        and: 'click on occurrence '
+        click occurrence
+
+        then:
+        check finishButton displayed
+
+        when:
+        fillMetadata 'Min Occurs': '1', 'Max Occurs': '10'
+        // click on appearance
+        click appearance
+        fill name with ' this is my name'
+
+        then:
+        check elementStep displayed
+
+        when: 'fill Element'
+        click elementStep
+        fill dataElement with 'TEST_ELEMENT'
+        click plusButton
+        click raw
+
+        and:
+        click Common.modalSuccessButton
+        fillMetadata foo: 'five'
+
+        and: 'click green button'
+        click finishButton
+        Thread.sleep(TIME_TO_REFRESH_SEARCH_RESULTS)
+        click exitButton
+        Thread.sleep(TIME_TO_REFRESH_SEARCH_RESULTS)
+
+        then:
+        check wizardSummary contains "NEW_TESTING_MODEL"
+    }
+
+    def "Create Data Element"() {
+        when:
+        selectInTree 'Data Elements'
+
+        then:
+        check Common.rightSideTitle is 'Active Data Elements'
+    }
+
+    def "navigate to data element creation page"() {
+        when:
+        click Common.create
+        then:
+        check Common.modalHeader contains 'Create Data Element'
+    }
+
+    def "fill the create data element form"() {
+        when:
+        fill Common.nameLabel with myName
+
+        fill Common.modelCatalogueId with myCatalogue
+
+        fill Common.description with myDescription
+
+        and: 'select a data type'
+
+        fill search with 'boolean'
+        Thread.sleep(2000l)
+
+        and: 'click on the save button'
+        println("////////////////////111111111111/////////////////")
+        click saveElement
+
+        then: 'verify that data is created'
+        check wizardSummary contains 'testing data element'
+    }
+
+    def "create tag"() {
+        when:
+        selectInTree 'Tags'
+
+        then:
+        check Common.rightSideTitle is 'Active Tags'
+    }
+
+
+    def "navigate to tag creation page"() {
+        when:
+        click Common.create
+        then:
+        check Common.modalHeader contains 'Create Tag'
+    }
+
+    def "fill tag form"() {
+        when:
+        fill Common.nameLabel with tagName
+        fill Common.description with myDescription
+        click saveElement
+        Thread.sleep(TIME_TO_REFRESH_SEARCH_RESULTS)
+        selectInTree 'Tags'
+
+        then:
+        check Common.rightSideTitle is 'Active Tags'
+        check tagElement is tagName
+    }
+}
