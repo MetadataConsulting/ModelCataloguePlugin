f152d2c187e72d2435f0e54a549138b1312790b5
Vijay <vshukla684@gmail.com>
2018-03-15 16:42:48 +0530

    Download file

--- ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/regression/nexthoughts/MET1561Spec.groovy
+++ ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/regression/nexthoughts/MET1561Spec.groovy
@@ -7,17 +7,18 @@ import org.modelcatalogue.core.geb.AbstractModelCatalogueGebSpec
 import org.modelcatalogue.core.geb.CatalogueAction
 import spock.lang.Stepwise
 import spock.lang.Ignore
+import groovy.io.FileType
 
 @Stepwise
-@Ignore
+//@Ignore
 class MET1561Spec extends AbstractModelCatalogueGebSpec {
     private static final myModel = "#my-models"
     private static final String modelHeaderName = 'h3.ce-name'
     private static final String exportButton = "a#role_item_export-menu-item-link"
-    private static final String catalougeExportButton = "a#catalogue-element-export-specific-reports_4-menu-item-link"
+    private static final String catalogueExportButton = "a#catalogue-element-export-specific-reports_12-menu-item-link"
     public static final CatalogueAction exportAction = CatalogueAction.runFirst('item', 'export')
 
-    def "Login to Model Catalouge"() {
+    def "Login to Model Catalogue"() {
 
         when: "Login using Curator Account"
         login curator
@@ -36,7 +37,20 @@ class MET1561Spec extends AbstractModelCatalogueGebSpec {
     }
 
     def "Click on the export button"() {
-        click exportAction
-    }
+        when:
+        File file = new File("/home/vijay/")
+        new File("/home/vijay/Cancer_Model.mc.xml").delete()
 
+        click exportButton
+        click catalogueExportButton
+        Thread.sleep(5000)
+        List<String> files = []
+        file.eachFile(FileType.FILES) {
+            println(it.name)
+            files.add(it.name)
+        }
+
+        then:
+        files.contains("Cancer_Model.mc.xml")
+    }
 }
