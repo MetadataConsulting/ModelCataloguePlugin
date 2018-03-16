941cc75e93665a28540543c59ae1740fb24a9bcf
James Dai <james.dai.uk@googlemail.com>
2018-03-16 15:14:43 +0000

    semanticVersion in DataModel minimalCatalogueElementJSON

--- ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/util/marshalling/CatalogueElementMarshaller.groovy
+++ ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/util/marshalling/CatalogueElementMarshaller.groovy
@@ -262,6 +262,7 @@ abstract class CatalogueElementMarshaller extends AbstractMarshaller {
         if (!element) return null
         def minimalJSON = minimalCatalogueElementJSONSkeleton(element)
         minimalJSON.remove('dataModel')
+        minimalJSON.put('semanticVersion', element.semanticVersion)
         return minimalJSON
     }
 
