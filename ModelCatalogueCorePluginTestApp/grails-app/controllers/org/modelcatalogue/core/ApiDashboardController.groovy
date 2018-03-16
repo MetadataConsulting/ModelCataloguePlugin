36c17f75eb074fdc2fdf8ca05d0c7cd5749eb15f
Sergio del Amo <sergio.delamo@softamo.com>
2018-03-16 08:00:04 +0100

    send gormUrl back

--- ModelCatalogueCorePluginTestApp/grails-app/controllers/org/modelcatalogue/core/ApiDashboardController.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/controllers/org/modelcatalogue/core/ApiDashboardController.groovy
@@ -1,9 +1,13 @@
 package org.modelcatalogue.core
 
+import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
 import org.modelcatalogue.core.dashboard.DashboardService
 import org.modelcatalogue.core.dashboard.SearchStatusQuery
+import org.modelcatalogue.core.util.GormUrlName
+import org.modelcatalogue.core.util.IdName
 import org.modelcatalogue.core.util.MetadataDomain
+import org.modelcatalogue.core.util.MetadataDomainEntity
 import org.modelcatalogue.core.util.PaginationQuery
 import org.modelcatalogue.core.util.SortQuery
 
@@ -19,16 +23,38 @@ class ApiDashboardController {
         }
     }
 
+    @CompileDynamic
     def catalogueElements(Long dataModelId) {
+        List<GormUrlName> dataElementList = dashboardService.findAllBySearchStatusQuery(dataModelId, MetadataDomain.DATA_ELEMENT, null, null, null).collect {
+            new GormUrlName(dataModelId: dataModelId, gormUrl: MetadataDomainEntity.stringRepresentation(new MetadataDomainEntity(domain: MetadataDomain.DATA_ELEMENT, id: it.id)), name: it.name)
+        } as List<GormUrlName>
+        List<GormUrlName> dataClassList = dashboardService.findAllBySearchStatusQuery(dataModelId, MetadataDomain.DATA_CLASS, null, null, null).collect {
+            new GormUrlName(dataModelId: dataModelId, gormUrl: MetadataDomainEntity.stringRepresentation(new MetadataDomainEntity(domain: MetadataDomain.DATA_CLASS, id: it.id)), name: it.name)
+        } as List<GormUrlName>
+        List<GormUrlName> enumeratedTypeList = dashboardService.findAllBySearchStatusQuery(dataModelId, MetadataDomain.ENUMERATED_TYPE, null, null, null).collect {
+            new GormUrlName(dataModelId: dataModelId, gormUrl: MetadataDomainEntity.stringRepresentation(new MetadataDomainEntity(domain: MetadataDomain.ENUMERATED_TYPE, id: it.id)), name: it.name)
+        } as List<GormUrlName>
+        List<GormUrlName> dataTypeList = dashboardService.findAllBySearchStatusQuery(dataModelId, MetadataDomain.DATA_TYPE, null, null, null).collect {
+            new GormUrlName(dataModelId: dataModelId, gormUrl: MetadataDomainEntity.stringRepresentation(new MetadataDomainEntity(domain: MetadataDomain.DATA_TYPE, id: it.id)), name: it.name)
+        }
+        List<GormUrlName> measurementUnitList = dashboardService.findAllBySearchStatusQuery(dataModelId, MetadataDomain.MEASUREMENT_UNIT, null, null, null).collect {
+            new GormUrlName(dataModelId: dataModelId, gormUrl: MetadataDomainEntity.stringRepresentation(new MetadataDomainEntity(domain: MetadataDomain.MEASUREMENT_UNIT, id: it.id)), name: it.name)
+        } as List<GormUrlName>
+        List<GormUrlName> businessRuleList = dashboardService.findAllBySearchStatusQuery(dataModelId, MetadataDomain.BUSINESS_RULE, null, null, null).collect {
+            new GormUrlName(dataModelId: dataModelId, gormUrl: MetadataDomainEntity.stringRepresentation(new MetadataDomainEntity(domain: MetadataDomain.BUSINESS_RULE, id: it.id)), name: it.name)
+        } as List<GormUrlName>
+        List<GormUrlName> tagsList = dashboardService.findAllBySearchStatusQuery(dataModelId, MetadataDomain.TAG, null, null, null).collect {
+            new GormUrlName(dataModelId: dataModelId, gormUrl: MetadataDomainEntity.stringRepresentation(new MetadataDomainEntity(domain: MetadataDomain.TAG, id: it.id)), name: it.name)
+        } as List<GormUrlName>
         render(contentType: "application/json") {
             [
-                dataElements    : dashboardService.findAllBySearchStatusQuery(dataModelId, MetadataDomain.DATA_ELEMENT, null, null, null),
-                dataClasses     : dashboardService.findAllBySearchStatusQuery(dataModelId, MetadataDomain.DATA_CLASS, null, null, null),
-                enumeratedTypes : dashboardService.findAllBySearchStatusQuery(dataModelId, MetadataDomain.ENUMERATED_TYPE, null, null, null),
-                dataTypes       : dashboardService.findAllBySearchStatusQuery(dataModelId, MetadataDomain.DATA_TYPE, null, null, null),
-                measurementUnits: dashboardService.findAllBySearchStatusQuery(dataModelId, MetadataDomain.MEASUREMENT_UNIT, null, null, null),
-                businessRules   : dashboardService.findAllBySearchStatusQuery(dataModelId, MetadataDomain.BUSINESS_RULE, null, null, null),
-                tags            : dashboardService.findAllBySearchStatusQuery(dataModelId, MetadataDomain.TAG, null, null, null)
+                dataElements    : dataElementList,
+                dataClasses     : dataClassList,
+                enumeratedTypes : enumeratedTypeList,
+                dataTypes       : dataTypeList,
+                measurementUnits: measurementUnitList,
+                businessRules   : businessRuleList,
+                tags            : tagsList,
             ]
         }
     }
---
+++ ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/util/GormUrlName.groovy
@@ -0,0 +1,10 @@
+package org.modelcatalogue.core.util
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class GormUrlName {
+    String gormUrl
+    String name
+    Long dataModelId
+}
