0fb3b0d1d0b641e1cfda92fa6488f36638e916d0
James Dai <james.dai.uk@googlemail.com>
2018-03-20 17:20:34 +0000

    do hasRole('ROLE_SUPERVISOR') instead of hasRole('SUPERVISOR') due to changes in SS2SS

--- ModelCatalogueCorePluginTestApp/grails-app/controllers/org/modelcatalogue/core/AbstractCatalogueElementController.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/controllers/org/modelcatalogue/core/AbstractCatalogueElementController.groovy
@@ -381,7 +381,7 @@ abstract class AbstractCatalogueElementController<T extends CatalogueElement> ex
             return
         }
 
-        if (!modelCatalogueSecurityService.hasRole('SUPERVISOR') && instance.status.ordinal() >= ElementStatus.FINALIZED.ordinal()) {
+        if (!modelCatalogueSecurityService.hasRole('ROLE_SUPERVISOR') && instance.status.ordinal() >= ElementStatus.FINALIZED.ordinal()) {
             instance.errors.rejectValue 'status', 'cannot.modify.finalized.or.deprecated', 'Cannot modify element in finalized or deprecated state!'
             respond instance.errors, view: 'edit' // STATUS CODE 422
             return
