904d4506e3662dd57940931d5b95dec592eabee8
Sergio del Amo <sergio.delamo@softamo.com>
2018-03-30 14:07:42 +0200

    workaround issues with batch names

--- ModelCatalogueCorePluginTestApp/grails-app/controllers/org/modelcatalogue/core/actions/BatchController.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/controllers/org/modelcatalogue/core/actions/BatchController.groovy
@@ -2,12 +2,14 @@ package org.modelcatalogue.core.actions
 
 import groovy.transform.CompileDynamic
 import groovy.util.logging.Slf4j
-import org.codehaus.groovy.grails.commons.GrailsApplication
 import org.modelcatalogue.core.AbstractRestfulController
 import org.modelcatalogue.core.DataModel
 import org.modelcatalogue.core.dataarchitect.DataArchitectService
 import org.modelcatalogue.core.mappingsuggestions.MapppingSuggestionsConfigurationService
-import org.modelcatalogue.core.mappingsuggestions.MatchAgainst
+import org.modelcatalogue.core.mappingsuggestions.MappingSuggestionRequest
+import org.modelcatalogue.core.mappingsuggestions.MappingSuggestionRequestImpl
+import org.modelcatalogue.core.mappingsuggestions.MappingSuggestionResponse
+import org.modelcatalogue.core.mappingsuggestions.MappingsSuggestionsGateway
 import org.modelcatalogue.core.persistence.BatchGormService
 import org.modelcatalogue.core.persistence.DataModelGormService
 import org.modelcatalogue.core.util.IdName
@@ -28,6 +30,21 @@ class BatchController extends AbstractRestfulController<Batch> {
     MessageSource messageSource
     def executorService
     MapppingSuggestionsConfigurationService mapppingSuggestionsConfigurationService
+    int defaultMax
+    int defaultScore
+
+    MappingsSuggestionsGateway mappingsSuggestionsGateway
+
+    @CompileDynamic
+    @PostConstruct
+    void setup() {
+        defaultMax = grailsApplication.config.mdx.mappingsuggestions.max ?: 20
+        defaultScore = grailsApplication.config.mdx.mappingsuggestions.score ?: 20
+    }
+
+    protected List<ActionState> defaultActionStates() {
+        ActionState.values()  as List<ActionState>
+    }
 
     static allowedMethods = [
             all: 'GET',
@@ -50,6 +67,31 @@ class BatchController extends AbstractRestfulController<Batch> {
 
     def all() {
         List<BatchViewModel> batchList = batchService.findAllActive()
+
+        for ( BatchViewModel batch : batchList ) {
+            MappingSuggestionRequest mappingSuggestionRequest = new MappingSuggestionRequestImpl(
+                    batchId: batch.id,
+                    max: 1,
+                    offset: 0,
+                    scorePercentage: defaultScore,
+                    stateList: defaultActionStates(),
+                    term:  null
+            )
+            MappingSuggestionResponse rsp = mappingsSuggestionsGateway.findAll(mappingSuggestionRequest)
+            if ( rsp && rsp.sourceName && rsp.destinationName) {
+                String suffix = ''
+                if ( batch.name ) {
+                    if ( batch.name.contains('Fuzzy') ) {
+                        suffix = ' - Fuzzy'
+                    }
+                    if ( batch.name.contains('Exact Matches') ) {
+                        suffix = ' - Exact Matches'
+                    }
+                }
+                batch.name = "${rsp.sourceName} vs ${rsp.destinationName} ${suffix}"
+            }
+        }
+
         Number total = batchGormService.countActive()
         [
                 batchList: batchList,
