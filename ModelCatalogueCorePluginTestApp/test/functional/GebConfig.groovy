7df5cbe11125a3b49150f48bddaa5e6ebdb72064
Vijay <vshukla684@gmail.com>
2018-04-02 12:25:00 +0530

    Added Config files too.

--- ModelCatalogueCorePluginTestApp/test/functional/GebConfig.groovy
+++ ModelCatalogueCorePluginTestApp/test/functional/GebConfig.groovy
@@ -5,6 +5,8 @@
 */
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.remote.CapabilityType
 
 waiting {
     timeout = 2
@@ -15,7 +17,22 @@ environments {
     // run via “./gradlew chromeTest”
     // See: http://code.google.com/p/selenium/wiki/ChromeDriver
     chrome {
-        driver = { new ChromeDriver() }
+        String downloadFilepath = "/home/vijay/"
+        System.setProperty("downloadFilepath", downloadFilepath)
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("download.default_directory", downloadFilepath);
+
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", chromePrefs);
+        options.addArguments("--test-type");
+        options.addArguments("--disable-extensions"); //to disable browser extension popup
+
+        DesiredCapabilities cap = DesiredCapabilities.chrome();
+        cap.setCapability(ChromeOptions.CAPABILITY, options);
+        cap.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+
+        driver = { new ChromeDriver(cap) }
     }
 
     // run via “./gradlew chromeHeadlessTest”
@@ -30,6 +47,7 @@ environments {
 
 }
 
+
+
 baseNavigatorWaiting = true
 atCheckWaiting = true
-
