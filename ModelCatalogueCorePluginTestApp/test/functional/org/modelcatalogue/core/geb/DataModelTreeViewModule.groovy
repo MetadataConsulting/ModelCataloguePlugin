730f7b68695036a7f22f7093442d902ab5bc362f
Sergio del Amo <sergio.delamo@softamo.com>
2018-04-06 18:21:59 +0200

    Test WIP

--- ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/geb/DataModelTreeViewModule.groovy
+++ ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/geb/DataModelTreeViewModule.groovy
@@ -13,6 +13,42 @@ class DataModelTreeViewModule extends Module {
         dataModelLink.click()
     }
 
+    void dataClasses() {
+        select('Data Classes')
+    }
+
+    void dataElements() {
+        select('Data Elements')
+    }
+
+    void dataTypes() {
+        select('Data Types')
+    }
+
+    void measurementUnits() {
+        select('Measurement Units')
+    }
+
+    void businessRules() {
+        select('Business Rules')
+    }
+
+    void tags() {
+        select('Tags')
+    }
+
+    void deprecatedItems() {
+        select('Deprecated Items')
+    }
+
+    void importedDataModels() {
+        select('Imported Data Models')
+    }
+
+    void versions() {
+        select('Versions')
+    }
+
     void select(String name) {
         item(name).click()
         sleep(2_000)
