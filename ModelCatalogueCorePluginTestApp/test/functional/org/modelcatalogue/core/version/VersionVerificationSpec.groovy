ac258302178b69ae4f05a8c8ab22d9f9b4081efc
sdelamo <sergio.delamo@softamo.com>
2018-04-20 09:03:32 +0200

    Implement VersionVerificationSpec
    See: https://github.com/MetadataConsulting/ModelCataloguePlugin/issues/1202

--- ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/version/VersionVerificationSpec.groovy
+++ ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/version/VersionVerificationSpec.groovy
@@ -1,8 +1,12 @@
 package org.modelcatalogue.core.version
 
+import geb.spock.GebSpec
+import org.modelcatalogue.core.geb.CodeVersionPage
+import org.modelcatalogue.core.geb.DashboardPage
+import org.modelcatalogue.core.geb.LoginPage
 import spock.lang.Issue
 import spock.lang.Narrative
-import spock.lang.Specification
+import spock.lang.Stepwise
 import spock.lang.Title
 
 @Issue('https://metadata.atlassian.net/browse/MET-1925')
@@ -13,5 +17,40 @@ import spock.lang.Title
 - Scroll down and select the Code Version option | you are redirected to a new page/window with the version number
 - Check that the model catalogue carries the correct version
 ''')
-class VersionVerificationSpec extends Specification {
+@Stepwise
+class VersionVerificationSpec extends GebSpec {
+
+    def "Login as user"() {
+        when:
+        LoginPage loginPage = to LoginPage
+        loginPage.login('user', 'user')
+
+        then:
+        at DashboardPage
+    }
+
+    def "select settings"() {
+        when:
+        DashboardPage dashboardPage = browser.page DashboardPage
+        dashboardPage.nav.cogMenu()
+        then:
+        at DashboardPage
+    }
+
+    def "select code version"() {
+        when:
+        DashboardPage dashboardPage = browser.page DashboardPage
+        dashboardPage.nav.codeversion()
+        then:
+        at CodeVersionPage
+    }
+
+    def "check version"() {
+        when:
+        CodeVersionPage codeVersionPage = browser.page CodeVersionPage
+
+        then:
+        codeVersionPage.isGithubLinkDisplayed()
+    }
+
 }
