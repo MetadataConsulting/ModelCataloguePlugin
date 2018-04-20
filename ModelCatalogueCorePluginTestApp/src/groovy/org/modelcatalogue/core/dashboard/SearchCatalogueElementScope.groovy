552811f9547756b2f8e6fac8ff2cb8734026ab5f
sdelamo <sergio.delamo@softamo.com>
2018-04-20 14:22:23 +0200

    SearchCatalogueElementScope

---
+++ ModelCatalogueCorePluginTestApp/src/groovy/org/modelcatalogue/core/dashboard/SearchCatalogueElementScope.groovy
@@ -0,0 +1,13 @@
+package org.modelcatalogue.core.dashboard
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+enum SearchCatalogueElementScope {
+    ALL,
+    NAME,
+    MODELCATALOGUEID,
+    DESCRIPTION,
+    EXTENSIONNAME,
+    EXTENSIONVALUE
+}
