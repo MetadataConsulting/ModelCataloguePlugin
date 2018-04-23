770e812d58f9c6b8dc45bf4f99d1d83fda356ace
sdelamo <sergio.delamo@softamo.com>
2018-04-23 12:21:03 +0200

    remove .jquery-safari-checkbox qualifier

--- ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/geb/UserEditPage.groovy
+++ ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/geb/UserEditPage.groovy
@@ -13,11 +13,9 @@ class UserEditPage extends Page {
         rolesTabLink { $('div#tabs ul li a', text: "Roles") }
         updateButton { $('a#update') }
         deleteButton { $('a#deleteButton') }
-        roleUser { $('div a', text: "ROLE_USER").siblings("span.jquery-safari-checkbox") }
-        roleSupervisor { $('div a', text: "ROLE_SUPERVISOR").siblings("span.jquery-safari-checkbox") }
-        roleMetadataCurator(wait: true) {
-            $('div a', text: "ROLE_METADATA_CURATOR").siblings("span.jquery-safari-checkbox")
-        }
+        roleUser { $('div a', text: "ROLE_USER").siblings("span") }
+        roleSupervisor { $('div a', text: "ROLE_SUPERVISOR").siblings("span") }
+        roleMetadataCurator(wait: true) { $('div a', text: "ROLE_METADATA_CURATOR").siblings("span") }
         logoutLink { $('div#loginLinkContainer a') }
     }
 
