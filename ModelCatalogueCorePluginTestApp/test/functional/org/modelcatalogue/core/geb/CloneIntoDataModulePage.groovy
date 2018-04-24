705d096dfaae9b24f522928c4395fe38cdfe4e22
Vijay <vshukla684@gmail.com>
2018-04-24 15:57:39 +0530

    minor fixes in selectors

--- ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/geb/CloneIntoDataModulePage.groovy
+++ ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/geb/CloneIntoDataModulePage.groovy
@@ -6,8 +6,8 @@ class CloneIntoDataModulePage extends Page {
     static at = { $("div.modal-header>h4").text().contains('Clone into') }
 
     static content = {
-        searchMore(wait: true) { $('span.ng-isolate-scope.fa.fa-fw.fa-file-o') }
-        cloneModalButton(wait: true) { $('button.btn.btn-primary', text: 'OK') }
+        searchMore(wait: true) { $('span.ng-isolate-scope.fa-file-o') }
+        cloneModalButton(wait: true) { $('button.btn-primary', text: 'OK') }
     }
 
     void cloneModal() {
