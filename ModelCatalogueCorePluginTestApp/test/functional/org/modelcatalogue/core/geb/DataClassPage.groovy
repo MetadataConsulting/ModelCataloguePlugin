a0ab7c51ee8361dd506a1a691a0ae573b9872af1
Vijay <vshukla684@gmail.com>
2018-04-26 11:26:07 +0530

    implemented UserCanEditDataClassesForAdminDataModel (#1201)

---
+++ ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/geb/DataClassPage.groovy
@@ -0,0 +1,45 @@
+package org.modelcatalogue.core.geb
+
+import geb.Page
+
+class DataClassPage extends Page {
+
+    static url = '/#'
+
+    static at = { title.startsWith('History of') }
+
+    static content = {
+        editButton { $('a#role_item-detail_inline-editBtn') }
+        saveButton(wait: false, required: false) { $('button#role_item-detail_inline-edit-submitBtn') }
+//        discription(wait: false, required: false) { $('textarea') }
+//        treeView { $('div.data-model-treeview-pane', 0).module(DataModelTreeViewModule) }
+        formMetadataLink { $('span.btn.btn-link btn-sm ng-binding') }
+        tabs { $('ul.nav.nav-tabs a', text: it) }
+        parentAddButton { $('span.fa.fa-plus-circle') }
+    }
+
+    void edit() {
+        editButton.click()
+    }
+
+    void save() {
+        saveButton.click()
+    }
+
+    /*void writeDiscription(String text) {
+        discription.value(text)
+    }*/
+
+    void formMetadata() {
+        formMetadataLink.click()
+    }
+
+    void selectParents() {
+        tabs("Parents").click()
+    }
+
+    void addParent() {
+        parentAddButton[2].click()
+    }
+
+}
