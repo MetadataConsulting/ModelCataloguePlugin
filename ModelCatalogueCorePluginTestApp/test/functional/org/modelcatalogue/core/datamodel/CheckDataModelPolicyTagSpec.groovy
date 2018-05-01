a99b9e12891d4d015a8a1b28ed4ab8926be39f44
johncross <johnatcross@gmail.com>
2018-05-01 10:53:52 +0100

    See: #1240

---
+++ ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/datamodel/CheckDataModelPolicyTagSpec.groovy
@@ -0,0 +1,26 @@
+package org.modelcatalogue.core.datamodel
+
+import geb.spock.GebSpec
+import spock.lang.Issue
+import spock.lang.Narrative
+import spock.lang.Specification
+import spock.lang.Title
+
+@Issue('https://metadata.atlassian.net/browse/MET-1526')
+@Title('Examine Data Model Policy Tag')
+@Narrative('''
+ - Login to Metadata Exchange As admin | Login successful
+ - Select 'Create New Data Model' button (plus sign) from the top-right hand menu | Redirected to 'Create Data Model' page
+ - Fill the form: Name, Semantic Version, Catalogue ID
+ - From the check list of Policies select Unique of Kind data policy | Unique of kind data policy is selected
+ - Click on the green Save button | Data model is created. Redirected to new Data Model main page
+ - In the display panel on the right hand side under the title and under policies select the 'Unique of Kind' link | Redirected to Unique of Kind policy page
+ - Verify that within the Policy text there is no version numbers and each property name is classed as 'Unique' | No version numbers and all properties unique
+ - On the data model page, navigate within the display panel to select the 'edit' button on the right hand side of the data model name | Data Model information is now editable
+ - In the display panel, Under the policies section, select the 'X' (delete) button next to the 'Unique of Kind' policy to delete the policy from the data model | Data Model Policy is deleted
+ - Select the 'tick' Save button to finish editing the data model | Data Model information is no longer editable
+ - Verify that underneath the policies section for the data model, there are no data model policies. | Verify that 'Unique of Kind' data model policy has been deleted
+''')
+
+class CheckDataModelPolicyTagSpec extends GebSpec {
+}
