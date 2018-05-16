f9e6b71de0e766d7cbad3cc695a927c52c567e41
Vijay <vshukla684@gmail.com>
2018-05-16 18:25:00 +0530

    add driver for phantomjs

--- ModelCatalogueCorePluginTestApp/grails-app/conf/BuildConfig.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/conf/BuildConfig.groovy
@@ -145,6 +145,9 @@ grails.project.dependency.resolution = {
         // Testing modules
         test "org.gebish:geb-spock:$gebVersion"
         test "org.seleniumhq.selenium:selenium-chrome-driver:${seleniumVersion}"
+        compile("com.codeborne:phantomjsdriver:1.4.3") {
+            transitive = false
+        }
         test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
 
         compile 'com.github.mpkorstanje:simmetrics-core:4.1.1'
