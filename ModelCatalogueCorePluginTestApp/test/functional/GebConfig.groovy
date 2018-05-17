e2ee5f347e4027203507a305c9948d41520f80d5
Vijay <vshukla684@gmail.com>
2018-05-17 15:48:50 +0530

    Change timeout

--- ModelCatalogueCorePluginTestApp/test/functional/GebConfig.groovy
+++ ModelCatalogueCorePluginTestApp/test/functional/GebConfig.groovy
@@ -13,7 +13,7 @@ import org.openqa.selenium.phantomjs.PhantomJSDriverService
 import org.openqa.selenium.remote.DesiredCapabilities
 
 waiting {
-    timeout = 2
+    timeout = 10
 }
 
 environments {
