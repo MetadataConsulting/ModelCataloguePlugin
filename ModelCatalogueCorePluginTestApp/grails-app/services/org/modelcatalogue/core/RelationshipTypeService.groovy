3e973ceac6f22627d17af3e9c3e43f1387e3902a
James Dai <james.dai.uk@googlemail.com>
2018-05-25 13:12:56 +0100

    Extracted getRelationshipConfiguration from CatalogueElementMarshaller to RelationshipTypeService

--- ModelCatalogueCorePluginTestApp/grails-app/services/org/modelcatalogue/core/RelationshipTypeService.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/services/org/modelcatalogue/core/RelationshipTypeService.groovy
@@ -1,5 +1,6 @@
 package org.modelcatalogue.core
 
+import org.codehaus.groovy.grails.commons.GrailsClassUtils
 import org.modelcatalogue.core.persistence.RelationshipTypeGormService
 
 class RelationshipTypeService {
@@ -19,6 +20,11 @@ class RelationshipTypeService {
         relationshipTypesCache = relationshipTypeList.collectEntries { [it.name, it] }
     }
 
+    /**
+     * named map of relationship types which a class is involved in.
+     * @param cls
+     * @return
+     */
     Map<String, RelationshipType> getRelationshipTypesFor(Class cls) {
         Map<String, RelationshipType> types = typesForDomainClassCache[cls]
 
@@ -38,6 +44,75 @@ class RelationshipTypeService {
         typesForDomainClassCache.clear()
     }
 
+    /**
+     * e.g. relationships = {
+     *     "incoming": {
+     *         "hierarchy": "childOf"
+     *     }
+     *     "outgoing": {
+     *         "hierarchy": "parentOf"
+     *     }
+     *     "bidirectional": {
+     *         "relatedTo": "relatedTo"
+     *     }
+     *  }
+     * @param type
+     * @return
+     */
+    Map<String, Map<String, String>> getRelationshipConfiguration(Class type) {
+
+        Map<String, Map<String, String>> relationships  = [incoming: [:], outgoing: [:], bidirectional: [:]]
+
+        if (type.superclass && CatalogueElement.isAssignableFrom(type.superclass)) { // recursively get relationships from superclasses
+            Map<String, Map<String, String>> fromSuperclass = getRelationshipConfiguration(type.superclass)
+            relationships.incoming.putAll(fromSuperclass.incoming ?: [:])
+            relationships.outgoing.putAll(fromSuperclass.outgoing ?: [:])
+            relationships.bidirectional.putAll(fromSuperclass.bidirectional ?: [:])
+        }
+
+        // get relationships from this class
+        Map<String, Map<String, String>> fromType = GrailsClassUtils.getStaticFieldValue(type, 'relationships') ?: [incoming: [:], outgoing: [:], bidirectional: [:]]
+        relationships.incoming.putAll(fromType.incoming ?: [:])
+        relationships.outgoing.putAll(fromType.outgoing ?: [:])
+        relationships.bidirectional.putAll(fromType.bidirectional ?: [:])
+
+        getRelationshipTypesFor(type).each { String name, RelationshipType relationshipType ->
+            if (relationshipType.system) {
+                relationships.each { String direction, Map config ->
+                    config.remove name
+                }
+                return
+            }
+
+            if (relationshipType.bidirectional) {
+                if (!relationships.bidirectional.containsKey(name)) {
+                    relationships.incoming.remove(name)
+                    relationships.outgoing.remove(name)
+
+                    relationships.bidirectional[name] = RelationshipType.toCamelCase(relationshipType.sourceToDestination)
+                }
+            } else {
+                if (relationshipType.sourceClass.isAssignableFrom(type)) {
+                    if (!relationships.outgoing.containsKey(name)){
+                        relationships.bidirectional.remove(name)
+
+                        relationships.outgoing[name] = RelationshipType.toCamelCase(relationshipType.sourceToDestination)
+                    }
+                }
+                if (relationshipType.destinationClass.isAssignableFrom(type)) {
+                    if (!relationships.incoming.containsKey(name)) {
+
+                        relationships.bidirectional.remove(name)
+
+                        relationships.incoming[name] = RelationshipType.toCamelCase(relationshipType.destinationToSource)
+                    }
+                }
+            }
+        }
+
+
+        relationships
+    }
 
 
 }
