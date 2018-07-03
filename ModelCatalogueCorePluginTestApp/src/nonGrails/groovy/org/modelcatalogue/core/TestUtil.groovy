bf6093548a74821490c704f6daa03e5b5ff9f7ce
James Dai <james.dai.uk@googlemail.com>
2018-07-03 11:49:36 +0100

    run just one test for each jenkins box for expediency

--- ModelCatalogueCorePluginTestApp/src/nonGrails/groovy/org/modelcatalogue/core/TestUtil.groovy
+++ ModelCatalogueCorePluginTestApp/src/nonGrails/groovy/org/modelcatalogue/core/TestUtil.groovy
@@ -40,6 +40,7 @@ class TestUtil {
                 }
                 treeBuilder.file(newJenkinsFileName) {
                     write getJenkinsFileContent("sh '/opt/grails/bin/grails test-app -Dserver.port=8081 -Dgeb.env=chrome -DdownloadFilepath=/home/ubuntu -Dwebdriver.chrome.driver=/opt/chromedriver functional: ${tests.join(" ")}'")
+//                    write getJenkinsFileContent("sh '/opt/grails/bin/grails test-app -Dserver.port=8081 -Dgeb.env=chrome -DdownloadFilepath=/home/ubuntu -Dwebdriver.chrome.driver=/opt/chromedriver functional: ${tests[0]}'")
                 }
             }
         }
