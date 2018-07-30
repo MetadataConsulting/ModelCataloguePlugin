af9ea6a6cbde0ff5b4a9db6ec62334cde8a743c2
Vijay Shukla <vshukla684@gmail.com>
2018-07-30 13:19:22 +0530

    Add 1431 and 1437

---
+++ ModelCatalogueCorePluginTestApp/test/functional/org/modelcatalogue/core/july18/VerifyRememberMeCheckboxOnLoginPageSpec.groovy
@@ -0,0 +1,34 @@
+package org.modelcatalogue.core.july18
+
+import geb.spock.GebSpec
+import org.modelcatalogue.core.geb.*
+import spock.lang.*
+
+@Issue('https://metadata.atlassian.net/browse/MET-1431')
+@Title('Test to verify (remember me) checkbox on the LoginPage​')
+@Stepwise
+class VerifyRememberMeCheckboxOnLoginPageSpec extends GebSpec {
+
+    def "login as supervisor"() {
+        when: 'login as a curator'
+        LoginPage loginPage = to LoginPage
+        loginPage.fillUsername("supervisor")
+        loginPage.fillPassword("supervisor")
+        loginPage.clickRememberMeCheckBox()
+        submitButton.click()
+        then:
+        at DashboardPage
+    }
+
+    def "Shutdown and restart server"() {
+
+    }
+
+    def "Refresh page and check for login"() {
+        when:
+        browser.driver.navigate().refresh()
+        then:
+        at DashboardPage
+    }
+
+}
