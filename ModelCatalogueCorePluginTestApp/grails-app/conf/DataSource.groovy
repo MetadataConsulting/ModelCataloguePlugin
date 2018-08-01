b76d61172c314ba0fa9d2fea0c69d76564996c73
James Dai <james.dai.uk@googlemail.com>
2018-08-01 15:06:42 +0100

    datasource get properties

--- ModelCatalogueCorePluginTestApp/grails-app/conf/DataSource.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/conf/DataSource.groovy
@@ -12,22 +12,22 @@ environments {
     development {
         dataSource {
             pooled = true
-            driverClassName = System.getenv('MDX_DB_DRIVER') ?: 'org.h2.Driver'
-            if ( System.getenv('MC_JDBC_DRIVER') == 'com.mysql.jdbc.Driver' ) {
+            driverClassName = (System.getProperty('MDX_DB_DRIVER') ?: System.getenv('MDX_DB_DRIVER')) ?: 'org.h2.Driver'
+            if ( (System.getProperty('MC_JDBC_DRIVER') ?: System.getenv('MC_JDBC_DRIVER')) == 'com.mysql.jdbc.Driver' ) {
                 dialect='org.hibernate.dialect.MySQL5InnoDBDialect'
             }
-            url = System.getenv('MDX_DB_URL') ?: "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-            username = System.getenv('MDX_DB_USERNAME') ?: 'sa'
-            password = System.getenv('MDX_DB_PASSWORD') ?: ''
+            url = (System.getProperty('MDX_DB_URL') ?: System.getenv('MDX_DB_URL')) ?: "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            username = (System.getProperty('MDX_DB_USERNAME') ?: System.getenv('MDX_DB_USERNAME')) ?: 'sa'
+            password = (System.getProperty('MDX_DB_PASSWORD') ?: System.getenv('MDX_DB_PASSWORD')) ?: ''
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
         }
     }
     test {
-        if (System.getenv('DOCKERIZED_TESTS') && System.properties["grails.test.phase"] == 'functional') {
+        if ((System.getProperty('DOCKERIZED_TESTS') ?: System.getenv('DOCKERIZED_TESTS')) && System.properties["grails.test.phase"] == 'functional') {
             dataSource {
                 driverClassName = "com.mysql.jdbc.Driver"
                 dialect='org.hibernate.dialect.MySQL5InnoDBDialect'
-                url = "jdbc:mysql://${System.getenv('MC_DOCKER_HOST') ?: 'localhost'}:43306/tester?autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8"
+                url = "jdbc:mysql://${(System.getProperty('MC_DOCKER_HOST') ?: System.getenv('MC_DOCKER_HOST')) ?: 'localhost'}:43306/tester?autoReconnect=true&useUnicode=yes&characterEncoding=UTF-8"
                 username = 'root'
                 password = 'pa$$w0rd'
                 dbCreate = "create-drop"
@@ -46,13 +46,13 @@ environments {
         } else {
             dataSource {
                 pooled = true
-                driverClassName = System.getenv('MDX_DB_DRIVER') ?: 'org.h2.Driver'
-                if ( System.getenv('MC_JDBC_DRIVER') == 'com.mysql.jdbc.Driver' ) {
+                driverClassName = (System.getProperty('MDX_DB_DRIVER') ?: System.getenv('MDX_DB_DRIVER')) ?: 'org.h2.Driver'
+                if ( (System.getProperty('MC_JDBC_DRIVER') ?: System.getenv('MC_JDBC_DRIVER')) == 'com.mysql.jdbc.Driver' ) {
                     dialect='org.hibernate.dialect.MySQL5InnoDBDialect'
                 }
-                url = System.getenv('MDX_DB_URL') ?: "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-                username = System.getenv('MDX_DB_USERNAME') ?: 'sa'
-                password = System.getenv('MDX_DB_PASSWORD') ?: ''
+                url = (System.getProperty('MDX_DB_URL') ?: System.getenv('MDX_DB_URL')) ?: "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+                username = (System.getProperty('MDX_DB_USERNAME') ?: System.getenv('MDX_DB_USERNAME')) ?: 'sa'
+                password = (System.getProperty('MDX_DB_PASSWORD') ?: System.getenv('MDX_DB_PASSWORD')) ?: ''
                 dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
             }
         }
@@ -62,9 +62,9 @@ environments {
             driverClassName = "com.mysql.jdbc.Driver"
             dialect='org.hibernate.dialect.MySQL5InnoDBDialect'
 
-            url = System.getenv('MDX_DB_URL')//"jdbc:mysql://localhost:3306/exceltest"
-            username = System.getenv('MDX_DB_USERNAME')//'mdradmin'
-            password = System.getenv('MDX_DB_PASSWORD')//'mdradmin123'
+            url = (System.getProperty('MDX_DB_URL') ?: System.getenv('MDX_DB_URL'))//"jdbc:mysql://localhost:3306/exceltest"
+            username = (System.getProperty('MDX_DB_USERNAME') ?: System.getenv('MDX_DB_USERNAME'))//'mdradmin'
+            password = (System.getProperty('MDX_DB_PASSWORD') ?: System.getenv('MDX_DB_PASSWORD'))//'mdradmin123'
             dbCreate = "update"
             properties {
                 maxActive = -1
