ff2b0c7b313ac0eba42bfb79a872172e89d83250
James Dai <james.dai.uk@googlemail.com>
2018-08-10 13:14:53 +0100

    generate test batches ignores most specs with @Ignore

--- ModelCatalogueCorePluginTestApp/scripts/testing/generateTestBatches.sh
+++ ModelCatalogueCorePluginTestApp/scripts/testing/generateTestBatches.sh
@@ -8,9 +8,40 @@ then
     exit 1
 fi
 
-SPECNAMES="$(find test/functional -print | grep Spec.groovy | sed "s|.*/\(.*\).groovy|\1|")"
+# SPECNAMES="$(find test/functional -print | grep Spec.groovy | sed "s|.*/\(.*\).groovy|\1|")"
 
-GROUPED_SPECNAMES="$(printf "$SPECNAMES" \
+SPECS_WITHOUT_IGNORE_NAMES="$(for SPECPATH in $(find test/functional -print | grep Spec.groovy)
+do
+    IGNORE="$(cat "${SPECPATH}" | sed -n "
+        /@Ignore/ {
+            N
+            N
+            N
+            N
+            /\nclass/ p
+        }
+    ")"
+
+    # the sed command will print the contents of the file iff
+    # there is a line matching @Ignore, with one line in the 4 below (N N N N)
+    # that matches /\nclass/ i.e. begins with "class"
+
+    # echo $IGNORE
+
+    if [ -z "${IGNORE}" ] # if the string is empty,
+    then
+        # echo "${SPECPATH}" MOST LIKELY does not have Ignore at Class level, so we will take these"
+        echo "${SPECPATH}" | sed "s|.*/\(.*\).groovy|\1|" # Strip out Spec name from file name
+    else
+        # echo "${SPECPATH}" definitely DOES have Ignore at Class level, so we will not take these"
+        true
+
+    fi
+done)"
+
+echo "SPECS_WITHOUT_IGNORE_NAMES: ${SPECS_WITHOUT_IGNORE_NAMES}"
+
+GROUPED_SPECNAMES="$(printf "$SPECS_WITHOUT_IGNORE_NAMES" \
 | sed -n "
     N
     N
@@ -20,8 +51,9 @@ GROUPED_SPECNAMES="$(printf "$SPECNAMES" \
   "
 )"
 # Collect three Specs at a time:
-# for each line pull in two lines: (-e N -e N),
+# for each line pull in two lines: (N N),
 # and then substitute newline \n with space (s/\n/ /g),
+# add quotes around the line (s/\(.*\)/\"\1\"/)
 # and then print (p)
 
 printf "GROUPED_SPECNAMES: ${GROUPED_SPECNAMES}\n"
