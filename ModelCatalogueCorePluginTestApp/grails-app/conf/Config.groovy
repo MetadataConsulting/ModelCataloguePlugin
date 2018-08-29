939ce51e12b7cbbed227f0670001e9984141dabc
James Dai <james.dai.uk@googlemail.com>
2018-08-29 13:28:40 +0100

    allow request to search endpoint

--- ModelCatalogueCorePluginTestApp/grails-app/conf/Config.groovy
+++ ModelCatalogueCorePluginTestApp/grails-app/conf/Config.groovy
@@ -494,6 +494,7 @@ grails.plugin.springsecurity.basic.realmName = "Metadata Exchange"
 grails.plugin.springsecurity.filterChain.chainMap = [
         '/api/dashboard/dataModels': 'JOINED_FILTERS,-exceptionTranslationFilter',
         '/api/dashboard/*/catalogueElements': 'JOINED_FILTERS,-exceptionTranslationFilter',
+        '/api/modelCatalogue/core/catalogueElement/search' : 'JOINED_FILTERS,-exceptionTranslationFilter',
         '/api/modelCatalogue/core/validationRule/rules': 'JOINED_FILTERS,-exceptionTranslationFilter',
         '/catalogue/upload':                    'JOINED_FILTERS,-exceptionTranslationFilter',
         '/catalogue/*/*/export':                'JOINED_FILTERS,-exceptionTranslationFilter',
